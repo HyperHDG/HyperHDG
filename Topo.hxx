dcbdfc112a1044b09f3601a51aba02548d0cb48e
AndreasRupp <andreas@rupp.ink>
2020-01-28 22:37:24 +0100

    Unified include for topologies and geometries!

---
+++ Topo.hxx
@@ -0,0 +1,14 @@
+/*!*************************************************************************************************
+ * \file    Topo.hxx
+ * \brief   Allows to include all topologies by including this general hxx file.
+ *
+ * \authors   Guido Kanschat, University of Heidelberg, 2020.
+ * \authors   Andreas Rupp, University of Heidelberg, 2020.
+ **************************************************************************************************/
+
+#ifndef TOPO_HXX
+#define TOPO_HXX
+
+#include "Topo_Cubic.hxx"
+
+#endif // end of ifndef TOPO_HXX
