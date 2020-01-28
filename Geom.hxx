6f309ac3f7aef3521f0ac86c458697a4ae3258a1
AndreasRupp <andreas@rupp.ink>
2020-01-28 22:37:24 +0100

    Unified include for topologies and geometries!

---
+++ Geom.hxx
@@ -0,0 +1,14 @@
+/*!*************************************************************************************************
+ * \file    Geom.hxx
+ * \brief   Allows to include all geometries by including this general hxx file.
+ *
+ * \authors   Guido Kanschat, University of Heidelberg, 2020.
+ * \authors   Andreas Rupp, University of Heidelberg, 2020.
+ **************************************************************************************************/
+
+#ifndef GEOM_HXX
+#define GEOM_HXX
+
+#include "Geom_UnitCube.hxx"
+
+#endif // end of ifndef GEOM_HXX
