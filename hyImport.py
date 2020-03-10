e079b708c13ce20edadd273541c8c8f0a70f6d9a
AndreasRupp <andreas@rupp.ink>
2020-03-10 20:36:55 +0100

    Correcting from inconsistent OS states and remove unnecessary file.

--- hyImport.py
+++ hyImport.py
@@ -3,6 +3,10 @@ import importlib
 
 def hyImport(names):
   if not os.path.isfile("build/cythonize"):
+    if not os.path.exists("build/CythonFiles"):
+      print("\nFull build path is not available. The program corrects this and eventually dies.")
+      print("Afterwards, it can be restarted and should work out!\n")
+    os.system("mkdir -p build build/CythonFiles")
     os.system("g++ Cythonize.cxx -std=c++17 -Iinclude -o build/cythonize; build/cythonize")
   from hyCythonizer import hyPyCythonize
   retval = hyPyCythonize(names)
