6ea4b4062aa33832fa7f9a077db8102b3c07e5f3
AndreasRupp <andreas@rupp.ink>
2020-03-10 19:10:37 +0100

    Further improved just in time compilation

--- Cythonize.cxx
+++ Cythonize.cxx
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <algorithm>
 #include <sstream>
 #include <vector>
 #include <iostream>
@@ -7,19 +8,26 @@
 
 using namespace std;
 
-void hyCython( vector<string> names )
+string hyCython( vector<string> names )
 {
   hy_assert( names.size() >= 2 ,
              "The size of the names vector must be large enough for all needed compile options!" );
   
   cout << "Cythonizing " << names[1] << " ... " << flush;
   
+  // Auxiliary string for name of internal Python class
+  
+  string python_name = names[1];
+  replace( python_name.begin(), python_name.end(), '<', '_' );
+  replace( python_name.begin(), python_name.end(), '>', '_' );
+  replace( python_name.begin(), python_name.end(), ',', '_' );
+  
   // Define names of input file and output file, and define system command to cythonize files.
   
   string infileName = "./cython/" + names[0];
-  string outfileName = "./build/CythonFiles/" + names[2];
+  string outfileName = "./build/CythonFiles/" + python_name;
   
-  string cythonCommand = "cd ./build/CythonFiles/; cython -3 --cplus " + names[2] + ".pyx";
+  string cythonCommand = "cd ./build/CythonFiles/; cython -3 --cplus " + python_name + ".pyx";
   // TODO: Make automatic adaption to latest python version.
   string compileCommand = "g++ \
     -pthread -g  -I\
@@ -34,7 +42,7 @@ void hyCython( vector<string> names )
     -Wl,-Bsymbolic-functions -Wl,-z,relro -g -fstack-protector-strong -Wformat \
     -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 "
     + outfileName + ".o "
-    + "-o build/" + names[2] + ".so \
+    + "-o build/" + python_name + ".so \
     -llapack";
   
   // Introduce auxiliary variables needed for copying files and substituting keywords.
@@ -57,8 +65,8 @@ void hyCython( vector<string> names )
     {
       linestream >> word;
       if (word == "C++ClassName")     word = "\"" + names[1] + "\"";
-      if (word == "CythonClassName")  word = names[2] + "_Cython";
-      if (word == "PythonClassName")  word = names[2];
+      if (word == "CythonClassName")  word = python_name + "_Cython";
+      if (word == "PythonClassName")  word = python_name;
       outfile << word << " ";
     }
     outfile << endl;
@@ -80,8 +88,8 @@ void hyCython( vector<string> names )
     {
       linestream >> word;
       if (word == "C++ClassName")     word = "\"" + names[1] + "\"";
-      if (word == "CythonClassName")  word = names[2] + "_Cython";
-      if (word == "PythonClassName")  word = names[2];
+      if (word == "CythonClassName")  word = python_name + "_Cython";
+      if (word == "PythonClassName")  word = python_name;
       outfile << word << " ";
     }
     outfile << endl;
@@ -97,6 +105,8 @@ void hyCython( vector<string> names )
   system(linkCommand.c_str());
   
   cout << " DONE." << endl;
+  
+  return python_name;
 }
 
 
@@ -104,7 +114,6 @@ int main()
 {
   vector<string> names;
   names.push_back("Cython");
-  names.push_back("xxx");
   names.push_back("importer");
   hyCython(names);
   return 0;
--- cython/Cython.pyx
+++ cython/Cython.pyx
@@ -3,4 +3,7 @@
 def importing(names):
   if isinstance(names[0],str):
     names = [i.encode() for i in names]
-  hyCython(names)
+  name = hyCython(names)
+  if isinstance(name,bytes):
+    name = name.decode()
+  return name
