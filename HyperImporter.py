4d8106212683e9130835581d01e23187363263f2
AndreasRupp <andreas@rupp.ink>
2020-03-10 19:14:52 +0100

    Just in time programming works now for easy testcase.

--- HyperImporter.py
+++ HyperImporter.py
@@ -1,12 +1,10 @@
 import os
-from os import path
-import importlib
+from importlib import import_module
 
 def importer(names):
-#   if not path.isfile("build/cythonize"):
-  os.system("g++ Cythonize.cxx -std=c++17 -Iinclude -o build/cythonize; build/cythonize")
+  if not os.path.isfile("build/cythonize"):
+    os.system("g++ Cythonize.cxx -std=c++17 -Iinclude -o build/cythonize; build/cythonize")
   from importer import importing
   retval = importing(names)
-  from DiffusionProblemRegularNaive_1_3_1_ import DiffusionProblemRegularNaive_1_3_1_
-  return DiffusionProblemRegularNaive_1_3_1_
-  print("Importer is running!")
+  mod = import_module(retval)
+  return getattr(mod, retval)
