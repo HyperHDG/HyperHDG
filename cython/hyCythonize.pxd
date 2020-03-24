d1d925390d1e38c6da3dfa47012fc2ac772c235f
andreas <andreas.rupp@fau.de>
2020-03-24 12:54:17 +0100

    Python version now is directly set as compile flag.

--- cython/hyCythonize.pxd
+++ cython/hyCythonize.pxd
@@ -4,4 +4,4 @@ from libcpp.vector cimport vector
 from libcpp.string cimport string
 
 cdef extern from "Cythonize.cxx" :
-  cdef string hyCythonize(vector[string])
+  cdef string hyCythonize(vector[string], unsigned int, unsigned int)
--- cython/hyCythonize.pyx
+++ cython/hyCythonize.pyx
@@ -1,9 +1,9 @@
 # distutils: language=c++
 
-def hyPyCythonize(names):
+def hyPyCythonize(names, py_ver_maj, py_ver_min):
   if isinstance(names[0],str):
     names = [i.encode() for i in names]
-  filename = hyCythonize(names)
+  filename = hyCythonize(names, py_ver_maj, py_ver_min)
   if isinstance(filename,bytes):
     filename = filename.decode()
   return filename
