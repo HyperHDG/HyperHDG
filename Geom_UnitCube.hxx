d408f3e379b464c74a0a6fdd10c9e45d4488fc89
andreas <andreas.rupp@fau.de>
2020-04-14 17:41:40 +0200

    TODO: Implement mapping.

--- Geom_UnitCube.hxx
+++ Geom_UnitCube.hxx
@@ -81,7 +81,7 @@ class UnitCube
     public:
       static constexpr unsigned int space_dim() { return space_dimT; }
       static constexpr unsigned int hyEdge_dim() { return hyEdge_dimT; }
-      Point<space_dimT> map_ref_to_phys(const Point<hyEdge_dimT> pt) const
+      Point<space_dimT> map_ref_to_phys(const Point<hyEdge_dimT>& pt) const
       {Point<space_dimT> a; return a;}
       /*!*******************************************************************************************
        * \brief   Construct a cubic hyperedge from its index and a \c std::array of elements in each
