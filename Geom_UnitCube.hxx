ff858619f7211fa5d4b65ddb612713cd0a8bee11
AndreasRupp <andreas@rupp.ink>
2020-06-04 13:43:22 +0200

    Geometry compiles, but yet does not yield the correct results.

--- Geom_UnitCube.hxx
+++ Geom_UnitCube.hxx
@@ -117,7 +117,8 @@ class UnitCube
        * \param   index           The index of the hyperedge to be created.
        * \param   num_elements    A \c std::array containing number of elements per dimension.
        ********************************************************************************************/
-      hyEdge(const hyEdge_index_t index, const std::array<unsigned int, space_dimT>& num_elements);
+      hyEdge(const hyEdge_index_t index, const std::array<unsigned int, space_dimT>& num_elements)
+      {};
     
       /*!*******************************************************************************************
        * \brief   Return vertex of specified index of a hyperedge.
@@ -177,6 +178,10 @@ class UnitCube
      * A \c std::array comprising the number of elements in each spatial dimension.
      **********************************************************************************************/
     std::array<unsigned int, space_dimT> num_elements_;
+    /*!*********************************************************************************************
+     * \brief   Tensor product chain complex for elements.
+     **********************************************************************************************/
+    tpcc_chain<hyEdge_dimT, space_dimT, hyEdge_index_t> tpcc_;
   public:
     /*!*********************************************************************************************
      * \brief   Defines the return value of the class.
@@ -210,7 +215,11 @@ class UnitCube
      * \param   other       The topology of the hypergraph that has the geometry of the unit cube.
      **********************************************************************************************/
     UnitCube(const constructor_value_type& num_elements)
-    { for (unsigned int dim = 0; dim < space_dimT; ++dim) num_elements_[dim] = num_elements[dim]; }
+    : tpcc_(create_tpcc< hyEdge_dimT, space_dimT, hyEdge_index_t >(num_elements_))
+    { 
+      for (unsigned int dim = 0; dim < space_dimT; ++dim) num_elements_[dim] = num_elements[dim];
+      tpcc_ = create_tpcc< hyEdge_dimT, space_dimT, hyEdge_index_t >(num_elements_);
+    }
     /*!*********************************************************************************************
      * \brief   Construct a cubic that describes a cube hypergraph from a \c HyperGraph_Cubic.
      *
@@ -220,7 +229,9 @@ class UnitCube
      * \param   other       The topology of the hypergraph that has the geometry of the unit cube.
      **********************************************************************************************/
     UnitCube(const Topology::Cubic<hyEdge_dimT,space_dimT>& other)
-    : num_elements_(other.num_elements()) { }
+    : num_elements_(other.num_elements()),
+      tpcc_(create_tpcc< hyEdge_dimT, space_dimT, hyEdge_index_t >(other.num_elements()))
+    { }
     /*!*********************************************************************************************
      * \brief   Get geometrical hyperedge of given index.
      *
@@ -246,287 +257,5 @@ class UnitCube
 
     return Tensor::MappingMultilinear<sdim, edim, npts, T>(vertices, points_1d);
   }
-
-
-
-
-
-
-/*
- * HyperEdge functions!
- */
-
-
-template
-< 
-  unsigned int space_dimT, typename hyEdge_index_t = unsigned int, 
-  typename hyNode_index_t = hyEdge_index_t
->
-std::array<Point<space_dimT>, 2> line_to_points(const std::array<unsigned int, space_dimT>& num_lines, const hyEdge_index_t index)
-{
-  hy_assert( num_lines.size() == space_dimT , "The size of the handed over parmeter does not fit!" );
-  unsigned int orientation;
-  hyEdge_index_t num_elements_in_direction;
-  hyEdge_index_t number_with_lower_orientation = 0;
-  hyEdge_index_t index_helper = index;
-  
-  std::array<Point<space_dimT>, 2> point_indices;
-  point_indices.fill(Point<space_dimT>());
-  
-  std::array<hyEdge_index_t, space_dimT> num_lines_with_orientation;
-  num_lines_with_orientation.fill(1);
-  
-  for (unsigned int dim_m = 0; dim_m < space_dimT; ++dim_m)
-    for (unsigned int dim_n = 0; dim_n < space_dimT; ++dim_n)
-      if (dim_m == dim_n)  num_lines_with_orientation[dim_m] *= num_lines[dim_n];
-      else                 num_lines_with_orientation[dim_m] *= num_lines[dim_n] + 1;
-  
-  for ( orientation = 0;
-        number_with_lower_orientation + num_lines_with_orientation[orientation] <= index ;
-        ++orientation)
-  {
-    number_with_lower_orientation += num_lines_with_orientation[orientation];
-    hy_assert( orientation <= space_dimT , "Orientation is a space_dimT and connot exceed it." );
-  }
-  
-  std::array<hyEdge_index_t, space_dimT> local_indices;
-  local_indices.fill(0);
-  
-  index_helper -= number_with_lower_orientation;
-  for (unsigned int dim = 0; dim < space_dimT; ++dim)
-  {
-    num_elements_in_direction = num_lines[(dim + orientation) % space_dimT] + (dim != 0);
-    local_indices[(dim + orientation) % space_dimT] = index_helper % num_elements_in_direction;
-    index_helper -= local_indices[(dim + orientation) % space_dimT];
-    index_helper /= num_elements_in_direction;
-  }
-  hy_assert( index_helper == 0 , "No lines should be left any more!" );
-  
-  for(unsigned int dim_m = 0; dim_m < space_dimT; ++dim_m)
-  {
-    double helper = num_lines[dim_m];
-    point_indices[0][dim_m] = local_indices[dim_m] / helper;
-    if (dim_m == orientation)  point_indices[1][dim_m] = (local_indices[dim_m] + 1) / helper;
-    else                       point_indices[1][dim_m] = local_indices[dim_m] / helper;
-  }
-  
-  return point_indices;
-}
-
-
-template
-< 
-  unsigned int space_dimT, typename hyEdge_index_t = unsigned int, 
-  typename hyNode_index_t = hyEdge_index_t
->
-std::array<hyNode_index_t, 4> square_to_line_index(const std::array<unsigned int, space_dimT>& num_squares, const hyEdge_index_t index)
-{
-  hy_assert( num_squares.size() == space_dimT , "The size of the handed over parmeter does not fit!" );
-  unsigned int orientation;
-  hyEdge_index_t num_elements_in_direction;
-  hyEdge_index_t number_with_lower_orientation = 0;
-  hyEdge_index_t index_helper = index;
-  
-  std::array<hyNode_index_t, 4> line_indices;
-  line_indices.fill(0);
-  
-  std::array<hyEdge_index_t, space_dimT> num_squares_with_orientation;
-  num_squares_with_orientation.fill(1);
-  
-  for (unsigned int dim_m = 0; dim_m < space_dimT; ++dim_m)
-    for (unsigned int dim_n = 0; dim_n < space_dimT; ++dim_n)
-      if ( dim_m != dim_n )     num_squares_with_orientation[dim_m] *= num_squares[dim_n];
-      else if (space_dimT == 2)  num_squares_with_orientation[dim_m] *= num_squares[dim_n];
-      else if (space_dimT == 3)  num_squares_with_orientation[dim_m] *= num_squares[dim_n] + 1;
-  
-  for ( orientation = 0;
-        number_with_lower_orientation + num_squares_with_orientation[orientation] <= index ;
-        ++orientation)
-  {
-    number_with_lower_orientation += num_squares_with_orientation[orientation];
-    hy_assert( orientation <= space_dimT , "Orientation is a space_dimT and connot exceed it." );
-  }
-  
-  std::array<hyEdge_index_t, space_dimT> local_indices;
-  local_indices.fill(0);
-  
-  index_helper -= number_with_lower_orientation;
-  for (unsigned int dim = 0; dim < space_dimT; ++dim)
-  {
-    if (space_dimT == 3)  num_elements_in_direction = num_squares[(dim + orientation) % space_dimT] + (dim == 0);
-    else                 num_elements_in_direction = num_squares[(dim + orientation) % space_dimT];
-    local_indices[(dim + orientation) % space_dimT] = index_helper % num_elements_in_direction;
-    index_helper -= local_indices[(dim + orientation) % space_dimT];
-    index_helper /= num_elements_in_direction;
-  }
-  hy_assert( index_helper == 0 , "No lines should be left any more!" );
-  
-  std::array<hyEdge_index_t, space_dimT> num_lines_with_orientation;
-  num_lines_with_orientation.fill(1);
-  
-  for (unsigned int dim_m = 0; dim_m < space_dimT; ++dim_m)
-    for (unsigned int dim_n = 0; dim_n < space_dimT; ++dim_n)
-      if (dim_m == dim_n)  num_lines_with_orientation[dim_m] *= num_squares[dim_n];
-      else                 num_lines_with_orientation[dim_m] *= num_squares[dim_n] + 1;
-  
-  unsigned int local_line_index = 0;
-  for (unsigned int line_orientation = 0; line_orientation < space_dimT; ++line_orientation)
-  {
-    if (space_dimT == 3 && line_orientation == orientation)  continue;
-    number_with_lower_orientation = 0;
-    for (unsigned int dim = 0; dim < line_orientation; ++dim)
-      number_with_lower_orientation += num_lines_with_orientation[dim];
-    for (int dim = space_dimT - 1; dim >= 0; --dim)
-    {
-      num_elements_in_direction = num_squares[(dim + line_orientation) % space_dimT] + (dim != 0);
-      line_indices[local_line_index] *= num_elements_in_direction;
-      line_indices[local_line_index] += local_indices[(dim + line_orientation) % space_dimT];
-      line_indices[local_line_index + 1] *= num_elements_in_direction;
-      if (space_dimT == 2) line_indices[local_line_index + 1] += local_indices[(dim + line_orientation) % space_dimT] + (dim != 0);
-      else  line_indices[local_line_index + 1] += local_indices[(dim + line_orientation) % space_dimT] 
-                                                  + ((dim + line_orientation) % space_dimT != orientation && dim != 0);
-    }
-    line_indices[local_line_index] += number_with_lower_orientation;
-    line_indices[local_line_index + 1] += number_with_lower_orientation;
-    local_line_index += 2;
-  }
-  
-  return line_indices;
-}
-
-
-template
-< 
-  unsigned int space_dimT, typename hyEdge_index_t = unsigned int, 
-  typename hyNode_index_t = hyEdge_index_t
->
-std::array<hyNode_index_t, 6> cube_to_square_index(const std::array<unsigned int, space_dimT>& num_cubes, const hyEdge_index_t index)
-{
-  hy_assert( num_cubes.size() == space_dimT , "The size of the handed over parmeter does not fit!" );
-  hyEdge_index_t num_elements_in_direction;
-  hyEdge_index_t number_with_lower_orientation = 0;
-  hyEdge_index_t index_helper = index;
-  
-  std::array<hyNode_index_t, 6> square_indices;
-  square_indices.fill(0);
-  
-  std::array<hyEdge_index_t, space_dimT> local_indices;
-  local_indices.fill(0);
-  
-  for (unsigned int dim = 0; dim < space_dimT; ++dim)
-  {
-    num_elements_in_direction = num_cubes[dim];
-    local_indices[dim] = index_helper % num_elements_in_direction;
-    index_helper -= local_indices[dim];
-    index_helper /= num_elements_in_direction;
-  }
-  hy_assert( index_helper == 0 , "No lines should be left any more!" );
-  
-  std::array<hyEdge_index_t, space_dimT> num_squares_with_orientation;
-  num_squares_with_orientation.fill(1);
-  
-  for (unsigned int dim_m = 0; dim_m < space_dimT; ++dim_m)
-    for (unsigned int dim_n = 0; dim_n < space_dimT; ++dim_n)
-      if ( dim_m != dim_n )     num_squares_with_orientation[dim_m] *= num_cubes[dim_n];
-      else if (space_dimT == 2)  num_squares_with_orientation[dim_m] *= num_cubes[dim_n];
-      else if (space_dimT == 3)  num_squares_with_orientation[dim_m] *= num_cubes[dim_n] + 1;
-  
-  unsigned int local_square_index = 0;
-  for (unsigned int square_orientation = 0; square_orientation < space_dimT; ++square_orientation)
-  {
-    number_with_lower_orientation = 0;
-    for (unsigned int dim = 0; dim < square_orientation; ++dim)
-      number_with_lower_orientation += num_squares_with_orientation[dim];
-    for (int dim = space_dimT - 1; dim >= 0; --dim)
-    {
-      num_elements_in_direction = num_cubes[(dim + square_orientation) % space_dimT] + (dim == 0);
-      square_indices[local_square_index] *= num_elements_in_direction;
-      square_indices[local_square_index] += local_indices[(dim + square_orientation) % space_dimT];
-      square_indices[local_square_index + 1] *= num_elements_in_direction;
-      square_indices[local_square_index + 1] += local_indices[(dim + square_orientation) % space_dimT] + (dim == 0);
-    }
-    square_indices[local_square_index] += number_with_lower_orientation;
-    square_indices[local_square_index + 1] += number_with_lower_orientation;
-    local_square_index += 2;
-  }
-  
-  return square_indices;
-}
-
-template <unsigned int hyEdge_dimT, unsigned int space_dimT, typename hyE>
-UnitCube<hyEdge_dimT,space_dimT, hyE>::hyEdge::
-hyEdge(const hyE index, const std::array<unsigned int, space_dimT>& num_elements)
-{
-  if constexpr ( hyEdge_dimT == 1 )
-  {
-    points_ = line_to_points<space_dimT>(num_elements, index);
-  }
-  else if constexpr ( hyEdge_dimT == 2 )
-  {
-//    hy_assert( 0 == 1 , "This should not be executed since elasticity is not yet defined for real HYPERgraphs!" );
-    std::array<hyE, 4> line_indices = square_to_line_index<space_dimT>(num_elements, index);
-    std::array<Point<space_dimT>, 2> points1 = line_to_points<space_dimT>(num_elements, line_indices[0]);
-    std::array<Point<space_dimT>, 2> points2 = line_to_points<space_dimT>(num_elements, line_indices[1]);
-    points_[0] = points1[0];  points_[1] = points1[1];  points_[2] = points2[0];  points_[3] = points2[1];
-  }
-  else if constexpr ( hyEdge_dimT == 3 )
-  {
-//    hy_assert( 0 == 1 , "This should not be executed since elasticity is not yet defined for real HYPERgraphs!" );
-    std::array<hyE, 6> square_indices = cube_to_square_index<space_dimT>(num_elements, index);
-    std::array<hyE, 4> line_indices1 = square_to_line_index<space_dimT>(num_elements, square_indices[0]);
-    std::array<hyE, 4> line_indices2 = square_to_line_index<space_dimT>(num_elements, square_indices[1]);
-    std::array<Point<space_dimT>, 2> points1 = line_to_points<space_dimT>(num_elements, line_indices1[0]);
-    std::array<Point<space_dimT>, 2> points2 = line_to_points<space_dimT>(num_elements, line_indices1[1]);
-    std::array<Point<space_dimT>, 2> points3 = line_to_points<space_dimT>(num_elements, line_indices2[0]);
-    std::array<Point<space_dimT>, 2> points4 = line_to_points<space_dimT>(num_elements, line_indices2[1]);
-    points_[0] = points1[0];  points_[1] = points1[1];  points_[2] = points2[0];  points_[3] = points2[1];
-    points_[4] = points3[0];  points_[5] = points3[1];  points_[6] = points4[0];  points_[7] = points4[1];
-  }
-//  sort(points_.begin(), points_.end());
-}
-
-
-template <unsigned int hyEdge_dimT, unsigned int space_dimT, typename hyE>
-Point<space_dimT> UnitCube<hyEdge_dimT,space_dimT,hyE>::hyEdge::
-normal(unsigned int index) const
-{
-  Point<space_dimT> normal;
-  if (index == 0)  normal = points_[0] - points_[1];
-  else             normal = points_[1] - points_[0];
-  normal /= norm_2(normal);
-  return normal;
-}
-
-/*
-const std::array<hyNode_index_t, 2*hyEdge_dimT>&
-UnitCube<hyEdge_dimT,space_dimT>::get_hyNode_indices() const
-{
-  return hyNode_indices_;
-}
-
-
-template <unsigned int hyEdge_dimT, unsigned int space_dimT>
-std::vector<double> UnitCube<hyEdge_dimT,space_dimT>::
-abs_det_of_jacobian_at_quad(const vector<double>& local_quadrature) const
-{
-  vector<double> det_at_quad(local_quadrature.size(), 1.);
-  return det_at_quad;
-}
-
-
-template <unsigned int hyEdge_dimT, unsigned int space_dimT>
-vector< vector<double> > UnitCube<hyEdge_dimT,space_dimT>::
-inv_of_transposed_jacobian_at_quad(const vector<double>& local_quadrature) const
-{
-  vector< vector<double> > jac_at_quad(local_quadrature.size());
-  for_each(jac_at_quad.begin(), jac_at_quad.end(), [](vector<double>& local_jac)
-  {
-    local_jac.resize(1);
-    local_jac[0] = 1.;
-  });
-  return jac_at_quad;
-}
-*/
-
 
 } // end namespace Geometry
