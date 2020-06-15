79d7a264f9dcab5d3d380e3e0e52b7036cc9c7c2
andreas <andreas.rupp@fau.de>
2020-06-15 17:10:18 +0200

    Dimension test done.

--- tests_c++/DiffusionTest2.cxx
+++ tests_c++/DiffusionTest2.cxx
@@ -1,8 +1,12 @@
 #include <HyperHDG/AbstractProblem.hxx>
 #include <HyperHDG/SparseLinearAlgebra.hxx>
-#include <HyperHDG/Geometry/Cubic.hxx>
-#include <HyperHDG/NodeDescriptor/Cubic.hxx>
+#include <HyperHDG/Geometry/File.hxx>
+#include <HyperHDG/NodeDescriptor/File.hxx>
 #include <HyperHDG/LocalSolver/Diffusion.hxx>
+
+#include <cmath>
+#include <string>
+#include <type_traits>
 
 using namespace std;
 using namespace SparseLA;
@@ -19,27 +23,33 @@ using namespace SparseLA;
  * \authors   Guido Kanschat, Heidelberg University, 2020.
  * \authors   Andreas Rupp, Heidelberg University, 2020.
  **************************************************************************************************/
-int main(int argc, char *argv[])
+template < unsigned int space_dim, typename float_t >
+int do_test_uniform()
 {
+  float_t solution_tolerance = 0.;
+  if constexpr (std::is_same<float_t, double>::value)      solution_tolerance = 1e-8;
+  else if constexpr (std::is_same<float_t, float>::value)  solution_tolerance = 1e-6;
+
   bool successful = true;
-  const double solution_tolerance = 1e-6; // Note that this is the minimum due to float
-  const vector<unsigned int> num_elements = { 4 , 2 , 2 };
+  const string file_name = "domains/simplex_1_" + to_string(space_dim) + ".geo" ;
   
-  AbstractProblem< Topology::Cubic<1,3>, Geometry::UnitCube<1,3>, NodeDescriptor::Cubic<1,3>, 
-                   Diffusion_TensorialUniform<1,1,2,float>
-                 >  diffusion_problem(num_elements, num_elements, 1.f);
+  AbstractProblem
+  < 
+    Topology::File<1,space_dim>, Geometry::File<1,space_dim>, NodeDescriptor::File<1,space_dim>,
+    Diffusion_TensorialUniform<1,1,2,float_t>
+  >  diffusion_problem(file_name, (float_t) 1.);
   
-  vector<float> vectorDirichlet = diffusion_problem.return_zero_vector<float>();
-  vectorDirichlet[0] = 1.;
-  vectorDirichlet[vectorDirichlet.size()-1] = 0.;
+  vector<float_t> vectorDirichlet = diffusion_problem.template return_zero_vector<float_t>();
+  vectorDirichlet[0] = (float_t) 0.;
+  vectorDirichlet[1] = (float_t) 1.;
   
-  const vector<unsigned int> index_vector = { 0 , (unsigned int) vectorDirichlet.size()-1 };
+  const vector<unsigned int> index_vector = { 0 , 1 };
   diffusion_problem.read_dirichlet_indices(index_vector);
   
-  vector<float> vectorRHS = diffusion_problem.matrix_vector_multiply(vectorDirichlet);
-  for (unsigned int i = 0; i < vectorRHS.size(); ++i)  vectorRHS[i] *= -1.f;
+  vector<float_t> vectorRHS = diffusion_problem.matrix_vector_multiply(vectorDirichlet);
+  for (unsigned int i = 0; i < vectorRHS.size(); ++i)  vectorRHS[i] *= (float_t) -1.;
   
-  vector<float> solution;
+  vector<float_t> solution;
   try { solution = conjugate_gradient( vectorRHS, diffusion_problem ); }
   catch (SparseLASolveException& exc)
   {
@@ -47,30 +57,149 @@ int main(int argc, char *argv[])
     successful = false;
   }
   
-  solution = linear_combination(1.f, solution, 1.f, vectorDirichlet);
-    
-  const std::vector<float> python_result = 
-  { 1.,         0.6999695,  0.55280737, 0.46359316, 0.41591649, 0.72849089,
-    0.62353531, 0.52383342, 0.4428244,  0.39207816, 0.63876017, 0.57986777,
-    0.5,        0.42013223, 0.36123983, 0.72849089, 0.62353531, 0.52383342,
-    0.4428244,  0.39207816, 0.65166809, 0.58551499, 0.5,        0.41448501,
-    0.34833191, 0.60792184, 0.5571756,  0.47616658, 0.37646469, 0.27150911,
-    0.63876017, 0.57986777, 0.5,        0.42013223, 0.36123983, 0.60792184,
-    0.5571756,  0.47616658, 0.37646469, 0.27150911, 0.58408351, 0.53640684,
-    0.44719263, 0.3000305,  0. };
+  solution = linear_combination( (float_t) 1., solution, (float_t) 1., vectorDirichlet);
   
-  hy_assert ( solution.size() == python_result.size() ,
+  hy_assert ( solution.size() == space_dim + 1 ,
               "Size of solution of C++ program must be size of reference Python solution." );
-  if ( solution.size() != python_result.size() )  successful = false;
+  if ( solution.size() != space_dim + 1 )  successful = false;
   
-  for (unsigned int i = 0; i < solution.size(); ++i)
+  hy_assert( abs( solution[0] - 0.0 ) < solution_tolerance ,
+             "Difference between Python's refrence solution ans the solution is too large, i.e. "
+             << "it is " << abs( solution[0] - 0.0 ) << " in the " << 0 << "-th " <<
+             "component of the solution vector!" );
+  if ( abs( solution[0] - 0.0 ) >= solution_tolerance )  successful = false;
+
+  hy_assert( abs( solution[1] - 1.0 ) < solution_tolerance ,
+             "Difference between Python's refrence solution ans the solution is too large, i.e. "
+             << "it is " << abs( solution[1] - 1.0 ) << " in the " << 1 << "-th " <<
+             "component of the solution vector!" );
+  if ( abs( solution[1] - 1.0 ) >= solution_tolerance )  successful = false;
+
+  for (unsigned int i = 2; i < solution.size(); ++i)
   {
-    hy_assert( abs( solution[i] - python_result[i] ) < solution_tolerance ,
+    hy_assert( abs( solution[i] - 0.5 ) < solution_tolerance ,
                "Difference between Python's refrence solution ans the solution is too large, i.e. "
-               << "it is " << abs( solution[i] - python_result[i] ) << " in the " << i << "-th " <<
+               << "it is " << abs( solution[i] - 0.5 ) << " in the " << i << "-th " <<
                "component of the solution vector!" );
-    if ( abs( solution[i] - python_result[i] ) >= solution_tolerance )  successful = false;
+    if ( abs( solution[i] - 0.5 ) >= solution_tolerance )  successful = false;
   }
   
   return successful - 1;
+}
+/*!*************************************************************************************************
+ * \brief   Default parameters for the diffusion equation, cf. below.
+ *
+ * \authors   Guido Kanschat, Heidelberg University, 2019--2020.
+ * \authors   Andreas Rupp, Heidelberg University, 2019--2020.
+ **************************************************************************************************/
+template < unsigned int space_dimT, typename param_float_t = double >
+struct TestParameters
+{
+  static constexpr std::array<unsigned int, 0U> dirichlet_nodes {};
+  static constexpr std::array<unsigned int, 0U> neumann_nodes {};
+  static param_float_t inverse_diffusion_coeff( const Point<space_dimT,param_float_t>& pt )
+  { return 1.; }
+  static param_float_t right_hand_side( const Point<space_dimT,param_float_t>& pt )
+  { return 0.; }
+  static param_float_t dirichlet_value( const Point<space_dimT,param_float_t>& pt )
+  { return 0.; }
+  static param_float_t neumann_value( const Point<space_dimT,param_float_t>& pt )
+  { return 0.; }
+};
+/*!*************************************************************************************************
+ * \brief   Function that tests several aspects of the C++ implementation against a given reference
+ *          solution obtained with the Python interface.
+ *
+ * \todo    Should we also add naive tests like checking whether return_zero_vector() returns vector
+ *          of correct size only containing zeros?
+ * 
+ * This function implements an alternative to Executable.py (which usses the Cython interface).
+ *
+ * \authors   Guido Kanschat, Heidelberg University, 2020.
+ * \authors   Andreas Rupp, Heidelberg University, 2020.
+ **************************************************************************************************/
+template < unsigned int space_dim, typename float_t >
+int do_test_standard()
+{
+  float_t solution_tolerance = 0.;
+  if constexpr (std::is_same<float_t, double>::value)      solution_tolerance = 1e-8;
+  else if constexpr (std::is_same<float_t, float>::value)  solution_tolerance = 1e-6;
+
+  bool successful = true;
+  const string file_name = "domains/simplex_1_" + to_string(space_dim) + ".geo" ;
+  
+  AbstractProblem
+  < 
+    Topology::File<1,space_dim>, Geometry::File<1,space_dim,float_t>, 
+    NodeDescriptor::File<1,space_dim>, Diffusion<1,space_dim,1,2,TestParameters,float_t>
+  >  diffusion_problem(file_name, (float_t) 1.);
+  
+  vector<float_t> vectorDirichlet = diffusion_problem.template return_zero_vector<float_t>();
+  vectorDirichlet[0] = (float_t) 0.;
+  vectorDirichlet[1] = (float_t) 1.;
+  
+  const vector<unsigned int> index_vector = { 0 , 1 };
+  diffusion_problem.read_dirichlet_indices(index_vector);
+  
+  vector<float_t> vectorRHS = diffusion_problem.matrix_vector_multiply(vectorDirichlet);
+  for (unsigned int i = 0; i < vectorRHS.size(); ++i)  vectorRHS[i] *= (float_t) -1.;
+  
+  vector<float_t> solution;
+  try { solution = conjugate_gradient( vectorRHS, diffusion_problem ); }
+  catch (SparseLASolveException& exc)
+  {
+    hy_assert( 0 == 1 , exc.what() );
+    successful = false;
+  }
+  
+  solution = linear_combination( (float_t) 1., solution, (float_t) 1., vectorDirichlet);
+  
+  hy_assert ( solution.size() == space_dim + 1 ,
+              "Size of solution of C++ program must be size of reference Python solution." );
+  if ( solution.size() != space_dim + 1 )  successful = false;
+  
+  hy_assert( abs( solution[0] - 0.0 ) < solution_tolerance ,
+             "Difference between Python's refrence solution ans the solution is too large, i.e. "
+             << "it is " << abs( solution[0] - 0.0 ) << " in the " << 0 << "-th " <<
+             "component of the solution vector!" );
+  if ( abs( solution[0] - 0.0 ) >= solution_tolerance )  successful = false;
+
+  hy_assert( abs( solution[1] - 1.0 ) < solution_tolerance ,
+             "Difference between Python's refrence solution ans the solution is too large, i.e. "
+             << "it is " << abs( solution[1] - 1.0 ) << " in the " << 1 << "-th " <<
+             "component of the solution vector!" );
+  if ( abs( solution[1] - 1.0 ) >= solution_tolerance )  successful = false;
+
+  float result = 1. / (1. + sqrt(2));
+
+  for (unsigned int i = 2; i < solution.size(); ++i)
+  {
+    hy_assert( abs( solution[i] - result ) < solution_tolerance ,
+               "Difference between Python's refrence solution ans the solution is too large, i.e. "
+               << "it is " << abs( solution[i] - result ) << " in the " << i << "-th " <<
+               "component of the solution vector!" );
+    if ( abs( solution[i] - result ) >= solution_tolerance )  successful = false;
+  }
+  
+  return successful - 1;
+}
+
+
+int main(int argc, char *argv[])
+{ 
+  int result = 0;
+
+  result += do_test_uniform<1,float>() + do_test_uniform<1,double>();
+  result += do_test_uniform<2,float>() + do_test_uniform<2,double>();
+  result += do_test_uniform<3,float>() + do_test_uniform<3,double>();
+  result += do_test_uniform<4,float>() + do_test_uniform<4,double>();
+  result += do_test_uniform<5,float>() + do_test_uniform<5,double>();
+
+  result += do_test_standard<1,float>() + do_test_standard<1,double>();
+  result += do_test_standard<2,float>() + do_test_standard<2,double>();
+  result += do_test_standard<3,float>() + do_test_standard<3,double>();
+  result += do_test_standard<4,float>() + do_test_standard<4,double>();
+  result += do_test_standard<5,float>() + do_test_standard<5,double>();
+
+  return result;
 }
