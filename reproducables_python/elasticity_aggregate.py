482646efbe3c8e92bd91cf158f9e14b821873a0e
andreas <andreas.rupp@fau.de>
2020-06-18 19:27:50 +0200

    Start some more reasonable treatment of larger simulations.

--- reproducables_python/elasticity_aggregate.py
+++ reproducables_python/elasticity_aggregate.py
@@ -1,4 +1,4 @@
-# Python running example for HDG solution of a diffusion problem which is formulated on a graph!
+# Python running example for HDG solution of an elasticity problem on a superaggregate!
 
 # Import printing functions.
 from __future__ import print_function
@@ -8,12 +8,11 @@ import numpy as np
 import scipy.sparse.linalg as sp_lin_alg
 from scipy.sparse.linalg import LinearOperator
 
+# Correct the python paths!
+import os, sys
+sys.path.append(os.path.dirname(__file__) + "/..")
+
 # Predefine problem to be solved.
-# problem = "AbstractProblem < Topology::Cubic< 1, 3 >, " \
-#          +                  "Geometry::UnitCube< 1, 3 >, " \
-#          +                  "Diffusion_TensorialUniform < 1, 1, 2 * 1 > " \
-#          +                ">"
-# filenames = [ "HyperHDG/Geometry/Cubic.hxx" , "HyperHDG/LocalSolver/Diffusion.hxx" ]
 problem = "AbstractProblem < Topology::File<1,3>, Geometry::File<1,3>, NodeDescriptor::File<1,3>, "\
          +                  "LengtheningBernoulliBendingBeam<1,3,1,2> > "
 filenames = [ "HyperHDG/Geometry/File.hxx" , \
@@ -24,15 +23,13 @@ from cython_import import cython_import
 PyDP = cython_import(["AbstractProblem", problem, "string", "string"], filenames)
 
 # Initialising the wrapped C++ class HDG_wrapper.
-#HDG_wrapper = PyDP([1,1,1])
-HDG_wrapper = PyDP( "domains/aggregate_5.pts" )
+HDG_wrapper = PyDP( "domains/aggregate_1000_tree.pts" )
 
 # Initialize vector containing the Dirichlet values: Indices not set in the index_vector are ignored
 # here. However, values not equal zero in vectorDirichlet that have indices that do not occur in the
 # index vector (next) will cause a wrong representation of the final result.
 vectorDirichlet = HDG_wrapper.return_zero_vector()
 vectorDirichlet[0] = 1.
-# vectorDirichlet[len(vectorDirichlet)-1] = 1. # Comment if checking for trivial solution.
 
 # Set the hypernodes that are supposed to be of Dirichlet type.
 # Note that all non-zero entries of vectorDirichlet are supposed to be contained in the index vector
@@ -41,16 +38,9 @@ index_vector = np.array([ 0, 1, 2, 3, 6, 7, \
   len(vectorDirichlet)-4, len(vectorDirichlet)-3, len(vectorDirichlet)-2, len(vectorDirichlet)-1 ])
 HDG_wrapper.read_dirichlet_indices(index_vector)
 
-# Print index vector and vector containing the Dirichlet values.
-print("Dirichlet indices: ", index_vector)
-print("Dirichlet values: ", vectorDirichlet)
-
 # Generate right-hand side vector "vectorRHS = - A * vectorDirichlet", where vectorDirichlet is the
 # vector of Dirichlet values.
 vectorRHS = [-i for i in HDG_wrapper.matrix_vector_multiply(vectorDirichlet)]
-
-# Print right-hand side vector.
-print("Right-hand side: ", vectorRHS)
 
 # Define LinearOperator in terms of C++ functions to use scipy linear solvers in a matrix-free
 # fashion.
@@ -60,13 +50,13 @@ A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.matrix_vector
 # Solve "A * x = b" in matrix-free fashion using scipy's CG algorithm.
 [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, maxiter=1500, tol=1e-9) # Parameters for CG.
 
-# Print Solution to the problem (which is x + x_D, i.e. vectorSolution + vectorDirichlet) or number
-# of CG iterations num_iter.
+# Check, whether solution has been successful!
 if num_iter == 0:
-  print("Solution:\n", vectorSolution + vectorDirichlet)
+  print("Solution has been successfully calculated!")
 else:
   print("The linear solver (conjugate gradients) failed (did not converge)!")
 
 # Plot solution to vtu File to be visualized using Paraview.
+HDG_wrapper.plot_option("fileName", "aggreagate_1000_tree")
 HDG_wrapper.plot_solution(vectorSolution + vectorDirichlet)
 print("Solution written to file" , HDG_wrapper.plot_option("fileName", ""), "in output directory.")
