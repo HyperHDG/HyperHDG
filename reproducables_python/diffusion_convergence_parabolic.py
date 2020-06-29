214446780e7a32961f8bf8c298c33cef47b12489
AndreasRupp <andreas@rupp.ink>
2020-06-29 17:04:17 +0200

    Convergence test seems to work with initial data being solved for!

--- reproducables_python/diffusion_convergence_parabolic.py
+++ reproducables_python/diffusion_convergence_parabolic.py
@@ -18,9 +18,9 @@ class helper_class():
     self.hdg_wrapper = hdg_class
     self.delta_time = delta_time
   def multiply(self, vec):
-    vec1 = np.multiply(self.hdg_wrapper.matrix_vector_multiply(vec), self.delta_time)
-    vec2 = HDG_wrapper.mass_matrix_multiply(vec)
-    return vec1 + vec2
+    vec1 = np.multiply(self.hdg_wrapper.matrix_vector_multiply(vec), -self.delta_time)
+    vec2 = self.hdg_wrapper.mass_matrix_multiply(vec)
+    return np.add(vec1, vec2)
 
 # --------------------------------------------------------------------------------------------------
 # Function bilaplacian_test.
@@ -38,33 +38,49 @@ def diffusion_test(dimension, iteration):
   # Import C++ wrapper class to use HDG method on graphs.
   from cython_import import cython_import
   PyDP = cython_import \
-         ( ["AbstractProblem", problem, "vector[unsigned int]", "vector[unsigned int]"], filenames )
+         ( ["AbstractProblem", problem, "vector[unsigned int]", "vector[unsigned int]"], filenames, True )
 
   # Initialising the wrapped C++ class HDG_wrapper.
   HDG_wrapper = PyDP( [2 ** iteration] * dimension )
-  time_steps  = 4 * iteration * iteration
+  time_steps  = 40 * iteration
   delta_time  = 1 / time_steps
 
   helper = helper_class(HDG_wrapper, delta_time)
 
   # Generate right-hand side vector.
+  system_size = HDG_wrapper.size_of_system()
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.mass_matrix_multiply )
   vectorSolution = HDG_wrapper.initial_flux_vector(HDG_wrapper.return_zero_vector())
+  [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorSolution, tol=1e-9)
+  
+  # Generate right-hand side vector.
+  vectorRHS = [-i for i in HDG_wrapper.total_flux_vector(HDG_wrapper.return_zero_vector())]
+  system_size = HDG_wrapper.size_of_system()
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.matrix_vector_multiply )
+  [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, tol=1e-9)
 
   # Define LinearOperator in terms of C++ functions to use scipy linear solvers in a matrix-free
   # fashion.
   system_size = HDG_wrapper.size_of_system()
   A = LinearOperator( (system_size,system_size), matvec= helper.multiply )
-  M = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.mass_matrix_multiply )
+
+  print("Delta time:" + str(delta_time))
 
   for time_step in range(time_steps):
 
-    vectorRHS = [-delta_time * i for i in HDG_wrapper.total_flux_vector(HDG_wrapper.return_zero_vector())]
+    vectorRHS = \
+      np.multiply(HDG_wrapper.total_flux_vector(HDG_wrapper.return_zero_vector()), delta_time)
+    vectorSolution = HDG_wrapper.mass_matrix_multiply(vectorSolution)
 
     # Solve "A * x = b" in matrix-free fashion using scipy's CG algorithm.
-    [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS + vectorSolution, tol=1e-9)
+    [vectorSolution, num_iter] = sp_lin_alg.cg(A, np.add(vectorRHS,vectorSolution), tol=1e-9)
+    
+    if num_iter != 0:
+      print("The linear solver (conjugate gradients) failed with a total number of ",
+            num_iter, " iterations.")
 
   # Print error.
-  print("Error: " + str(HDG_wrapper.calculate_L2_error(vectorSolution, 1.)))
+  print("Error: " + str(HDG_wrapper.calculate_L2_error(vectorSolution, 0.)))
   
   # Plot obtained solution.
   HDG_wrapper.plot_option( "fileName" , "diff_c-" + str(dimension) + "-" + str(iteration) );
@@ -78,7 +94,7 @@ def diffusion_test(dimension, iteration):
 # --------------------------------------------------------------------------------------------------
 def main():
   for dimension in range(1,4):
-    for iteration in range(1, 6 - dimension):
+    for iteration in range(1, 8 - dimension):
       diffusion_test(dimension, iteration)
 
 
