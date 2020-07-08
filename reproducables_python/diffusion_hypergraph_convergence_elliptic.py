564711045c971421c9f6fe377aa3fca1eecd02bf
AndreasRupp <andreas@rupp.ink>
2020-07-08 23:12:46 +0200

    Testcase on real hypergraphs.

---
+++ reproducables_python/diffusion_hypergraph_convergence_elliptic.py
@@ -0,0 +1,75 @@
+# Python running example for HDG solution of an elasticity problem on a superaggregate!
+
+# Import printing functions.
+from __future__ import print_function
+
+# Import numpy and spares linear algebra from scipy to use the Python maths libraries.
+import numpy as np
+import scipy.sparse.linalg as sp_lin_alg
+from scipy.sparse.linalg import LinearOperator
+
+# Correct the python paths!
+import os, sys
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+
+# --------------------------------------------------------------------------------------------------
+# Function bilaplacian_test.
+# --------------------------------------------------------------------------------------------------
+def diffusion_test(poly_degree, dimension, iteration):
+  # Predefine problem to be solved.
+  problem = "AbstractProblem < Topology::Cubic<" + str(dimension) + ",3>, " \
+          + "Geometry::UnitCube<" + str(dimension) + ",3,double>, " \
+          + "NodeDescriptor::Cubic<" + str(dimension) + ",3>, " \
+          + "Diffusion<" + str(dimension) + "," + str(poly_degree) + "," + str(2*poly_degree) \
+          + ",TestParametersQuadEllipt" + str(dimension) + ",double> >"
+  filenames = [ "HyperHDG/Geometry/Cubic.hxx" , "HyperHDG/NodeDescriptor/Cubic.hxx", \
+                "HyperHDG/LocalSolver/Diffusion.hxx", \
+                "reproducables_python/parameters/diffusion.hxx" ]
+
+  # Import C++ wrapper class to use HDG method on graphs.
+  from cython_import import cython_import
+  PyDP = cython_import \
+         ( ["AbstractProblem", problem, "vector[unsigned int]", "vector[unsigned int]"], filenames )
+
+  # Initialising the wrapped C++ class HDG_wrapper.
+  HDG_wrapper = PyDP( [2 ** iteration] * 3, tau= (2**iteration) ) # Why is this so good?
+
+  # Generate right-hand side vector.
+  vectorRHS = [-i for i in HDG_wrapper.total_flux_vector(HDG_wrapper.return_zero_vector())]
+
+  # Define LinearOperator in terms of C++ functions to use scipy linear solvers in a matrix-free
+  # fashion.
+  system_size = HDG_wrapper.size_of_system()
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.matrix_vector_multiply )
+
+  # Solve "A * x = b" in matrix-free fashion using scipy's CG algorithm.
+  [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, tol=1e-9)
+
+  # Print error.
+  print("Error: ", HDG_wrapper.calculate_L2_error(vectorSolution))
+  
+  # Plot obtained solution.
+  HDG_wrapper.plot_option( "fileName" , "diff_c-" + str(dimension) + "-" + str(iteration) );
+  HDG_wrapper.plot_option( "printFileNumber" , "false" );
+  HDG_wrapper.plot_option( "scale" , "0.95" );
+  HDG_wrapper.plot_solution(vectorSolution);
+  
+
+# --------------------------------------------------------------------------------------------------
+# Function main.
+# --------------------------------------------------------------------------------------------------
+def main():
+  for poly_degree in range(1,3):
+    print("\n Polynomial degree is set to be ", poly_degree, "\n\n")
+    for dimension in range(1,4):
+      print("Dimension is ", dimension, "\n")
+      for iteration in range(6):
+        diffusion_test(poly_degree, dimension, iteration)
+
+
+# --------------------------------------------------------------------------------------------------
+# Define main function.
+# -------------------------------------------------------------------------------------------------- 
+if __name__ == "__main__":
+    main()
