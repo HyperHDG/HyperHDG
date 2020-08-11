1f77ee1df4fc26d6e37bbd0ead552a8e5aa57817
AndreasRupp <andreas@rupp.ink>
2020-08-11 22:32:11 +0200

    Transfer to smaller loop classes.

--- reproducables_python/bilaplacian_convergence_elliptic.py
+++ reproducables_python/bilaplacian_convergence_elliptic.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(__file__) + "/..")
 # --------------------------------------------------------------------------------------------------
 def bilaplacian_test(poly_degree, dimension, iteration):
   # Predefine problem to be solved.
-  problem = "AbstractProblem < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
+  problem = "EllipticLoop < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "Geometry::UnitCube<" + str(dimension) + "," + str(dimension) + ",double>, " \
           + "NodeDescriptor::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "bilaplacian<" + str(dimension) + "," + str(poly_degree) + "," + str(2*poly_degree) \
