9cf09229dd5d66fa70013ad8e8e6d40fb5215cb8
AndreasRupp <andreas@rupp.ink>
2020-08-11 22:56:53 +0200

    Shifted inverse transferred.

--- reproducables_python/bilaplacian_convergence_eigenvalue_shifted_inverse.py
+++ reproducables_python/bilaplacian_convergence_eigenvalue_shifted_inverse.py
@@ -73,7 +73,7 @@ class helper_ev_approx():
 def eigenvalue_approx_SI(poly_degree, dimension, iteration):
   
   # Predefine problem to be solved.
-  problem = "AbstractProblem < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
+  problem = "ShiftedEigenvalue < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "Geometry::UnitCube<" + str(dimension) + "," + str(dimension) + ",double>, " \
           + "NodeDescriptor::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "bilaplacian_eigs<" + str(dimension) + "," + str(poly_degree) + "," + str(2*poly_degree) \
