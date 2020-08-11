ba81d34b71dce25e2dabedba0619e9e05dee0405
AndreasRupp <andreas@rupp.ink>
2020-08-11 22:52:54 +0200

    Mass approx moved.

--- reproducables_python/bilaplacian_convergence_eigenvalue_approx.py
+++ reproducables_python/bilaplacian_convergence_eigenvalue_approx.py
@@ -69,7 +69,7 @@ class helper_ev_approx():
 def eigenvalue_approx_MA(poly_degree, dimension, iteration):
   
   # Predefine problem to be solved.
-  problem = "AbstractProblem < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
+  problem = "MassEigenvalue < Topology::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "Geometry::UnitCube<" + str(dimension) + "," + str(dimension) + ",double>, " \
           + "NodeDescriptor::Cubic<" + str(dimension) + "," + str(dimension) + ">, " \
           + "bilaplacian<" + str(dimension) + "," + str(poly_degree) + "," + str(2*poly_degree) \
