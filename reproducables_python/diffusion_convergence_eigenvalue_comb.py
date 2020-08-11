7552132ebe56b0823a9a5e30ce51989e826e4d30
AndreasRupp <andreas@rupp.ink>
2020-08-11 10:36:02 +0200

    Better approximations described and implemented for diffusion.

--- reproducables_python/diffusion_convergence_eigenvalue_comb.py
+++ reproducables_python/diffusion_convergence_eigenvalue_comb.py
@@ -4,7 +4,8 @@
 from __future__ import print_function
 
 # Import other eigenvalue solvers.
-from diffusion_convergence_eigenvalue_shifted_inverse import eigenvalue_approx
+from diffusion_convergence_eigenvalue_approx import eigenvalue_approx_MA
+from diffusion_convergence_eigenvalue_shifted_inverse import eigenvalue_approx_SI
 from diffusion_convergence_eigenvalue_newt import eigenvalue_newt
 
 # Correct the python paths!
@@ -21,9 +22,27 @@ def main():
     for dimension in range(1,3):
       print("\nDimension is ", dimension, "\n")
       for iteration in range(2,6):
-        value, vector = eigenvalue_approx(poly_degree, dimension, iteration)
-        vector.append(value)
-        eigenvalue_newt(poly_degree, dimension, iteration, vector)
+        for approx_type in range(2):
+          if approx_type == 0:
+            value, vector, errorSI = eigenvalue_approx_SI(poly_degree, dimension, iteration)
+            vector.append(value)
+            _, _, errorNLSI = eigenvalue_newt(poly_degree, dimension, iteration, vector)
+            f = open("output/diffusion_convergence_eigenvalue_combSI.txt", "a")
+            f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+                    + ". Iteration = " + str(iteration) + ". ErrorSI = " + str(errorSI) + ".\n")
+            f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+                    + ". Iteration = " + str(iteration) + ". ErrorNL = " + str(errorNLSI) + ".\n")
+            f.close()
+          else:
+            value, vector, errorMA = eigenvalue_approx_MA(poly_degree, dimension, iteration)
+            vector.append(value)
+            _, _, errorNLMA = eigenvalue_newt(poly_degree, dimension, iteration, vector)
+            f = open("output/diffusion_convergence_eigenvalue_combMA.txt", "a")
+            f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+                    + ". Iteration = " + str(iteration) + ". ErrorSI = " + str(errorMA) + ".\n")
+            f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+                    + ". Iteration = " + str(iteration) + ". ErrorNL = " + str(errorNLMA) + ".\n")
+            f.close()
 
 
 # --------------------------------------------------------------------------------------------------
