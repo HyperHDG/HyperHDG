704d0d6484f7fb274cdaf7fb0d2be143ac2ac18c
andreas <andreas.rupp@fau.de>
2020-09-22 12:14:14 +0200

    Allow execution in release mode.

--- cython/cythonize.pyx
+++ cython/cythonize.pyx
@@ -1,11 +1,11 @@
 # distutils: language=c++
 
-def hyper_cythonize(names, filenames, py_ver_maj, py_ver_min, force_comp):
+def hyper_cythonize(names, filenames, py_ver_maj, py_ver_min, force_comp, debug_mode):
   if isinstance(names[0],str):
     names = [i.encode() for i in names]
   if isinstance(filenames[0],str):
     filenames = [i.encode() for i in filenames]
-  classname = cythonize(names, filenames, py_ver_maj, py_ver_min, force_comp)
+  classname = cythonize(names, filenames, py_ver_maj, py_ver_min, force_comp, debug_mode)
   if isinstance(classname,bytes):
     classname = classname.decode()
   return classname
