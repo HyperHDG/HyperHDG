381d06d0dc316adfddc90521e523b60586a9049d
andreas <andreas.rupp@fau.de>
2020-11-24 20:21:29 +0100

    Introduce testcase for hypergraph and ip.

--- reproducables_python/parameters/diffusion.hxx
+++ reproducables_python/parameters/diffusion.hxx
@@ -72,6 +72,11 @@ struct HG
       1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
       14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
     static constexpr std::array<unsigned int, 0U> neumann_nodes{};
+    static param_float_t diffusion_coeff(const Point<space_dimT, param_float_t>& point,
+                                                 const param_float_t time = 0.)
+    {
+      return 1.;
+    }
     static param_float_t inverse_diffusion_coeff(const Point<space_dimT, param_float_t>& point,
                                                  const param_float_t time = 0.)
     {
