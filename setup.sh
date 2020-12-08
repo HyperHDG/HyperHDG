86515d8e73419cb49ba90778a155724725aac3d8
andreas <andreas.rupp@fau.de>
2020-12-08 18:54:25 +0100

    Cope with warnings in release mode and improve output of setup.

--- setup.sh
+++ setup.sh
@@ -6,23 +6,14 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "${RED}${BOLD}!! Initialize git-based submodules:${NC}"
-echo -e "${RED}   git submodule update --init --recursive${NC}"
-git submodule update --init --recursive
+(set -x; git submodule update --init --recursive)
 echo -e "${RED}${BOLD}!! Remove previous build and __pycache__ directories:${NC}"
-echo -e "${RED}   rm -rf build __pycache__${NC}"
-rm -rf build __pycache__
+(set -x; rm -rf build output */output __pycache__ */__pycache__)
 echo -e "${RED}${BOLD}!! Make new build directory:${NC}"
-echo -e "${RED}   mkdir -p build${NC}"
-mkdir -p build
-echo -e "${RED}${BOLD}!! Enter build directoy:${NC}"
-echo -e "${RED}   cd build${NC}"
-cd build
+(set -x; mkdir -p build)
 echo -e "${RED}${BOLD}!! Do the cmake:${NC}"
-echo -e "${RED}   cmake ..${NC}"
-cmake ..
+(set -x; cd build; cmake ..)
 echo -e "${RED}${BOLD}!! Build the executables:${NC}"
-echo -e "${RED}   make${NC}"
-make
+(set -x; cd build; make)
 echo -e "${RED}${BOLD}!! Conduct the tests:${NC}"
-echo -e "${RED}   test${NC}"
-make test
+(set -x; cd build; make test)
