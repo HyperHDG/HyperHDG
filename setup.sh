80af13a3ce22293796e472c851e0fb0eb8e8ee42
andreas <andreas.rupp@fau.de>
2020-12-09 12:34:10 +0100

    Remove GitHub internal warnings.

--- setup.sh
+++ setup.sh
@@ -2,18 +2,34 @@
 
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
-RED='\033[0;31m'
+COL='\033[1;33m'
 NC='\033[0m' # No Color
 
-echo -e "${RED}${BOLD}!! Initialize git-based submodules:${NC}"
+
+echo -e "${COL}${BOLD}Initialize git-based submodules ...${NC}"
 (set -x; git submodule update --init --recursive)
-echo -e "${RED}${BOLD}!! Remove previous build and __pycache__ directories:${NC}"
+
+echo -e "${COL}${BOLD}\nDo the tests as if we were GitHub ...${NC}"
+echo -e "${COL}Remove previous build and __pycache__ directories:${NC}"
 (set -x; rm -rf build output */output __pycache__ */__pycache__)
-echo -e "${RED}${BOLD}!! Make new build directory:${NC}"
+echo -e "${COL}Make new build directory:${NC}"
 (set -x; mkdir -p build)
-echo -e "${RED}${BOLD}!! Do the cmake:${NC}"
-(set -x; cd build; cmake ..)
-echo -e "${RED}${BOLD}!! Build the executables:${NC}"
+echo -e "${COL}Do the cmake:${NC}"
+(set -x; cd build; cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_FLAGS="-DNOFILEOUT" \
+           -DNOPYTHONTESTS=True ..)
+echo -e "${COL}Build the executables:${NC}"
 (set -x; cd build; make)
-echo -e "${RED}${BOLD}!! Conduct the tests:${NC}"
+echo -e "${COL}Conduct the tests:${NC}"
+(set -x; cd build; make test)
+
+echo -e "${COL}${BOLD}\nDo the full testing and installing of components ...${NC}"
+echo -e "${COL}Remove previous build and __pycache__ directories:${NC}"
+(set -x; rm -rf build output */output __pycache__ */__pycache__)
+echo -e "${COL}Make new build directory:${NC}"
+(set -x; mkdir -p build)
+echo -e "${COL}Do the cmake:${NC}"
+(set -x; cd build; cmake ..)
+echo -e "${COL}Build the executables:${NC}"
+(set -x; cd build; make)
+echo -e "${COL}Conduct the tests:${NC}"
 (set -x; cd build; make test)
