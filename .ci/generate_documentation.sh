e2603404bb82f478b0a9a859e76b48b2057e7934
andreas <andreas.rupp@fau.de>
2020-12-19 00:16:18 +0100

    Weid travis combination.

--- .ci/generate_documentation.sh
+++ .ci/generate_documentation.sh
@@ -56,7 +56,7 @@ cp -r ../doxygen/html .
 
 ##### Configure git.
 # Set the push default to simple i.e. push only the current branch.
-#git config --global push.default simple
+git config --global push.default simple
 # Pretend to be an user called Travis CI.
 #git config user.name "Travis CI"
 #git config user.email "travis@travis-ci.org"
