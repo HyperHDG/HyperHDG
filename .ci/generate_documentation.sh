f4b63ccc9e82f241077e0da1e4e6287eb135ed1b
andreas <andreas.rupp@fau.de>
2020-12-18 23:40:06 +0100

    Remove redundant line

--- .ci/generate_documentation.sh
+++ .ci/generate_documentation.sh
@@ -46,7 +46,7 @@ DOXYFILE=$TRAVIS_BUILD_DIR/doxygen/Doxyfile
 
 
 # Get the current gh-pages branch
-git clone -b gh-pages https://github.com/$GH_REPO_ORG/$GH_REPO_NAME code_docs
+git clone -b gh-pages https://github.com/$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
 cd code_docs
 
 cp -r ../doxygen/html .
@@ -96,7 +96,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push --force
+    git push
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
