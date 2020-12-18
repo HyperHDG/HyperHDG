5fe05975f8fc569a2399271a2069202710485ff5
andreas <andreas.rupp@fau.de>
2020-12-19 00:02:11 +0100

    Weid travis combination.

--- .ci/generate_documentation.sh
+++ .ci/generate_documentation.sh
@@ -96,7 +96,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    # git push
+    git push --github-token=$TRAVIS_ALL_RIGHTS_ON_REPO
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
