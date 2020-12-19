e436d2191eefefe60c1257d3cf4b07579bb0a23c
andreas <andreas.rupp@fau.de>
2020-12-19 01:05:27 +0100

    ...

--- .ci/generate_documentation.sh
+++ .ci/generate_documentation.sh
@@ -19,46 +19,32 @@
 # - GH_REPO_REF         : The GitHub reference to the repository.
 # - GH_REPO_TOKEN       : Secure token to the github repository.
 #
-# For information on how to encrypt variables for Travis CI please go to
-# https://docs.travis-ci.com/user/environment-variables/#Encrypted-Variables
-# or https://gist.github.com/vidavidorra/7ed6166a46c537d3cbd2
-# For information on how to create a clean gh-pages branch from the master
-# branch, please go to https://gist.github.com/vidavidorra/846a2fc7dd51f4fe56a0
-#
-# This script will generate Doxygen documentation and push the documentation to
-# the gh-pages branch of a repository specified by GH_REPO_REF.
-# Before this script is used there should already be a gh-pages branch in the
-# repository.
-# 
-################################################################################
+# This script will generate Doxygen documentation and push the documentation to the gh-pages branch
+# of a repository specified by GH_REPO_REF. Before this script is used there should already be a
+# gh-pages branch in the repository.
+###################################################################################################
 
-################################################################################
-##### Setup this script and get the current gh-pages branch.               #####
+# Setup this script and get the current gh-pages branch.
 echo 'Setting up the script...'
-# Exit with nonzero exit code if anything fails
+
+# Exit with nonzero exit code if anything fails.
 set -e
 
+# Set global variables.
 GH_REPO_ORG=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 1`
 GH_REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 2`
-GH_REPO_REF="github.com/$GH_REPO_ORG/$GH_REPO_NAME.git"
 DOXYFILE=$TRAVIS_BUILD_DIR/doxygen/Doxyfile
 
-
-# Get the current gh-pages branch
+# Get the current gh-pages branch and enter its directory.
 git clone -b gh-pages https://github.com/$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
 cd code_docs
-
-cp -r ../doxygen/html .
-
-# Copy DoxygenLayout.xml here
-# cp $TRAVIS_BUILD_DIR/doc/DoxygenLayout.xml .
 
 ##### Configure git.
 # Set the push default to simple i.e. push only the current branch.
 git config --global push.default simple
 # Pretend to be an user called Travis CI.
-#git config user.name "AndreasRupp"
-#git config user.email "HyperHDG@rupp.ink"
+git config user.name "AndreasRupp"
+git config user.email "HyperHDG@rupp.ink"
 
 # go back to first commit
 git reset --hard `git rev-list --max-parents=0 --abbrev-commit HEAD`
@@ -68,6 +54,9 @@ git reset --hard `git rev-list --max-parents=0 --abbrev-commit HEAD`
 # Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
 # to NO, which it is by default. So creating the file just in case.
 echo "" > .nojekyll
+
+cp -r ../doxygen/html .
+
 
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
