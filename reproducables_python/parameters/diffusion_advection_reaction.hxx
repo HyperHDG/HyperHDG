9122c35d08a3fe1e4b1f2f56e1958730c25fd8ba
andreas <andreas.rupp@fau.de>
2020-12-22 13:49:00 +0100

    Use M_PI

--- reproducables_python/parameters/diffusion_advection_reaction.hxx
+++ reproducables_python/parameters/diffusion_advection_reaction.hxx
@@ -12,21 +12,20 @@
 template <unsigned int space_dimT, typename param_float_t = double>
 struct TestParametersSinEllipt
 {
-  static constexpr double pi = 3.141592653589793238462643383279502884197169399375105820974944592307;
   static constexpr std::array<unsigned int, 26U> dirichlet_nodes{
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
   static constexpr std::array<unsigned int, 0U> neumann_nodes{};
   static param_float_t inverse_diffusion_coeff(const Point<space_dimT, param_float_t>& point,
                                                const param_float_t = 0.)
   {
-    return 1. / cos(0.25 * pi * point[0]);
+    return 1. / cos(0.25 * M_PI * point[0]);
   }
   static SmallVec<space_dimT, param_float_t> advective_velocity(
     const Point<space_dimT, param_float_t>& point,
     const param_float_t = 0.)
   {
     SmallVec<space_dimT, param_float_t> advection;
-    advection[0] = -0.25 * pi * sin(0.25 * pi * point[0]) + 1.;
+    advection[0] = -0.25 * M_PI * sin(0.25 * M_PI * point[0]) + 1.;
     return inverse_diffusion_coeff(point) * advection;
   }
   static param_float_t reaction_rate(const Point<space_dimT, param_float_t>&,
@@ -37,13 +36,13 @@ struct TestParametersSinEllipt
   static param_float_t analytic_result(const Point<space_dimT, param_float_t>& point,
                                        const param_float_t = 0.)
   {
-    return sin(0.25 * pi * point[0]);
+    return sin(0.25 * M_PI * point[0]);
   }
 
   static param_float_t right_hand_side(const Point<space_dimT, param_float_t>& point,
                                        const param_float_t = 0.)
   {
-    return sin(0.25 * pi * point[0]) + 0.25 * pi * cos(0.25 * pi * point[0]);
+    return sin(0.25 * M_PI * point[0]) + 0.25 * M_PI * cos(0.25 * M_PI * point[0]);
   }
 
   static param_float_t dirichlet_value(const Point<space_dimT, param_float_t>& point,
