d3885082a8e94f40562f364b0aeaf9ae43c2ad5f
Andreas Rupp <57216080+AndreasRupp@users.noreply.github.com>
2020-12-23 11:02:45 +0100

    Decrease execution time of reproducables (#25)

--- reproducables_python/diffusion_hypergraph_convergence_eigenvalue_shifted_inverse.py
+++ reproducables_python/diffusion_hypergraph_convergence_eigenvalue_shifted_inverse.py
@@ -23,30 +23,24 @@ class helper_ev_approx():
     self.hdg_wrapper       = hdg_wrapper
     self.sigma             = sigma
     self.dirichlet_inidces = hdg_wrapper.dirichlet_indices()
+    self.index_vector      = [-1] * self.short_vector_size()
+    self.vector_index      = [-1] * self.long_vector_size()
+    n_indices = 0
+    for i in range(len(self.index_vector)):
+      while n_indices < len(self.dirichlet_inidces) and \
+            i + n_indices == self.dirichlet_inidces[n_indices]:
+        n_indices = n_indices + 1
+      self.index_vector[i] = i + n_indices
+      self.vector_index[i + n_indices] = i
+    assert -1 not in self.index_vector
   def long_vector_size(self):
     return self.hdg_wrapper.size_of_system()
   def short_vector_size(self):
     return self.hdg_wrapper.size_of_system() - len(self.dirichlet_inidces)
   def long_vector(self, vector):
-    assert len(vector) == self.short_vector_size()
-    vec = [0.] * (len(vector)+len(self.dirichlet_inidces))
-    n_indices = 0
-    for i in range(len(vector)):
-      while n_indices < len(self.dirichlet_inidces) and \
-            i + n_indices == self.dirichlet_inidces[n_indices]:
-        n_indices = n_indices + 1
-      vec[i + n_indices] = vector[i]
-    return vec
+    return [vector[x] if x > -1 else 0. for x in self.vector_index]
   def short_vector(self, vector):
-    assert len(vector) == self.long_vector_size()
-    vec = [0.] * (len(vector)-len(self.dirichlet_inidces))
-    n_indices = 0
-    for i in range(len(vec)):
-      while n_indices < len(self.dirichlet_inidces) and \
-            i + n_indices == self.dirichlet_inidces[n_indices]:
-        n_indices = n_indices + 1
-      vec[i] = vector[i + n_indices]
-    return vec
+    return [vector[x] for x in self.index_vector]
   def multiply_stiff(self, vector):
     vec = self.hdg_wrapper.matrix_vector_multiply(self.long_vector(vector))
     vec = np.multiply(self.short_vector(vec), -1.)
@@ -98,6 +92,7 @@ def eigenvalue_approx_SI(poly_degree, dimension, iteration, debug_mode=False):
   const.cython_replacements = ["vector[unsigned int]", "vector[unsigned int]"]
   const.include_files   = ["reproducables_python/parameters/diffusion.hxx"]
   const.debug_mode      = debug_mode
+  const.allow_file_output = False
 
   PyDP = cython_import.cython_import(const)
 
