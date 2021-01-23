ef92a544326e86a4d9a540203c5dab3dda336d9e
AndreasRupp <andreas.rupp@fau.de>
2021-01-24 00:41:05 +0100

    Require compiler to be passed to scripts.

--- shell_scripts/setup.sh
+++ shell_scripts/setup.sh
@@ -6,6 +6,14 @@ COL='\e[0;97;41m'
 NOR='\e[0m'
 
 
+if [ $# -ne 1 ]; then
+  echo "You have not set a compiler, please provide the compiler to be utilized."
+  exit 1
+fi
+
+CXX=$1
+
+
 echo -e "${COL}${BOLD}Initialize git-based submodules ...${NOR}"
 (set -x; cd $(dirname $(readlink -f "$0"))/..; make submodules)
 
@@ -16,4 +24,4 @@ echo -e "\n${COL}${BOLD}Make doxygen ...${NOR}"
 (set -x; cd $(dirname $(readlink -f "$0"))/..; make doxygen > doxygen/doxy_log.txt)
 
 echo -e "\n${COL}${BOLD}Install and test all components ...${NOR}"
-(set -x; cd $(dirname $(readlink -f "$0"))/..; make build)
+(set -x; cd $(dirname $(readlink -f "$0"))/..; make build CXX=$CXX)
