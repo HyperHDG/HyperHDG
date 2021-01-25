f862825a5ec33a6076304d08a48ec35874bbd5cd
AndreasRupp <andreas.rupp@fau.de>
2021-01-25 16:00:36 +0100

    Use latest version of TPCC.

--- shell_scripts/push_test.sh
+++ shell_scripts/push_test.sh
@@ -6,14 +6,6 @@ COL='\e[0;97;41m'
 NOR='\e[0m'
 
 
-if [ $# -ne 1 ]; then
-  echo "You have not set a compiler, please provide the compiler to be utilized."
-  exit 1
-fi
-
-CXX=$1
-
-
 echo -e "${COL}${BOLD}Clean up ...${NOR}"
 (set -x; cd $(dirname $(readlink -f "$0"))/..; make clean |& tee output/push_test.txt)
 
@@ -32,7 +24,7 @@ echo -e "\n${COL}${BOLD}Make doxygen ...${NOR}"
   make doxygen > doxygen/doxy_log.txt |& tee -a output/push_test.txt)
 
 echo -e "\n${COL}${BOLD}Re-install and test all components ...${NOR}"
-(set -x; cd $(dirname $(readlink -f "$0"))/..; make build CXX=$CXX |& tee -a output/push_test.txt)
+(set -x; cd $(dirname $(readlink -f "$0"))/..; make build |& tee -a output/push_test.txt)
 
 echo -e "\n${COL}${BOLD}Check whether tests have passed ...${NOR}"
 (set -x; cd $(dirname $(readlink -f "$0")); python3 check_push_test.py)
