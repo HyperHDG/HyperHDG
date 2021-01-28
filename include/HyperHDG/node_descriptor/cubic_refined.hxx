ca5206d1e34e940810e8979fc37c0903ae6fcade
AndreasRupp <andreas.rupp@fau.de>
2021-01-28 13:36:49 +0100

    Refined node descriptor.

--- include/HyperHDG/node_descriptor/cubic_refined.hxx
+++ include/HyperHDG/node_descriptor/cubic_refined.hxx
@@ -22,11 +22,12 @@ namespace NodeDescriptor
  **************************************************************************************************/
 template <unsigned int hyEdge_dimT,
           unsigned int space_dimT,
+          unsigned int n_subintervalsT = 1,
           typename NodeTypeVecT = SmallVec<2 * hyEdge_dimT, unsigned int>,
           typename ConstructorVecT = SmallVec<space_dimT, unsigned int>,
           typename hyEdge_index_t = unsigned int,
           typename hyNode_index_t = hyEdge_index_t>
-class Cubic
+class CubicRefined
 {
   /*!***********************************************************************************************
    * \brief   Definition of the node types of a hypergraph's edges.
@@ -52,7 +53,7 @@ class Cubic
     hyEdge(const hyEdge_index_t index, const Cubic& node_desc)
     {
       Wrapper::tpcc_elem_t<hyEdge_dimT, space_dimT> elem =
-        Wrapper::get_element(node_desc.tpcc_elements_, index);
+        Wrapper::get_element(node_desc.tpcc_elements_, index / Wrapper::n_elements(tpcc_ref_elem_));
       for (unsigned int i = 0; i < hyFace_types_.size(); ++i)
       {
         Wrapper::tpcc_elem_t<hyEdge_dimT - 1, space_dimT> face = Wrapper::get_face(elem, i);
@@ -64,6 +65,16 @@ class Cubic
           if (coordinate == 0 || coordinate == node_desc.n_elements().operator[](direction))
             hyFace_types_[i] += (1 + (coordinate != 0)) * std::pow(3, direction);
         }
+      }
+
+      Wrapper::tpcc_elem_t<hyEdge_dimT, hyEdge_dimT> ref_elem =
+        Wrapper::get_element(topology.tpcc_ref_elem_, index % Wrapper::n_elements(tpcc_ref_elem_));
+      for (unsigned int i = 0; i < hyNode_indices_.size(); ++i)
+      {
+        Wrapper::tpcc_elem_t<hyEdge_dimT - 1, hyEdge_dimT> face = Wrapper::get_face(ref_elem, i);
+        if (Wrapper::exterior_coordinate(face, 0) != 0 &&
+            Wrapper::exterior_coordinate(face, 0) != n_subintervalsT + 1)
+          hyNode_indices_[i] = 0;
       }
     }
     /*!*********************************************************************************************
@@ -100,6 +111,8 @@ class Cubic
    ************************************************************************************************/
   const Wrapper::tpcc_t<hyEdge_dimT, space_dimT, TPCC::boundaries::both, hyNode_index_t>
     tpcc_elements_;
+
+  const Wrapper::tpcc_t<hyEdge_dimT, hyEdge_dimT, hyNode_index_t> tpcc_ref_elem_;
   /*!***********************************************************************************************
    * \brief   Total amount of hyperedges in hypergraph.
    ************************************************************************************************/
@@ -119,11 +132,14 @@ class Cubic
    *
    * \param   n_elements    A vector/array containing number of elements per spatial dimension.
    ************************************************************************************************/
-  Cubic(const ConstructorVecT& n_elements)
+  CubicRefined(const ConstructorVecT& n_elements)
   : n_elements_(n_elements),
     tpcc_elements_(
       Wrapper::create_tpcc<hyEdge_dimT, space_dimT, TPCC::boundaries::both, hyEdge_index_t>(
         n_elements)),
+    tpcc_ref_elem_(
+      Wrapper::create_tpcc<hyEdge_dimT, hyEdge_dimT, TPCC::boundaries::both, hyEdge_index_t>(
+        SmallVec<hyEdge_dimT, unsigned int>(n_subintervalsT))),
     n_hyEdges_(Wrapper::n_elements(tpcc_elements_))
   {
   }
@@ -132,9 +148,10 @@ class Cubic
    *
    * \param   other         Topology to which the node descriptor will fit.
    ************************************************************************************************/
-  Cubic(const Topology::Cubic<hyEdge_dimT, space_dimT>& other)
+  Cubicrefined(const Topology::Cubic<hyEdge_dimT, space_dimT>& other)
   : n_elements_(other.n_elements()),
     tpcc_elements_(other.tpcc_elem()),
+    tpcc_ref_elem_(other.tpcc_ref_elem_),
     n_hyEdges_(other.n_hyEdges())
   {
   }
@@ -167,6 +184,6 @@ class Cubic
    * \retval  n_hyperedges  The total amount of hyperedges of a hypergraph.
    ************************************************************************************************/
   const hyEdge_index_t n_hyEdges() const { return n_hyEdges_; }
-};  // end of class Cubic
+};  // end of class CubicRefined
 
 }  // end of namespace NodeDescriptor
