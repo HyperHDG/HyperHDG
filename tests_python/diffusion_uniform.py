4e0d556852ff3e33a3558f2a7045fd879b0c79e1
AndreasRupp <andreas.rupp@fau.de>
2021-01-30 18:22:35 +0100

    Check whether failure is possible

--- tests_python/diffusion_uniform.py
+++ tests_python/diffusion_uniform.py
@@ -88,5 +88,6 @@ reference_solution = np.array(
     0.63876017, 0.57986777, 0.5,        0.42013223, 0.36123983, 0.60792184,
     0.5571756,  0.47616658, 0.37646469, 0.27150911, 0.58408351, 0.53640684,
     0.44719263, 0.3000305,  0. ])
-    
+
+assert 0==1
 assert np.linalg.norm(reference_solution - vectorSolution - vectorDirichlet, np.inf) < tolerance
