fda330e7e61437b4c3d5233690185cc15524a081
AndreasRupp <andreas.rupp@fau.de>
2021-04-14 19:48:20 +0200

    Also fail in case of assertions.

--- shell_scripts/check_reproducibles_test.py
+++ shell_scripts/check_reproducibles_test.py
@@ -4,8 +4,8 @@ with open(os.path.dirname(os.path.abspath(__file__)) + "/../output/reproducibles
   as file:
   content = file.read()
 
-assert ("warning" or "Warning" or "error" or "fail" or "Fail") not in content, \
-  "Some tests seem to have failed!"
+assert ("warning" or "Warning" or "error" or "fail" or "Fail" or "assert" or "Assert") \
+  not in content, "Some tests seem to have failed!"
 
 index = re.search("Error", content)
 while index != None:
