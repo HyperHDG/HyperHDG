499f44d3c9fac0fc737a4c01790ec8ca659307a0
AndreasRupp <andreas.rupp@fau.de>
2021-04-14 19:13:06 +0200

    Implement local testing for all reproducibles.

---
+++ shell_scripts/reproducibles_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+BOLD=$(tput bold)
+COL='\e[0;97;41m'
+NOR='\e[0m'
+
+TIME=15s
+FILES=$(dirname $(readlink -f "$0"))/../reproducibles_python/*.py
+
+
+(set -x; cd $(dirname $(readlink -f "$0"))/..; rm -f output/reproducibles_test.txt)
+for file in $FILES
+do
+  echo -e "\n\n${COL}${BOLD}Try $file ...${NOR}" |& tee -a output/reproducibles_test.txt
+  (set -x; cd $(dirname $(readlink -f "$0"))/..; \
+    timeout $TIME python3 $file True |& tee -a output/reproducibles_test.txt)
+done
