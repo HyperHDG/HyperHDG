09c7fa9a2c98a7efb7334fad949a527d53ed6588
Andreas Rupp <57216080+AndreasRupp@users.noreply.github.com>
2022-02-10 13:06:16 +0200

    Program version of ESAIM:M2AN publication (#89)

---
+++ reproducibles_python/diffusion_convergence_hypergraph_elliptic_high_order_postprocess.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+
+import numpy as np
+import scipy.sparse.linalg as sp_lin_alg
+from scipy.sparse.linalg import LinearOperator
+
+from datetime import datetime
+
+import os, sys
+
+
+# --------------------------------------------------------------------------------------------------
+# Function bilaplacian_test.
+# --------------------------------------------------------------------------------------------------
+def diffusion_test(poly_degree, dimension, iteration, refinement, debug_mode=False):
+  start_time = datetime.now()
+  print("Starting time is", start_time)
+  os.system("mkdir -p output")
+  
+  try:
+    import HyperHDG
+  except (ImportError, ModuleNotFoundError) as error:
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../import")
+    import HyperHDG
+  
+  const                 = HyperHDG.config()
+  const.global_loop     = "Elliptic"
+  const.topology        = "Cubic<" + str(dimension) + ",3>"
+  const.geometry        = "UnitCube<" + str(dimension) + ",3,double>"
+  const.node_descriptor = "Cubic<" + str(dimension) + ",3>"
+  const.local_solver    = "DiffusionPostprocess<" + str(dimension) + "," + str(poly_degree) + "," \
+    + str(6) + ",HG<" + str(dimension) + ">::TestParametersQuadElliptHO,double>"
+  const.cython_replacements = ["vector[unsigned int]", "vector[unsigned int]"]
+  const.include_files   = ["reproducibles_python/parameters/diffusion.hxx"]
+  const.debug_mode      = debug_mode
+
+  PyDP = HyperHDG.include(const)
+  HDG_wrapper = PyDP( [2 ** iteration] * 3 )
+  
+  HDG_wrapper.refine( 2 ** refinement )
+
+  vectorRHS = np.multiply(HDG_wrapper.residual_flux(HDG_wrapper.zero_vector()), -1.)
+
+  system_size = HDG_wrapper.size_of_system()
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
+
+  [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, tol=1e-13)
+  if num_iter != 0:
+    raise RuntimeError("Linear solver did not converge!")
+
+  error = HDG_wrapper.errors(vectorSolution)[0]
+  print("Refinement: ", refinement, " Error: ", error)
+  f = open("output/diffusion_convergence_hypergraph_elliptic_high_order_pp.txt", "a")
+  f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+          + ". Refinement = " + str(refinement) + ". Error = " + str(error) + ".\n")
+  f.close()
+  
+  HDG_wrapper.plot_option( "fileName", "diff_hg_conv_ho_pp-"+str(dimension)+"-"+str(refinement) )
+  HDG_wrapper.plot_option( "printFileNumber", "false" )
+  HDG_wrapper.plot_option( "scale", "0.95" )
+  HDG_wrapper.plot_solution(vectorSolution)
+  
+  end_time = datetime.now()
+  print("Program ended at", end_time, "after", end_time-start_time)
+  
+
+# --------------------------------------------------------------------------------------------------
+# Function main.
+# --------------------------------------------------------------------------------------------------
+def main(debug_mode):
+  iteration = 1
+  for poly_degree in range(4):
+    print("\n Polynomial degree is set to be ", poly_degree, "\n\n")
+    for dimension in range(2,3):
+      print("Dimension is ", dimension, "\n")
+      for refinement in range(7):
+        try:
+          diffusion_test(poly_degree, dimension, iteration, refinement, debug_mode)
+        except RuntimeError as error:
+          print("ERROR: ", error)
+
+
+# --------------------------------------------------------------------------------------------------
+# Define main function.
+# -------------------------------------------------------------------------------------------------- 
+if __name__ == "__main__":
+  main(len(sys.argv) > 1 and sys.argv[1] == "True")
--- reproducibles_python/parameters/diffusion.hxx
+++ reproducibles_python/parameters/diffusion.hxx
@@ -101,6 +101,55 @@ struct HG
       return 0.;
     }
   };
+  template <unsigned int space_dimT, typename param_float_t = double>
+  struct TestParametersQuadElliptHO
+  {
+    static constexpr std::array<unsigned int, 26U> dirichlet_nodes{
+      1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
+    static constexpr std::array<unsigned int, 0U> neumann_nodes{};
+    static param_float_t diffusion_coeff(const Point<space_dimT, param_float_t>&,
+                                         const param_float_t = 0.)
+    {
+      return 0.25 / M_PI / M_PI;
+    }
+    static param_float_t inverse_diffusion_coeff(const Point<space_dimT, param_float_t>&,
+                                                 const param_float_t = 0.)
+    {
+      return 4. * M_PI * M_PI;
+    }
+
+    static param_float_t analytic_result(const Point<space_dimT, param_float_t>& point,
+                                         const param_float_t = 0.)
+    {
+      param_float_t result = 0;
+      for (unsigned int dim = 0; dim < space_dimT; ++dim)
+        result += sin(2. * M_PI * point[dim]);
+      return result;
+    }
+
+    static param_float_t right_hand_side(const Point<space_dimT, param_float_t>& point,
+                                         const param_float_t = 0.)
+    {
+      return analytic_result(point);
+    }
+
+    static param_float_t dirichlet_value(const Point<space_dimT, param_float_t>& point,
+                                         const param_float_t = 0.)
+    {
+      return analytic_result(point);
+    }
+    static param_float_t initial(const Point<space_dimT, param_float_t>& point,
+                                 const param_float_t = 0.)
+    {
+      return analytic_result(point);
+    }
+    static param_float_t neumann_value(const Point<space_dimT, param_float_t>&,
+                                       const param_float_t = 0.)
+    {
+      return 0.;
+    }
+  };
 };
 
 template <unsigned int space_dimT, typename param_float_t = double>
