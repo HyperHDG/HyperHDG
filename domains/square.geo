d81deda588199b448a1df38682ea922b58030d53
AndreasRupp <andreas.rupp@fau.de>
2024-02-13 15:46:17 +0200

    Base case added.

---
+++ domains/square.geo
@@ -0,0 +1,38 @@
+# File containing geometric and topological information for of hypergraph being a triangle.
+
+# The following parameters need to be given in the specified order!
+
+Space_Dim     = 2;  # Dimension of space indicating amount of coordinates of points.
+HyperEdge_Dim = 2;  # Dimension of hyperedge (must be uniform).
+
+N_Points      = 4;  # Number of vertices that are contained in the hypergraph.
+N_HyperNodes  = 4;  # Number of hypernodes that are contained in the hypergraph.
+N_HyperEdges  = 1;  # Number of hyperedges that are contained in the hypergraph.
+
+
+# After "POINTS:" the coordinates of the points have to appear. That is, the next lines contain one
+# point and therefore "Space_Dimension" numbers.
+
+POINTS:
+0.0 0.0
+1.0 0.0
+1.0 1.0
+0.0 1.0
+
+# After HYPERNODES_OF_HYPEREDGES the indices of hypernodes belonging to a hyperedge have to appear.
+
+HYPERNODES_OF_HYPEREDGES:
+ 0  1  2  3
+
+# After TYPES OF HYPERFACES the type of the hyperface is denoted. As an example, 0 might indicate
+# an interior face, 1 a Dirichlet face, and 2 a Neumann face.
+# Note that a node can be an interior and a Neumann face, but if it is Dirichlet, all related faces
+# have to be of Dirichlet type.
+
+TYPES_OF_HYPERFACES:
+1 1 1 1
+
+# After POINTS_OF_HYPEREDGES the indices of points belonging to a hyperedge have to appear.
+
+POINTS_OF_HYPEREDGES:
+ 0  1  3  2
---
+++ reproducibles_python/test_from_file.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+import numpy as np
+import scipy.sparse.linalg as sp_lin_alg
+from scipy.sparse.linalg import LinearOperator
+
+from datetime import datetime
+
+import os, sys
+
+
+# --------------------------------------------------------------------------------------------------
+# Function bilaplacian_test.
+# --------------------------------------------------------------------------------------------------
+def diffusion_test(poly_degree, dimension, iteration, debug_mode=False):
+  start_time = datetime.now()
+  print("Starting time is", start_time)
+  os.system("mkdir -p output")
+  
+  try:
+    import HyperHDG
+  except (ImportError, ModuleNotFoundError) as error:
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../import")
+    import HyperHDG
+  
+  const                 = HyperHDG.config()
+  const.global_loop     = "Elliptic"
+  # const.topology        = "Cubic<" + str(dimension) + "," + str(dimension) + ">"
+  # const.geometry        = "UnitCube<" + str(dimension) + "," + str(dimension) + ",double>"
+  # const.node_descriptor = "Cubic<" + str(dimension) + "," + str(dimension) + ">"
+  const.topology        = "File<2,2,std::vector,Point<2,double> >"
+  const.geometry        = "File<2,2,std::vector,Point<2,double> >"
+  const.node_descriptor = "File<2,2,std::vector,Point<2,double> >"
+  const.local_solver    = "Diffusion<" + "2" + "," + str(poly_degree) + "," \
+    + str(2*poly_degree) + ",TestParametersSinEllipt,double>"
+  const.cython_replacements = ["string", "string"]
+  const.include_files   = ["reproducibles_python/parameters/diffusion.hxx"]
+  const.debug_mode      = debug_mode
+
+  PyDP = HyperHDG.include(const)
+  HDG_wrapper = PyDP( os.path.dirname(os.path.abspath(__file__)) + \
+    "/../domains/square.geo")
+  HDG_wrapper.refine( 2 ** iteration )
+
+  vectorRHS = np.multiply( HDG_wrapper.residual_flux(HDG_wrapper.zero_vector()), -1. )
+
+  system_size = HDG_wrapper.size_of_system()
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
+
+  [vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, tol=1e-13)
+  if num_iter != 0:
+    print("CG solver failed with a total number of ", num_iter, "iterations.")
+    [vectorSolution, num_iter] = sp_lin_alg.gmres(A, vectorRHS, tol=1e-13)
+    if num_iter != 0:
+      print("GMRES also failed with a total number of ", num_iter, "iterations.")
+      raise RuntimeError("Linear solvers did not converge!")
+
+  error = HDG_wrapper.errors(vectorSolution)[0]
+  print("Iteration: ", iteration, " Error: ", error)
+  f = open("output/diffusion_convergence_elliptic.txt", "a")
+  f.write("Polynomial degree = " + str(poly_degree) + ". Dimension = " + str(dimension) \
+          + ". Iteration = " + str(iteration) + ". Error = " + str(error) + ".\n")
+  f.close()
+  
+  HDG_wrapper.plot_option( "fileName" , "diff_conv_ellip-" + str(dimension) + "-" + str(iteration) )
+  HDG_wrapper.plot_option( "printFileNumber" , "false" )
+  HDG_wrapper.plot_option( "scale" , "0.95" )
+  HDG_wrapper.plot_solution(vectorSolution)
+  
+  end_time = datetime.now()
+  print("Program ended at", end_time, "after", end_time-start_time)
+  
+
+# --------------------------------------------------------------------------------------------------
+# Function main.
+# --------------------------------------------------------------------------------------------------
+def main(debug_mode):
+  for poly_degree in range(1,4):
+    print("\n Polynomial degree is set to be ", poly_degree, "\n\n")
+    for dimension in range(1,3):
+      print("Dimension is ", dimension, "\n")
+      for iteration in range(6):
+        try:
+          diffusion_test(poly_degree, dimension, iteration, debug_mode)
+        except RuntimeError as error:
+          print("ERROR: ", error)
+
+
+# --------------------------------------------------------------------------------------------------
+# Define main function.
+# -------------------------------------------------------------------------------------------------- 
+if __name__ == "__main__":
+  main(len(sys.argv) > 1 and sys.argv[1] == "True")
