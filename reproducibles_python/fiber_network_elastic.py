9793ea24a3c294dd8135035e2c67e0bb65558dfc
AndreasRupp <andreas.rupp@fau.de>
2024-03-08 18:14:57 +0200

    Play with non-ready Python script.

--- reproducibles_python/fiber_network_elastic.py
+++ reproducibles_python/fiber_network_elastic.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
 
 import numpy as np
+import scipy.sparse as sp
 import scipy.sparse.linalg as sp_lin_alg
 from scipy.sparse.linalg import LinearOperator
+
+import matplotlib.pyplot as plt
 
 from datetime import datetime
 
@@ -29,36 +32,76 @@ except (ImportError, ModuleNotFoundError) as error:
   
 const                 = HyperHDG.config()
 const.global_loop     = "Elliptic"
-const.local_solver    = "LengtheningBernoulliBendingBeam<1,3,1,2>"
-const.topology        = "File<1,3>"
-const.geometry        = "File<1,3>"
-const.node_descriptor = "File<1,3>"
-const.cython_replacements = ["string", "string"]
-const.include_files   = [ "HyperHDG/local_solver/diffusion_uniform_ldgh.hxx", \
-                          "HyperHDG/local_solver/bilaplacian_uniform_ldgh.hxx" ]
+const.local_solver    = "LengtheningBeam<1,2,1,2>"
+# const.local_solver    = "Bilaplacian_cont_flux<1,4,8>"
+# const.local_solver    = "Bilaplacian<1,1,2>"
+# const.topology        = "File<1,3>"
+# const.geometry        = "File<1,3>"
+# const.node_descriptor = "File<1,3>"
+const.topology        = "Cubic<1,1>"
+const.geometry        = "UnitCube<1,1,double>"
+const.node_descriptor = "Cubic<1,1>"
+# const.cython_replacements = ["string", "string"]
+const.cython_replacements = ["vector[unsigned int]", "vector[unsigned int]"]
+const.include_files   = [ "HyperHDG/local_solver/diffusion_ldgh.hxx", \
+                          "HyperHDG/local_solver/bilaplacian_ldgh_cont_flux.hxx" ]
 const.debug_mode      = False
 
 PyDP = HyperHDG.include(const)
-HDG_wrapper = PyDP( os.path.dirname(os.path.abspath(__file__)) + \
-                    "/../domains/aggregate_" + aggregate + ".pts" )
+# HDG_wrapper = PyDP( os.path.dirname(os.path.abspath(__file__)) + \
+#                     "/../domains/aggregate_" + aggregate + ".pts" )
+# HDG_wrapper = PyDP( os.path.dirname(os.path.abspath(__file__)) + \
+#     "/../domains/fiber_network_1000.geo" )
+HDG_wrapper = PyDP( [4] * 1 )
 
 vectorDirichlet = HDG_wrapper.zero_vector()
-vectorDirichlet[0] = 1.
+# vectorDirichlet[0] = 1.
 
-index_vector = np.array([ 0, 1, 2, 3, \
-  len(vectorDirichlet)-4, len(vectorDirichlet)-3, len(vectorDirichlet)-2, len(vectorDirichlet)-1 ])
-HDG_wrapper.read_dirichlet_indices(index_vector)
+# index_vector = np.array([ 0, 4 ])
+# HDG_wrapper.read_dirichlet_indices(index_vector)
 
-vectorRHS = [-i for i in HDG_wrapper.trace_to_flux(vectorDirichlet)]
+vectorRHS = [-i for i in HDG_wrapper.residual_flux(vectorDirichlet)]
+# print(vectorRHS)
 
 system_size = HDG_wrapper.size_of_system()
-A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
+# A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
 
-[vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, maxiter=5500, tol=1e-9)
+col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat()
+A = sp.csr_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
+
+# B = A.todense()
+# B = B[2:-2,2:-2]
+# print(B)
+
+# col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat()
+# C = sp.csr_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
+
+# print(np.subtract(A.dot(np.ones(system_size)),C.dot(np.ones(system_size))))
+
+vectorSolution, num_iter = sp_lin_alg.cg(A, vectorRHS, tol=1e-9)
 if num_iter != 0:
   raise RuntimeError("Linear solver did not converge!")
 
-print("The linear solver (conjugate gradients) worked!")
+# print("The linear solver (conjugate gradients) worked!")
+
+
+
+print(vectorSolution)
+
+# vectorSolution = np.array([0,0,.5,1,0,0])
+# print(vectorSolution)
+print(A.dot(vectorSolution))
+print(vectorRHS)
+print(HDG_wrapper.residual_flux(vectorSolution))
+
+
+
+# print(vectorRHS)
+
+error = HDG_wrapper.errors(vectorSolution)[0]
+print(" Error: ", error)
+
+# print(vectorSolution)
 
 HDG_wrapper.plot_option("fileName", "aggregate_" + aggregate)
 HDG_wrapper.plot_option("printFileNumber", "false" )
