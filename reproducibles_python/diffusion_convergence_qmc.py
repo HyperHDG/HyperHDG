aa3dd0f67cd631efd3975be18e151f7db6d42257
gutendorf <rubgut3008@gmail.com>
2025-05-08 11:14:30 +0200

    bug fix

--- reproducibles_python/diffusion_convergence_qmc.py
+++ reproducibles_python/diffusion_convergence_qmc.py
@@ -34,29 +34,27 @@ PyDP = HyperHDG.include(const)
 HDG_wrapper = PyDP([100, 100])
 
 system_size = HDG_wrapper.size_of_system()
-mean = np.zeros(system_size)
-mean_sa = np.zeros(system_size)
 
 with open("output/kvgr.txt", "w") as f:
 	f.write("n_qmc_points\tsq_err\n")
 
 def solve(k, gen_vec, shift, n_qmc_points, HDG_wrapper):
-	print("Punkt " + str(k+1) + " von " + str(n_qmc_points))
+	print("Quadrature point " + str(k+1) + " of " + str(n_qmc_points))
 	system_size = HDG_wrapper.size_of_system()
 	arr = HyperHDG.qmc_methods.get_quadrature_point(gen_vec, shift, k+1, n_qmc_points) - 0.5
 	vectorRHS = np.multiply( HDG_wrapper.residual_flux(np.zeros(system_size), arr), -1. )
 	col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat(arr)
 	A = sp.csr_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
 	[vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, tol=1e-11)
-	return HDG_wrapper.errors(vectorSolution)
+	return HDG_wrapper.errors(vectorSolution)[0]
 
 for i in range(2, 4):
 	n_qmc_points = 2**i
-	print("Quadraturpunkte: " + str(n_qmc_points))
+	print("Quadrature points: " + str(n_qmc_points))
 	mean_sa = 0
 	for m in range(n_shifts):
-		mean_sa = 0
-		print("Shift " + str(m+1) + " von " + str(n_shifts))
+		mean = 0
+		print("Shift " + str(m+1) + " of " + str(n_shifts))
 		shift = zfgen.random(100)
 		errs = Parallel(n_jobs=-2, prefer="threads")( delayed(solve) (k, gen_vec, shift, n_qmc_points, HDG_wrapper) for k in range(n_qmc_points))
 		mean = np.mean(np.array(errs))
