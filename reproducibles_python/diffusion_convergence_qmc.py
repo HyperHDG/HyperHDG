2a605e0063f9f44cd0606a4f85780e80f40402b0
gutendorf <rubgut3008@gmail.com>
2025-05-15 17:35:12 +0200

    lognormal case

--- reproducibles_python/diffusion_convergence_qmc.py
+++ reproducibles_python/diffusion_convergence_qmc.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as sp_lin_alg
+from scipy.stats import norm as sp_norm
 from joblib import Parallel, delayed
 
 import os, sys
@@ -16,25 +17,26 @@ pol_g = 1
 n_shifts = 16
 gen_vec = HyperHDG.qmc_methods.generating_vector("lattice-32001-1024-1048576.3600", 100)
 
+zfgen = np.random.default_rng(27)
 
-zfgen = np.random.default_rng(13)
+def phi_affine(arr):
+	return arr - 0.5
+
+def phi_lognormal(arr):
+	return sp_norm.ppf(arr)
 
 const = HyperHDG.config()
 const.global_loop = "Elliptic"
 const.topology = "Cubic<2, 2>"
 const.geometry = "UnitCube<2, 2, double>"
 const.node_descriptor = "Cubic<2, 2>"
-const.local_solver = "Diffusion<2, " + str(pol_g) + "," + str(2*pol_g) + ", TestParametersDiffusionAffine, double, std::vector<double> >"
 const.param_t = "std::vector<double>"
 const.cython_replacements = ["vector[unsigned int]", "vector[unsigned int]", "double", "double", "vector[double]"]
 const.include_files   = ["reproducibles_python/parameters/diffusion_qmc.hxx"]
 const.debug_mode      = False
 
 
-with open("output/kvgr.txt", "w") as f:
-	f.write("n_qmc_points\terr_u\terr_q\terr_g\n")
-
-def solve(k, gen_vec, shift, n_qmc_points):
+def solve(k, gen_vec, shift, n_qmc_points, morph):
 	PyDP = HyperHDG.include(const)
 	HDG_wrapper = PyDP([100, 100])
 
@@ -42,32 +44,57 @@ def solve(k, gen_vec, shift, n_qmc_points):
 	
 	print("Quadrature point " + str(k+1) + " of " + str(n_qmc_points))
 	system_size = HDG_wrapper.size_of_system()
-	arr = HyperHDG.qmc_methods.get_quadrature_point(gen_vec, shift, k+1, n_qmc_points) - 0.5
+	arr = morph(HyperHDG.qmc_methods.get_quadrature_point(gen_vec, shift, k+1, n_qmc_points))
 	vectorRHS = np.multiply( HDG_wrapper.residual_flux(np.zeros(system_size), arr), -1. )
 	col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat(arr)
 	A = sp.csr_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
 	[vectorSolution, num_iter] = sp_lin_alg.cg(A, vectorRHS, rtol=1e-11)
-	return HDG_wrapper.mean(vectorSolution, arr)
+	return [vectorSolution, HDG_wrapper.mean(vectorSolution, arr)]
 
-for i in range(2, 5):
-	n_qmc_points = 2**i
-	print("Quadrature points: " + str(n_qmc_points))
+def get_err(gen_vec, n_qmc_points, morph):
 	mean_sa = 0
-	expec = np.zeros((n_shifts, 3))
+	expec = np.zeros((n_shifts, 4))
 	for m in range(n_shifts):
 		print("Shift " + str(m+1) + " of " + str(n_shifts))
 		shift = zfgen.random(100)
-		means = np.array(Parallel(n_jobs=32)( delayed(solve) (k, gen_vec, shift, n_qmc_points) for k in range(n_qmc_points)))
+		asg = Parallel(n_jobs=32)( delayed(solve) (k, gen_vec, shift, n_qmc_points, phi_affine) for k in range(n_qmc_points))
+		means = np.array(asg[:, 1])
 		print(means)
 		expec[m, 0] = np.mean(means[:, -1])
 		expec[m, 1] = np.mean(np.linalg.norm(means[:, :2], axis=1))
 		expec[m, 2] = np.mean(np.linalg.norm(means[:, 2:-1], axis=1))
+		scelet_v = np.array(asg[:, 0])
+		expec[m, 3] = np.mean(np.linalg.norm(scelet_v, axis=1))
 	print(expec)
 	mean_sa = np.mean(expec, axis=0)
 	print(mean_sa)
 	print(expec - mean_sa)
 	err = np.linalg.norm(mean_sa - expec, axis=0) / np.sqrt(n_shifts - 1) / np.sqrt(n_shifts)
 	print(err)
-	with open("output/kvgr.txt", "a") as f:
-		f.write(str(n_qmc_points) + "\t" + str(err[0]) + "\t" + str(err[1]) + "\t" + str(err[2]) + "\n")
+	return err
 
+
+#affine case
+
+const.local_solver = "Diffusion<2, " + str(pol_g) + "," + str(2*pol_g) + ", TestParametersDiffusionAffine, double, std::vector<double> >"
+
+with open("output/kvgr_affine.txt", "w") as f:
+	f.write("n_qmc_points\terr_u\terr_q\terr_g\terr_s\n")
+
+for i in range(2, 5):
+	get_err(gen_vec, 2**i, phi_affine)
+	with open("output/kvgr_affine.txt", "a") as f:
+		f.write(str(n_qmc_points) + "\t" + str(err[0]) + "\t" + str(err[1]) + "\t" + str(err[2]) + "\t" + str(err[3]) + "\n")
+
+
+#lognormal case
+
+const.local_solver = "Diffusion<2, " + str(pol_g) + "," + str(2*pol_g) + ", TestParametersDiffusionLognormal, double, std::vector<double> >"
+
+with open("output/kvgr_lognormal.txt", "w") as f:
+	f.write("n_qmc_points\terr_u\terr_q\terr_g\terr_s\n")
+
+for i in range(2, 5):
+	get_err(gen_vec, 2**i, phi_lognormal)
+	with open("output/kvgr_lognormal.txt", "a") as f:
+		f.write(str(n_qmc_points) + "\t" + str(err[0]) + "\t" + str(err[1]) + "\t" + str(err[2]) + "\t" + str(err[3]) + "\n")
--- reproducibles_python/parameters/diffusion_qmc.hxx
+++ reproducibles_python/parameters/diffusion_qmc.hxx
@@ -71,3 +71,68 @@ private:
     }
   }
 };
+
+template <unsigned int space_dimT, typename param_float_t = double>
+struct TestParametersDiffusionLognormal
+{
+ typedef std::vector<param_float_t> param_time_t;
+  static constexpr std::array<unsigned int, 26U> dirichlet_nodes{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
+  static constexpr std::array<unsigned int, 0U> neumann_nodes{};
+  static param_float_t diffusion_coeff(const Point<space_dimT, param_float_t>& point,
+                                       const param_time_t arr)
+  {
+    return 1. / inverse_diffusion_coeff(point, arr);
+  }
+  static param_float_t inverse_diffusion_coeff(const Point<space_dimT, param_float_t>& point,
+                                               const param_time_t arr)
+  {
+    param_float_t r = 0.;
+    unsigned int kl[] = {1, 1};
+    std::for_each(arr.begin(), arr.end(), [&r, &kl, point](const param_float_t& n) {
+      param_float_t w = pow(kl[0] * kl[0] + kl[1] * kl[1], -1.3);
+      w *= sin(kl[0] * M_PI * point[0]) * sin(kl[1] * M_PI * point[1]);
+      r += n * w;
+      //std::cout << kl[0] << " " << kl[1] << " " << kl[0] * kl[0] + kl[1] * kl[1] << " " << n << std::endl;
+      akt(kl);
+    });
+    return exp(r); 
+  }
+
+  static param_float_t right_hand_side(const Point<space_dimT, param_float_t>& point,
+                                       const param_time_t = 0)
+  {
+    return point[0];
+  }
+
+  static param_float_t dirichlet_value(const Point<space_dimT, param_float_t>& point,
+                                       const param_time_t = 0)
+  {
+    return 0.;
+  }
+  static param_float_t initial(const Point<space_dimT, param_float_t>& point,
+                               const param_time_t = 0)
+  {
+    return 0.;
+  }
+  static param_float_t neumann_value(const Point<space_dimT, param_float_t>&,
+                                     const param_time_t = 0)
+  {
+    return 0.;
+  }
+  static param_float_t analytic_result(const Point<space_dimT, param_float_t>& p,
+                                       const param_time_t = 0)
+  {
+    return 0.;
+  }
+private:
+  static void akt(unsigned int kl[2]) {
+    if(kl[0] == 1) {
+      kl[0] = kl[1] + 1;
+      kl[1] = 1;
+    } else {
+      kl[0] -= 1;
+      kl[1] += 1;
+    }
+  }
+};
