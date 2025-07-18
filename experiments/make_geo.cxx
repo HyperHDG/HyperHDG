4db6a444c523c5cd2395110a68dc083ef0b40e34
Joseph Holten <joseph@holten.com>
2025-07-18 19:22:32 +0200

    save

--- experiments/make_geo.cxx
+++ experiments/make_geo.cxx
@@ -27,6 +27,11 @@ struct Connection {
   Real a2;
 };
 
+struct ConnectionPoint {
+  ID nodeid;
+  Real a;
+};
+
 using Point = std::array<Real, 3>;
 using Edge = std::pair<ID, ID>;
 using Prop = std::array<Real, 12>;
@@ -212,7 +217,7 @@ int main(int argc, char** argv) {
   pcloud.pts.reserve(connections.size()*2 + nodes.size());
 
   // mapping of fiber to nodeids of connection points
-  std::vector<std::vector<u64>> fiber_segments(fibers.size());
+  std::vector<std::vector<ConnectionPoint>> fiber_segments(fibers.size());
 
   // first all connection points
   for (const Connection& con : connections) {
@@ -233,9 +238,13 @@ int main(int argc, char** argv) {
     u64 p2id = pcloud.pts.size();
     pcloud.pts.push_back(p2);
 
-    fiber_segments[con.f1].push_back(p1id);
-    fiber_segments[con.f2].push_back(p2id);
+    fiber_segments[con.f1].push_back({p1id, con.a1});
+    fiber_segments[con.f2].push_back({p2id, con.a2});
   }
+
+  // sort all fiber segments by their a
+  for (auto& seg : fiber_segments)
+    std::sort(seg.begin(), seg.end(), [](const ConnectionPoint& p1, const ConnectionPoint& p2){ return p1.a < p2.a; } );
 
   // next all fiber endpoints
   for (const Point& p : nodes)
@@ -257,32 +266,30 @@ int main(int argc, char** argv) {
   // init to 0 to not skip first (and thus all) node(s)
   std::vector<u64> merge_map(pcloud.pts.size(), (u64)-1);
 
+  // TODO: do this slightly differntly
+  //   use nearest neighbor and then add them directly to the new list...
+
   u64 nn_avg = 0, nn_max = 0;
   Real r = 1e-10;
   using Neighbor = nanoflann::ResultItem<uint32_t, Real>; // Neighbor = (id,distance)
   std::vector<Neighbor> neighbors;
   neighbors.reserve(1000); // reserve more than enough
-  for (u64 nodeid = 0; nodeid < pcloud.pts.size(); nodeid++) {
+  for (u64 nodeid = 0; nodeid < 2*connections.size(); nodeid++) {
     // else find close neighbors
     const double* p = pcloud.pts[nodeid].data();
     const u64 num_neighbors = kdtree.radiusSearch(p, r, neighbors);
 
-    // if there were none, continue
-    if (num_neighbors == 0)
-      continue;
-
     // else map myself to neighbor with lowest idx
-    u64 newid = nodeid;
+    u64 newid = (u64)-1;
     for (const Neighbor& neighbor : neighbors)
       newid = std::min(newid, (u64)neighbor.first);
-    merge_map[nodeid] = newid;
+    merge_map[nodeid] = std::min(nodeid,newid);
 
     // count avg, max number of neighbors
     nn_avg += num_neighbors;
     nn_max = std::max(nn_max, num_neighbors);
   }
 
-#if 0
   // for the node endpoints use much smaller r
   r = 1e-15;
   for (u64 nodeid = 2*connections.size(); nodeid < pcloud.pts.size(); nodeid++) {
@@ -290,21 +297,19 @@ int main(int argc, char** argv) {
     const double* p = pcloud.pts[nodeid].data();
     const u64 num_neighbors = kdtree.radiusSearch(p, r, neighbors);
 
-    // if there were none, continue
-    if (num_neighbors == 0)
-      continue;
-
     // else map myself to neighbor with lowest idx
-    u64 newid = nodeid;
+    u64 newid = (u64)-1;
     for (const Neighbor& neighbor : neighbors)
       newid = std::min(newid, (u64)neighbor.first);
-    merge_map[nodeid] = newid;
+    merge_map[nodeid] = std::min(nodeid,newid);
 
     // count avg, max number of neighbors
     nn_avg += num_neighbors;
     nn_max = std::max(nn_max, num_neighbors);
   }
-#endif
+
+  for (u64 i = 0; i < pcloud.pts.size(); i++)
+    assert(merge_map[i] != (u64)-1);
 
   logi("num neighbors: avg = {:.3f}, max = {}",
        (double)nn_avg / pcloud.pts.size(), nn_max);
@@ -312,21 +317,19 @@ int main(int argc, char** argv) {
   // create mapping from old node ids to consecutive new ids after merging
   u64 new_count = 0;
   std::vector<u64> new_ids(pcloud.pts.size(), 0);
+  std::vector<Point> vertices(new_count);
   for (u64 old_id = 0; old_id < pcloud.pts.size(); old_id++) {
     // node is kept
     if (merge_map[old_id] == old_id) {
       new_ids[old_id] = new_count;
+      vertices.push_back(pcloud.pts[old_id]);
       new_count++;
     } else {
       u64 merge_partner_old = merge_map[old_id];
+      assert(merge_partner_old < old_id);
       new_ids[old_id] = new_ids[merge_partner_old];
     }
   }
-
-  // create new node list
-  std::vector<Point> vertices(new_count);
-  for (u64 old_id = 0; old_id < pcloud.pts.size(); old_id++)
-    vertices[new_ids[old_id]] = pcloud.pts[old_id];
 
   logi("create edge list");
 
@@ -335,41 +338,46 @@ int main(int argc, char** argv) {
 
   edges.reserve(connections.size()*2+fibers.size());
 
-  // for every connection with id two nodes are created with 2*id, 2*id+1 which form the connection
-  for (u64 cid = 0; cid < connections.size(); cid++) {
-    u64 nid1 = new_ids[2*cid+0];
-    u64 nid2 = new_ids[2*cid+1];
-    // ignore self loops (TODO: multiedges?)
-    if (nid1 != nid2) {
-      edges.push_back({nid1, nid2});
-      edge_props.push_back(connection_props[cid]);
+  u64 selfloops = 0;
+
+  // TODO: test for multi-edges?
+  auto add_edge_if_simple = [&](u64 u, u64 v, const Prop& p){
+    u64 nu = new_ids[u];
+    u64 nv = new_ids[v];
+    if (nu != nv) {
+      edges.push_back({nu,nv});
+      edge_props.push_back(p);
+    } else {
+      selfloops++;
     }
-  }
+  };
+
+  // for every connection with id two nodes are created with 2*id, 2*id+1 which form the connection
+  for (u64 cid = 0; cid < connections.size(); cid++)
+    add_edge_if_simple(2*cid, 2*cid+1, connection_props[cid]);
 
   // add segmentation of fiber as edges
   for (u64 fid = 0; fid < fibers.size(); fid++) {
     Edge fiber = fibers[fid];
-    const std::vector<u64>& cpoints = fiber_segments[fid];
-
-    // TODO: test for multi-edges?
-    auto add_edge_if_simple = [&](u64 u, u64 v){
-      u64 nu = new_ids[u];
-      u64 nv = new_ids[v];
-      if (nu != nv) {
-        edges.push_back({nu,nv});
-        edge_props.push_back(fiber_props[fid]);
-      }
-    };
+    // need this offset
+    u64 start = fiber.first + connections.size()*2;
+    u64 end = fiber.second + connections.size()*2;
+    const std::vector<ConnectionPoint>& cpoints = fiber_segments[fid];
+    const Prop& prop = fiber_props[fid];
 
     if (cpoints.empty()) {
-      add_edge_if_simple(fiber.first,fiber.second);
+      add_edge_if_simple(start,end,prop);
     } else {
-      add_edge_if_simple(fiber.first, cpoints[0]);
+      add_edge_if_simple(start, cpoints[0].nodeid, prop);
       for (u64 i = 1; i < cpoints.size(); i++)
-        add_edge_if_simple(cpoints[i-1], cpoints[i]);
-      add_edge_if_simple(cpoints.back(), fiber.second);
+        add_edge_if_simple(cpoints[i-1].nodeid, cpoints[i].nodeid, prop);
+      add_edge_if_simple(cpoints.back().nodeid, fiber.second, prop);
     }
   }
+
+  logi("selfloops={}", selfloops);
+
+  assert(edge_props.size() == edges.size());
 
   logi("generate output");
 
