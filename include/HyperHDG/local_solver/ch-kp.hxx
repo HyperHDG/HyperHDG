16a21d8c0edecdbc849ac5d200589a51d3ff0493
gutendorf <rubgut3008@gmail.com>
2025-08-07 17:23:14 +0200

    first variational equation

---
+++ include/HyperHDG/local_solver/ch-kp.hxx
@@ -0,0 +1,332 @@
+#pragma once  // Ensure that file is included only once in a single compilation.
+
+#include <HyperHDG/dense_la.hxx>
+#include <HyperHDG/hypercube.hxx>
+#include <tpp/quadrature/tensorial.hxx>
+#include <tpp/shape_function/shape_function.hxx>
+
+#include <algorithm>
+#include <tuple>
+#include <iostream>
+
+namespace LocalSolver
+{
+/*!*************************************************************************************************
+ * \brief   Default parameters for the CH-KP equation.
+ *
+ * \authors   Ruben Gutendorf, Saarland University, 2025.
+ **************************************************************************************************/
+template <unsigned int space_dimT, typename param_float_t = double>
+struct ChkpParametersDefault
+{
+  /*!***********************************************************************************************
+   * \brief   Array containing hypernode types corresponding to Dirichlet boundary.
+   ************************************************************************************************/
+  static constexpr std::array<unsigned int, 0U> dirichlet_nodes{};
+  /*!***********************************************************************************************
+   * \brief   Array containing hypernode types corresponding to Neumann boundary.
+   ************************************************************************************************/
+  static constexpr std::array<unsigned int, 0U> neumann_nodes{};
+  /*!***********************************************************************************************
+   * \brief   Inverse diffusion coefficient in PDE as analytic function.
+   ************************************************************************************************/
+  static param_float_t inverse_diffusion_coeff(const Point<space_dimT, param_float_t>&,
+                                               const param_float_t = 0.)
+  {
+    return 1.;
+  }
+  /*!***********************************************************************************************
+   * \brief   Dirichlet values of solution as analytic function.
+   ************************************************************************************************/
+  static param_float_t dirichlet_value(const Point<space_dimT, param_float_t>&,
+                                       const param_float_t = 0.)
+  {
+    return 0.;
+  }
+  /*!***********************************************************************************************
+   * \brief   Neumann values of solution as analytic function.
+   ************************************************************************************************/
+  static param_float_t neumann_value(const Point<space_dimT, param_float_t>&,
+                                     const param_float_t = 0.)
+  {
+    return 0.;
+  }
+  /*!***********************************************************************************************
+   * \brief   Analytic result of PDE (for convergence tests).
+   ************************************************************************************************/
+  static param_float_t analytic_result(const Point<space_dimT, param_float_t>&,
+                                       const param_float_t = 0.)
+  {
+    return 0.;
+  }
+  static constexpr param_float_t kappa=-1.;
+};  
+
+/*!*************************************************************************************************
+ * \brief   Local solver for parabolic diffusion equation on hypergraph.
+ *
+ * \note    Theta must be one, since only implicit Euler has been implemented at the moment.
+ *
+ * This class contains the local solver for an isotropic diffusion equation, i.e.,
+ * \f[
+ *  \partial_t u - \nabla \cdot ( d \nabla u ) = f \quad \text{ in } \Omega, \qquad
+ *  u = u_\text D \quad \text{ on } \partial \Omega_\text D, \qquad
+ *  - d \nabla u \cdot \nu = g_\text N \quad \text{ on } \partial \Omega_\text N
+ * \f]
+ * in a spatial domain \f$\Omega \subset \mathbb R^d\f$. Here, \f$d\f$ is the spatial dimension
+ * \c space_dim, \f$\Omega\f$ is a regular graph (\c hyEdge_dimT = 1) or hypergraph whose
+ * hyperedges are surfaces (\c hyEdge_dimT = 2) or volumes (\c hyEdge_dimT = 3) or hypervolumes (in
+ * case of \c hyEdge_dimT > 3). \f$f\f$ and \f$d\f$ are scalars defined in the whole domain, the
+ * Dirichlet and Neumann boundary data needs to be defined on their respective hypernodes.
+ *
+ * \tparam  hyEdge_dimT   Dimension of a hyperedge, i.e., 1 is for PDEs defined on graphs, 2 is for
+ *                        PDEs defined on surfaces, and 3 is for PDEs defined on volumes.
+ * \tparam  poly_deg      The polynomial degree of test and trial functions.
+ * \tparam  quad_deg      The order of the quadrature rule.
+ * \tparam  parametersT   Struct depending on templates \c space_dimTP and \c lSol_float_TP that
+ *                        contains static parameter functions.
+ *                        Defaults to above functions included in \c DiffusionParametersDefault.
+ * \tparam  lSol_float_t  The floating point type calculations are executed in. Defaults to double.
+ *
+ * \authors   Guido Kanschat, Heidelberg University, 2019--2020.
+ * \authors   Andreas Rupp, Heidelberg University, 2019--2020.
+ **************************************************************************************************/
+template <unsigned int hyEdge_dimT,
+          unsigned int poly_deg,
+          unsigned int quad_deg,
+          template <unsigned int, typename> typename parametersT = ChkpParametersDefault,
+          typename lSol_float_t = double>
+class Chkp
+{
+ public:
+  // -----------------------------------------------------------------------------------------------
+  // Public, static constexpr functions
+  // -----------------------------------------------------------------------------------------------
+
+  /*!***********************************************************************************************
+   * \brief   Dimension of hyper edge type that this object solves on.
+   ************************************************************************************************/
+  static constexpr unsigned int hyEdge_dim() { return hyEdge_dimT; }
+  /*!***********************************************************************************************
+   * \brief   Evaluate amount of global degrees of freedom per hypernode.
+   *
+   * This number must be equal to HyperNodeFactory::n_dofs_per_node() of the HyperNodeFactory
+   * cooperating with this object.
+   *
+   * \retval  n_dofs        Number of global degrees of freedom per hypernode.
+   ************************************************************************************************/
+  static constexpr unsigned int n_glob_dofs_per_node()
+  {
+    return 2 * Hypercube<hyEdge_dimT - 1>::pow(poly_deg + 1);
+  }
+ private:
+  // -----------------------------------------------------------------------------------------------
+  // Private, static constexpr functions
+  // -----------------------------------------------------------------------------------------------
+
+  /*!***********************************************************************************************
+   * \brief   Number of local shape functions (with respect to all spatial dimensions).
+   ************************************************************************************************/
+  static constexpr unsigned int n_shape_fct_ = n_glob_dofs_per_node() * (poly_deg + 1) / 2;
+  /*!***********************************************************************************************
+   * \brief   Number of local  shape functions (with respect to a face / hypernode).
+   ************************************************************************************************/
+  static constexpr unsigned int n_shape_bdr_ = n_glob_dofs_per_node() / 2;
+  /*!***********************************************************************************************
+   * \brief   Number of (local) degrees of freedom per hyperedge.
+   ************************************************************************************************/
+  static constexpr unsigned int n_loc_dofs_ = 7 * n_shape_fct_;
+  /*!***********************************************************************************************
+   * \brief   Find out whether a node is of Dirichlet type.
+   ************************************************************************************************/
+  template <typename parameters>
+  static constexpr bool is_dirichlet(const unsigned int node_type)
+  {
+    return std::find(parameters::dirichlet_nodes.begin(), parameters::dirichlet_nodes.end(),
+                     node_type) != parameters::dirichlet_nodes.end();
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Private, const members: Parameters and auxiliaries that help assembling matrices, etc.
+  // -----------------------------------------------------------------------------------------------
+
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_ppu_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_mpu_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_mpv_;
+
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_pzu_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_mzu_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_mzv_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_pvu_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_f_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_uqq_;
+  /*!***********************************************************************************************
+   * \brief   (Globally constant) penalty parameter for HDG scheme.
+   ************************************************************************************************/
+  const lSol_float_t tau_yvu_;
+//TODO: Add support for non-constant tau_f. This requires new integration routines.
+  /*!***********************************************************************************************
+   * \brief   Parameter theta that defines the one-step theta scheme.
+   ************************************************************************************************/
+  const lSol_float_t theta_ = 1.;
+  /*!***********************************************************************************************
+   * \brief   Time step size.
+   ************************************************************************************************/
+  const lSol_float_t delta_t_;
+  /*!***********************************************************************************************
+   * \brief   An integrator helps to easily evaluate integrals (e.g. via quadrature).
+   ************************************************************************************************/
+  typedef TPP::Quadrature::Tensorial<
+    TPP::Quadrature::GaussLegendre<quad_deg>,
+    TPP::ShapeFunction<TPP::ShapeType::Tensorial<TPP::ShapeType::Legendre<poly_deg>, hyEdge_dimT> >,
+    lSol_float_t>
+    integrator;
+    
+  public:
+  // -----------------------------------------------------------------------------------------------
+  // Public functions (and one typedef) to be utilized by external functions.
+  // -----------------------------------------------------------------------------------------------
+
+  /*!***********************************************************************************************
+   *  \brief  Define type of (hyperedge related) data that is stored in HyDataContainer.
+   ************************************************************************************************/
+  struct data_type
+  {
+    SmallVec<n_shape_fct_, lSol_float_t> u_old;
+    SmallMat<n_shape_bdr_, 2 * hyEdge_dimT, lSol_float_t> ub_old, uh_old;
+  };
+  /*!***********************************************************************************************
+   *  \brief  Define type of node elements, especially with respect to nodal shape functions.
+   ************************************************************************************************/
+  struct node_element
+  {
+    typedef std::tuple<TPP::ShapeFunction<
+      TPP::ShapeType::Tensorial<TPP::ShapeType::Legendre<poly_deg>, hyEdge_dimT - 1> > >
+      functions;
+  };
+   /*!***********************************************************************************************
+   * \brief   Class is constructed using a single double indicating the penalty parameter.
+   ************************************************************************************************/
+  typedef std::vector<lSol_float_t> constructor_value_type;
+  /*!***********************************************************************************************
+   * \brief   Constructor for local solver.
+   *
+   * \param   constru       Constructor object.
+   ************************************************************************************************/
+  Chkp(const constructor_value_type& constru = std::vector(11, 1.))
+  : delta_t_(constru[0]), tau_ppu_(constru[1]), tau_mpu_(constru[2]), tau_mpv_(constru[3]), 
+  tau_pzu_(constru[4]), tau_mzu_(constru[5]), tau_mzv_(constru[6]), tau_pvu_(constru[7]), 
+  tau_f_(constru[8]), tau_uqq_(constru[9]), tau_yvu_(constru[10])
+  {
+  }
+  /*!***********************************************************************************************
+   * \brief   Solve local problem (with right-hand side from skeletal).
+   *
+   * \tparam  hyEdgeT       The geometry type / typename of the considered hyEdge's geometry.
+   * \tparam  SmallMatT     The data type of the \c lambda_values.
+   * \param   lambda_values Encodes uh, qh.
+   * \param   coeff         Coefficients of u, q, p, s, v, z, r to be tested.
+   * \param   hyper_edge    The geometry of the considered hyperedge (of typename GeomT).
+   * \param   time          Point of time the problem is solved.
+   * \retval  residual      Residual (should be zero).
+   ************************************************************************************************/
+  template <typename hyEdgeT, typename SmallMatT>
+  inline std::array<lSol_float_t, n_loc_dofs_> get_residual(const SmallMatT& lambda_values,
+                                                            const std::array<SmallVec<n_shape_fct_, lSol_float_t>, 7> coeff,
+                                                            hyEdgeT& hyper_edge,
+                                                            const lSol_float_t time) const
+  {
+    static_assert(std::is_same<typename SmallMatT::value_type::value_type, lSol_float_t>::value,
+        "Lambda values ...");
+    hy_assert(lambda_values.size() == 2 * hyEdge_dimT,
+        "The size ...");
+    for (unsigned int i = 0; i < hyEdge_dimT; ++i)
+      hy_assert(lambda_values[i].size() == 2 * n_shape_bdr_,
+          "The size of ...");
+
+    std::array<lSol_float_t, n_loc_dofs_> residual;
+    using parameters = parametersT<decltype(hyEdgeT::geometry)::space_dim(), lSol_float_t>;
+
+    //rearrange lambda values
+    std::array<SmallVec<n_shape_bdr_, lSol_float_t>, 2 * hyEdge_dim()> u_hat, q_hat;
+    for (unsigned int bdr = 0; bdr < 2 * hyEdge_dim(); ++bdr) 
+    {
+      for (unsigned int i = 0; i < n_shape_bdr_; i++)
+      {
+        u_hat[bdr][i] = lambda_values[bdr][i];
+        q_hat[bdr][i] = lambda_values[bdr][n_shape_bdr_ + i];
+      }
+    }
+
+    //calculate frequently used coefficients
+    SmallSquareMat<n_shape_fct_, lSol_float_t> mass, mdx, mdy;
+    std::array<SmallMat<n_shape_fct_, n_shape_bdr_, lSol_float_t>, 2 * hyEdge_dim()> bdr_int;
+    std::array<SmallVec<hyEdge_dim(), lSol_float_t>, 2 * hyEdge_dim()> loc_normal;
+    for (unsigned int i = 0; i < n_shape_fct_; ++i) 
+    {
+      for (unsigned int j = 0; j < n_shape_fct_; ++j) 
+      {
+        mass(i, j) = integrator::template integrate_vol_phiphi<decltype(hyEdgeT::geometry)>(
+            i, j, hyper_edge.geometry);
+        mdx(i, j) = integrator::template integrate_vol_phiDphi<decltype(hyEdgeT::geometry)>(
+            j, i, 0, hyper_edge.geometry);
+        mdy(i, j) = integrator::template integrate_vol_phiDphi<decltype(hyEdgeT::geometry)>(
+            j, i, 1, hyper_edge.geometry);
+        
+      }
+    }
+    for (unsigned int bdr = 0; bdr < 2 * hyEdge_dim(); ++bdr) 
+    {
+      loc_normal[bdr] = hyper_edge.geometry.local_normal(bdr);
+      for (unsigned int i = 0; i < n_shape_fct_; ++i)
+      {
+        for (unsigned int j = 0; j < n_shape_bdr_; j++) 
+        {
+          bdr_int[bdr].operator()(i, j) = integrator::template integrate_bdr_phipsi<decltype(hyEdgeT::geometry)>(
+            i, j, bdr, hyper_edge.geometry);
+        }
+      }
+    }
+    std::cout << bdr_int[0] << std::endl;
+    std::cout << u_hat[0] << std::endl;
+    SmallVec<n_shape_fct_, lSol_float_t> helper;
+    //first eq.
+    helper = mass * coeff[1] + mdx * coeff[0];
+    for (unsigned int bdr = 0; bdr < 2 * hyEdge_dim(); ++bdr) 
+      helper += (bdr_int[bdr] * u_hat[bdr]) * loc_normal[bdr][0];
+    std::cout << helper << std::endl;
+    for (unsigned int i = 0; i < n_shape_fct_; ++i) 
+    {
+      residual[i] += helper[i];
+    }
+    return residual;
+  }
+};
+}
---
+++ tests_c++/chkp_dev.cxx
@@ -0,0 +1,44 @@
+#include <HyperHDG/local_solver/ch-kp.hxx>
+#include <HyperHDG/hdg_hypergraph.hxx>
+#include <HyperHDG/hy_assert.hxx>
+#include <HyperHDG/topology/cubic.hxx>
+#include <HyperHDG/geometry/unit_cube.hxx>
+#include <HyperHDG/node_descriptor/cubic.hxx>
+#include <HyperHDG/dense_la.hxx>
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+
+int main() {
+  typedef LocalSolver::Chkp<2, 1, 3> lst;
+  const lst ls;
+  SmallVec<2, unsigned int> top_con(1U);
+  HDGHyperGraph<lst::n_glob_dofs_per_node(),
+                Topology::Cubic<2, 2>,
+                Geometry::UnitCube<2, 2>,
+                NodeDescriptor::Cubic<2, 2>,
+                lst::data_type>
+    hg(top_con);
+  std::array<std::array<double, 4>, 4> lambda_n;
+  std::vector<double> xv;
+  for(unsigned int i = 0; i < hg.n_global_dofs(); i++)
+    xv.push_back((double) i);
+  std::array<SmallVec<4>, 7> coeff;
+  coeff.fill(SmallVec<4>(0.));
+  SmallVec<4, unsigned int> hyEdge_hyNodes;
+  std::for_each(hg.begin(), hg.end(), [&](auto he)
+      {
+        hyEdge_hyNodes = he.topology.get_hyNode_indices();
+        for (unsigned int n = 0; n < 4; ++n)
+        {
+          hg.hyNode_factory().get_dof_values(hyEdge_hyNodes[n], xv, lambda_n[n]);
+          std::for_each(lambda_n[n].begin(), lambda_n[n].end(), [](auto i){std::cout << i <<"\t";});
+          std::cout << "\n";
+        }
+        std::cout << hyEdge_hyNodes << "\n";
+        std::array<double, 28> res = ls.get_residual(lambda_n, coeff, he, 0.);
+        std::cout << res[0] << " " << res[1] << " " << res[2] << " " << res[3] << std::endl;
+      });
+  return 0;
+}
