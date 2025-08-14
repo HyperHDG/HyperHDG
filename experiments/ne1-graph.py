96f8a41375f3a3693d0a2568a990cab4efaeee99
Joseph Holten <joseph@holten.com>
2025-08-14 17:50:38 +0200

    better graphs

--- experiments/ne1-graph.py
+++ experiments/ne1-graph.py
@@ -5,10 +5,13 @@ import json
 import argparse
 from pprint import pprint
 import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-x", help="name of x variable")
-parser.add_argument("-y", help="name of y variable")
+parser.add_argument("-y", help="name of y variable(s)")
+parser.add_argument("--group-by", help="how to group input data to display")
 parser.add_argument("--xlabel", help="label of x-axis, default=name of x variable")
 parser.add_argument("--ylabel", help="label of y-axis, default=name of y variable")
 parser.add_argument("--save", help="save plot instead of showing it")
@@ -30,13 +33,22 @@ for line in sys.stdin:
 
 objs = sorted(objs, key=lambda o: o[args.x])
 
-plt.xlabel(args.x)
-plt.xlabel(args.y)
+if args.group_by:
+    group_variable = args.group_by
+    groups = defaultdict(list)
+    for o in objs:
+        groups[o[group_variable]].append(o)
+else:
+    groups = {"group": objs}
 
-xs = [o[args.x] for o in objs]
-ys = [o[args.y] for o in objs]
+for group_name, group in groups.items():
+    xys = np.array([(o[args.x],o[args.y]) for o in group])
+    plt.plot(xys[:, 0], xys[:, 1], label=group_name)
 
-plt.plot(xs, ys)
+plt.xlabel(args.xlabel or args.x)
+plt.ylabel(args.ylabel or args.y[0])
+plt.title(args.title)
+plt.legend()
 
 if args.save:
     plt.savefig(args.save)
