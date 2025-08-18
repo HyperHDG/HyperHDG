1b27abf9b953c921896d44ae4078c88b8c4e8a21
Joseph Holten <joseph@holten.com>
2025-08-18 16:47:11 +0200

    gen all plots

--- experiments/ne1-part.sh
+++ experiments/ne1-part.sh
@@ -11,10 +11,10 @@ BACKENDS="kahip naive"
 LOGLEVEL="debug"
 PFLAGS="--progress --eta"
 
-echo "varying p"
+echo "partitioner: varying p"
 parallel $PFLAGS "$PART $DOM {1} $OUTPUT-ps-{1}-d{2}-{3} --delta={2} --backend={3} --log-level=${LOGLEVEL} --log-file=$OUTPUT-ps-{1}-d{2}-{3}.log --square=true" \
   ::: $(seq 2 2 20) ::: 2 ::: ${BACKENDS}
 
-echo "varying d"
+echo "partitioner: varying delta"
 parallel $PFLAGS "$PART $DOM {1} $OUTPUT-ds-{1}-d{2}-{3} --delta={2} --backend={3} --log-level=${LOGLEVEL} --log-file=$OUTPUT-ds-{1}-d{2}-{3}.log --square=true" \
   ::: 10 ::: $(seq 0 1 10) ::: ${BACKENDS}
