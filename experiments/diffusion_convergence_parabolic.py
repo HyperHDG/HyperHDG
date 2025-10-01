424c5a4e8bd3e0b2dfd4556a4be28f178bd65205
Joseph Holten <joseph@holten.com>
2025-10-01 14:11:27 +0200

    .

--- experiments/diffusion_convergence_parabolic.py
+++ experiments/diffusion_convergence_parabolic.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.sparse as sp
 from scipy.sparse.linalg import LinearOperator
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import os, sys
 import argparse
@@ -17,6 +17,10 @@ parser.add_argument("-p", "--degree",    help="polynomial degree of approximatio
 parser.add_argument("-i", "--iteration", help="iteration", default=1, type=int)
 parser.add_argument("--debug", help="toggle debug mode", action="store_true")
 parser.add_argument("--log-level", help="set the log level")
+parser.add_argument("--rtol", help="rtol", type=float, default=1e-10)
+parser.add_argument("--direct", help="direct", action="store_true")
+parser.add_argument("--mat", help="mat", action="store_true")
+parser.add_argument("-o", "--output", help="output file")
 args = parser.parse_args()
 
 logging.setLoggerClass(prin2.Logger)
@@ -57,35 +61,56 @@ const.local_solver    = "DiffusionParab<" + str(dimension) + "," + str(poly_degr
   + str(2*poly_degree) + ",TestParametersSinParab,double>"
 const.cython_replacements = ["vector[unsigned int]", "vector[unsigned int]", \
   "double", "vector[double]"]
-const.include_files   = ["reproducibles_python/parameters/diffusion.hxx"]
+const.include_files   = ["reproducibles_python/parameters/diffusion.hxx", "experiments/parameters.hxx"]
 const.debug_mode      = debug_mode
 
 PyDP = HyperHDG.include(const)
 HDG_wrapper = PyDP( [2 ** iteration] * dimension, lsol_constr= [1.,theta,delta_time] )
 
-vectorSolution = HDG_wrapper.make_initial(HDG_wrapper.zero_vector())
 
 system_size = HDG_wrapper.size_of_system()
 logger.info(f"{system_size=}")
-col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat()
-#A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
-A = sp.csc_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
+
+if args.direct or args.mat:
+  logger.info("assembling matrix...")
+  col_ind, row_ind, vals = HDG_wrapper.sparse_stiff_mat()
+  A = sp.csc_matrix((vals, (row_ind,col_ind)), shape=(system_size,system_size))
+  print(A)
+  if args.direct:
+    splu = sp.linalg.splu(A)
+else:
+  A = LinearOperator( (system_size,system_size), matvec= HDG_wrapper.trace_to_flux )
+  
+iters = 0
+start = datetime.now()
+def count_iter(x):
+  global iters
+  iters +=1
+
+logger.info("timestepping...")
+
+vectorSolution = HDG_wrapper.make_initial(HDG_wrapper.zero_vector())
+HDG_wrapper.plot_option( "fileName" , args.output)
+HDG_wrapper.plot_option( "printFileNumber" , "false" )
+HDG_wrapper.plot_option( "scale" , "0.95" )
+HDG_wrapper.plot_solution(vectorSolution, 0.)
 
 for time_step in range(time_steps):
-  
-  vectorRHS = np.multiply(HDG_wrapper.residual_flux(HDG_wrapper.zero_vector(), \
+  rhs = np.multiply(HDG_wrapper.residual_flux(HDG_wrapper.zero_vector(), \
                (time_step+1) * delta_time), -1.)
   
-  [vectorSolution, num_iter] = sp.linalg.cg(A, vectorRHS, rtol=1e-13)
+  [vectorSolution, num_iter] = sp.linalg.cg(A, rhs, rtol=args.rtol, callback=count_iter)
+
   if num_iter != 0:
     logger.error(f"no convergence in {num_iter} iterations")
+    break
 
   HDG_wrapper.set_data(vectorSolution, (time_step+1)*delta_time)
   
 error = HDG_wrapper.errors(vectorSolution, 1.)[0]
-logger.info(f"{iteration=}, {error=}")
+logger.info(f"{iteration=}, {error=}, avg num iters={iters/time_steps}")
 
-HDG_wrapper.plot_option( "fileName" , "diff_conv_parab" + str(dimension) + "-" + str(iteration) )
+HDG_wrapper.plot_option( "fileName" , args.output)
 HDG_wrapper.plot_option( "printFileNumber" , "false" )
 HDG_wrapper.plot_option( "scale" , "0.95" )
 HDG_wrapper.plot_solution(vectorSolution, 1.)
