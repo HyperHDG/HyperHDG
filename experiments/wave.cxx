28eaf19d244359f211ee5cb185d3a4d73e194408
Joseph Holten <joseph@holten.com>
2025-11-19 11:57:40 +0100

    basic setup for first order

--- experiments/wave.cxx
+++ experiments/wave.cxx
@@ -5,6 +5,7 @@
 #include <HyperHDG/geometry/unit_cube.hxx>
 #include <HyperHDG/node_descriptor/cubic.hxx>
 #include <HyperHDG/local_solver/diffusion_wave_ldgh.hxx>
+#include <HyperHDG/local_solver/diffusion_wave_first_ldgh.hxx>
 #include <HyperHDG/global_loop/hyperbolic.hxx>
 #include "parameters.hxx"
 #include <map>
