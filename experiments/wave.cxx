5032c938433978ad4933935ccc71d3611775812f
Joseph Holten <joseph@holten.com>
2026-01-08 12:58:37 +0100

    test again

--- experiments/wave.cxx
+++ experiments/wave.cxx
@@ -6,6 +6,7 @@
 #include <HyperHDG/node_descriptor/cubic.hxx>
 #include <HyperHDG/local_solver/diffusion_wave_ldgh.hxx>
 #include <HyperHDG/local_solver/diffusion_wave_first_ldgh.hxx>
+#include <HyperHDG/local_solver/diffusion_wave1_ldgh.hxx>
 #include <HyperHDG/global_loop/hyperbolic.hxx>
 #include "parameters.hxx"
 #include <map>
@@ -103,12 +104,12 @@ int main(int argc, char **argv) {
     using Top = Topology::Cubic<space_dim,space_dim>;
     using Geo = Geometry::UnitCube<space_dim,space_dim,PetscReal>;
     using NDes = NodeDescriptor::Cubic<space_dim,space_dim>;
-    using LSol = LocalSolver::DiffusionWave<space_dim,poly_deg,2*poly_deg,TestWave2,PetscReal>;
+    using LSol = LocalSolver::DiffusionWave1<space_dim,poly_deg,2*poly_deg,TestHeat,PetscReal>;
     using HDG = GlobalLoop::Hyperbolic<Top,Geo,NDes,LSol>;
 
     PetscBool help = false;
     PetscReal tau = 1; // HDG penalty
-    PetscReal theta = .25; // one-step theta method
+    PetscReal theta = .5; // one-step theta method
     PetscInt iteration = 1;
     PetscInt ts = 1;
     PetscReal end_time = 1;
