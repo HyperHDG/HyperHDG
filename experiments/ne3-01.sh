68387600f70b8106b38a7d6bc35b120e5d3b9b80
Joseph Holten <joseph@holten.com>
2026-01-14 20:18:40 +0100

    ne3-01.sh: group by theta,timesteps

--- experiments/ne3-01.sh
+++ experiments/ne3-01.sh
@@ -5,4 +5,4 @@ parallel --progress --bar \
     'python experiments/heat.py -i {1} -n {2} -t {=3 $_=2**-$_ =} | tee output/ne3-01-i{1}-n{2}-t{3}.yaml' \
     ::: $(seq 6) ::: $(seq 6 13) ::: 0 1
 yq -cr "." output/ne3-01*.yaml \
-    | experiments/plot.py -x iteration -y error -f json --log xy -g timesteps --where 'theta == 1' --save ne3-01.png
+    | experiments/plot.py -f json -x iteration -y error -g theta,timesteps --log xy --save output/ne3-01.png
