c3d891a7e0834c4cdc65571676685f0559b96784
Joseph Holten <joseph@holten.com>
2026-01-14 18:19:09 +0100

    wave rename inputs and don't exponentiate

--- experiments/wave.cxx
+++ experiments/wave.cxx
@@ -105,27 +105,21 @@ int main(int argc, char **argv) {
     using LSol = LocalSolver::DiffusionWave<space_dim,poly_deg,2*poly_deg,TestWave2,PetscReal>;
     using HDG = GlobalLoop::Hyperbolic<Top,Geo,NDes,LSol>;
 
-    PetscBool help = false;
-    PetscReal tau = 1; // HDG penalty
+    PetscBool help = false, is_set;
+    PetscInt nx = 2, nt = 1;
+    PetscInt N;            // global system size
+    PetscReal tau = 1;     // HDG penalty
     PetscReal theta = .25; // one-step theta method
-    PetscInt iteration = 1;
-    PetscInt ts = 1;
-    PetscReal end_time = 1;
-    PetscReal dt = 0;
-    PetscReal rtol = 1e-10;
-
+    PetscReal T = 1, dt = 0, rtol = 1e-10, h = 0;
+    PetscInt iterations = 0, its = 0;
+    PetscReal avg_iterations = 0, rnorm;
+    const char* creason = NULL;
     PetscBool plot = true;
     char output_directory[PATH_MAX] = "output";
     char output_filename[PATH_MAX] = "wave";
     char plot_scale[PATH_MAX] = "0.95";
 
     PetscLogStage s_as, s_ts, s_rf;
-
-    PetscBool is_set;
-    PetscInt N;
-    PetscInt iterations = 0, its = 0;
-    PetscReal avg_iterations = 0, rnorm;
-    const char* creason;
 
     std::vector<PetscReal> temp, temp2, temp3, zero_v;
     std::vector<PetscInt> itemp;
@@ -139,9 +133,9 @@ int main(int argc, char **argv) {
     PetscOptionsBegin(PETSC_COMM_WORLD, NULL, "HDG Wave Equation Options", NULL);
     PetscCall(PetscOptionsReal("-theta", "time-step averaging weight, 0 < theta <= 0.5, use theta=0.25 for CN", NULL, theta, &theta, &is_set));
     PetscCall(PetscOptionsReal("-tau", "hdg penalty parameter, recommended: tau ~ h^s for s in {-1,0,1}", NULL, tau, &tau, &is_set));
-    PetscCall(PetscOptionsInt("-i", "number of subdivisions of the domain, i >= 1", NULL, iteration, &iteration, &is_set));
-    PetscCall(PetscOptionsInt("-ts", "number of timesteps", NULL, ts, &ts, &is_set));
-    PetscCall(PetscOptionsReal("-T", "end time", NULL, end_time, &end_time, &is_set));
+    PetscCall(PetscOptionsInt("-nx", "number of elements divide the domain into", NULL, nx, &nx, &is_set));
+    PetscCall(PetscOptionsInt("-nt", "number of timesteps", NULL, nt, &nt, &is_set));
+    PetscCall(PetscOptionsReal("-T", "end time", NULL, T, &T, &is_set));
     PetscCall(PetscOptionsString("-o", "output filename", NULL, output_filename, output_filename, PATH_MAX, &is_set));
     PetscCall(PetscOptionsString("-od", "output directory", NULL, output_directory, output_directory, PATH_MAX, &is_set));
     PetscCall(PetscOptionsBool("-plot", "plot solution", NULL, plot, &plot, &is_set));
@@ -159,10 +153,18 @@ int main(int argc, char **argv) {
     PetscCall(PetscLogStageRegister("Timestepping", &s_ts));
     PetscCall(PetscLogStageRegister("residual_flux", &s_rf));
 
-    ts = 1<<ts;
-    dt = end_time / ts;
+    dt = T / nt;
+    h = 1. / nx;
 
-    HDG hdg((1 << iteration) * space_dim, {tau, theta, dt});
+    HDG hdg(nx, {tau, theta, dt});
+    PRIN2IY(space_dim);
+    PRIN2IY(poly_deg);
+    PRIN2FY(tau);
+    PRIN2FY(theta);
+    PRIN2IY(nt);
+    PRIN2FY(dt);
+    PRIN2FY(T);
+    PRIN2FY(h);
     hdg.plot_option("fileName", output_filename);
     hdg.plot_option("outputDir", output_directory);
     hdg.plot_option("printFileNumber", "true");
@@ -178,7 +180,6 @@ int main(int argc, char **argv) {
     PetscCall(VecCreateSeq(PETSC_COMM_SELF, N, &rhs));
 
     PRIN2S(s_as);
-    PetscCall(PetscPrintf(PETSC_COMM_SELF, "assembly...\n"));
     mat_coo = hdg.trace_to_flux_mat(0.);
     PetscCall(MatCreateFromOptions(PETSC_COMM_SELF, NULL, 1, PETSC_DECIDE, PETSC_DECIDE, N, N, &mat));
     PetscCall(MatSetPreallocationCOO(mat, mat_coo.value_vec.size(), (PetscInt*)mat_coo.row_vec.data(), (PetscInt*)mat_coo.col_vec.data()));
@@ -195,7 +196,7 @@ int main(int argc, char **argv) {
     PetscCall(KSPSetFromOptions(ksp));
 
     PRIN2S(s_ts);
-    for (PetscInt i = 0; i < ts; i++) {
+    for (PetscInt i = 0; i < nt; i++) {
         std::span<PetscReal> span;
         PetscCall(VecGetSpan(rhs, span));
         PetscLogStagePush(s_rf);
@@ -220,11 +221,11 @@ int main(int argc, char **argv) {
     PetscCall(KSPGetResidualNorm(ksp, &rnorm));
 
     // zero_v unused
-    temp2 = hdg.errors(zero_v, end_time);
-    temp3 = hdg.norms(zero_v, end_time);
+    temp2 = hdg.errors(zero_v, T);
+    temp3 = hdg.norms(zero_v, T);
     for (size_t i = 0; i < temp3.size(); i++)
       temp3[i] = temp2[i] / temp3[i];
-    avg_iterations = ((PetscReal)iterations) / ts;
+    avg_iterations = ((PetscReal)iterations) / nt;
 
     PetscCall(PetscPrintf(PETSC_COMM_SELF, "e_abs: %.5e\n", temp2[0]));
     PetscCall(PetscPrintf(PETSC_COMM_SELF, "e_rel: %.5e\n", temp3[0]));
