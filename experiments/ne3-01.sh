735001b825e108c37d28fecd871f3c3581cf59ca
Joseph Holten <joseph@holten.com>
2026-01-14 20:24:24 +0100

    ne3-0*.sh: flexible running

--- experiments/ne3-01.sh
+++ experiments/ne3-01.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -euox pipefail
+set -eox pipefail
 
 mkdir -p output
-parallel --progress --bar --results output/ne3-01.json \
-    'build/rel/experiments/heat -i {1} -ts {2} -t {=3 $_=2**-$_ =}' \
-    ::: $(seq 6) ::: $(seq 6 13) ::: 1
-yq -I0 -o=json ".Stdout | from_yaml" output/ne3-01.json \
+[ -z $NOGEN ] && parallel --shuf --progress --bar --results output/ne3-01.json \
+    "build/rel/experiments/heat -plot 0 -i {1} -ts {2}" \
+    ::: $(seq 6) ::: $(seq 6 13)
+[ -z $NOPLOT ] && yq -I0 -o=json ".Stdout | from_yaml" output/ne3-01.json \
     | experiments/plot.py -f json -x it -y e_abs -g timesteps \
         --where "theta == .5" --log xy --trans "2.**-x,y" --xlabel h --xbase 2 \
-        --ref "4;2,1;1e-5"
+        --ref "4;2,1;1e-5" --save output/ne3-01.png,output/ne3-01.pgf $PLOTARGS
