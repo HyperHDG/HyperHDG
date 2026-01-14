361983aae4dac23ad16c7b8eb620acbd9664e7f8
Joseph Holten <joseph@holten.com>
2026-01-14 18:58:43 +0100

    ne3-02 basic convergence test for for wave

---
+++ experiments/ne3-02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euox pipefail
+
+mkdir -p output
+parallel --progress --bar --results output/ne3-02.json \
+    'build/rel/experiments/wave -nx {=1 $_=2**$_ =} -nt {=2 $_=2**$_ =}' \
+    ::: $(seq 10) ::: $(seq 7 13)
+yq -I0 -o=json ".Stdout | from_yaml" output/ne3-02.json \
+    | experiments/plot.py -f json -x h -y e_abs -g nt \
+        --log xy --xbase 2 --ref "4;.5,.25;1e-2"
