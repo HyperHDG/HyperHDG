c8986b4e04c7b3094df402141c159bf59b3db90b
Joseph Holten <joseph@holten.com>
2026-01-14 20:24:24 +0100

    ne3-0*.sh: flexible running

--- experiments/ne3-02.sh
+++ experiments/ne3-02.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -euox pipefail
+set -eox pipefail
 
 mkdir -p output
-parallel --progress --bar --results output/ne3-02.json \
-    'build/rel/experiments/wave -nx {=1 $_=2**$_ =} -nt {=2 $_=2**$_ =}' \
+[ -z $NOGEN ] && parallel --shuf --progress --bar --results output/ne3-02.json \
+    'build/rel/experiments/wave -plot 0 -nx {=1 $_=2**$_ =} -nt {=2 $_=2**$_ =}' \
     ::: $(seq 10) ::: $(seq 7 13)
-yq -I0 -o=json ".Stdout | from_yaml" output/ne3-02.json \
+[ -z $NOPLOT ] && yq -I0 -o=json ".Stdout | from_yaml" output/ne3-02.json \
     | experiments/plot.py -f json -x h -y e_abs -g nt \
-        --log xy --xbase 2 --ref "4;.25,.5;1e-3"
+        --log xy --xbase 2 --ref "4;.25,.5;1e-3" --save output/ne3-02.png,output/ne3-02.pgf $PLOTARGS
