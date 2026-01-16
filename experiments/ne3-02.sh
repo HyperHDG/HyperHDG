6831d22945e4d5f5d15569826babf14818c5d46f
Joseph Holten <joseph@holten.com>
2026-01-16 16:41:54 +0100

    ne3: update ergonomics

--- experiments/ne3-02.sh
+++ experiments/ne3-02.sh
@@ -2,9 +2,14 @@
 set -eox pipefail
 
 mkdir -p output
-[ -z $NOGEN ] && parallel --shuf --progress --bar --results output/ne3-02.json \
+OUT=output/$(basename "${0%.sh}")
+
+if [ -z $NOGEN ]; then
+   mv $OUT.json $OUT.json.1 || true
+   parallel --shuf --progress --bar --results $OUT.json \
     'build/rel/experiments/wave -plot 0 -nx {=1 $_=2**$_ =} -nt {=2 $_=2**$_ =}' \
-    ::: $(seq 10) ::: $(seq 7 13)
-[ -z $NOPLOT ] && yq -I0 -o=json ".Stdout | from_yaml" output/ne3-02.json \
+    ::: $(seq 8) ::: $(seq 7 3 16)
+fi
+[ -z $NOPLOT ] && yq -I0 -o=json ".Stdout | from_yaml" $OUT.json \
     | experiments/plot.py -f json -x h -y e_abs -g nt \
-        --log xy --xbase 2 --ref "4;.25,.5;1e-3" --save output/ne3-02.png,output/ne3-02.pgf $PLOTARGS
+        --log xy --xbase 2 --ref "4;.25,.5;1e-5" --save $OUT.png,$OUT.pgf $PLOTARGS
