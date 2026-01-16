d7aef9906340f3f3997668b20cbfdca56c48d71f
Joseph Holten <joseph@holten.com>
2026-01-16 11:28:41 +0100

    ne3 ergonomics, heat output h

--- experiments/ne3-03-wave-nt.sh
+++ experiments/ne3-03-wave-nt.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -eox pipefail
+set -x
 
-mkdir -p output
-OUT=output/$(basename "${0%.sh}")
+: "${OUT:=${OUT_DIR:=output}/$(basename "${0%.sh}")}" # set default if unset
+mkdir -p $OUT_DIR
 
 if [ -z $NOGEN ]; then
-   mv $OUT.json $OUT.json.1 || true
    parallel --shuf --progress --bar --results $OUT.json \
      'build/rel/experiments/wave -plot 0 -nx {1} -nt {=2 $_=2**$_ =} -theta {3}' \
      ::: 1024 ::: $(seq 15) ::: 1 .5
+   echo "gen exit: $?"
+   cp $OUT.json{,$(date +%s)}
 fi
 [ -z $NOPLOT ] && yq -I0 -o=json ".Stdout | from_yaml" $OUT.json \
     | experiments/plot.py -f json -x dt -y e_abs -g theta \
