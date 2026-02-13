30d0a6ab411bbbf02e0e614e2df7af7772309ba6
Joseph Holten <joseph@holten.com>
2026-02-13 13:18:26 +0100

    better timing plot

--- experiments/ne4-01.sh
+++ experiments/ne4-01.sh
@@ -5,11 +5,11 @@ set -x
 mkdir -p $OUT_DIR
 
 if [ -z $NOGEN ]; then
-  parallel --shuf --progress --bar --results $OUT.json \
+  parallel -j 1 --progress --bar --results $OUT.json \
     'build/rel/experiments/network -plot 0 -net2as_print_local -domain domains/paper-small.geo.h5 -mat_cache output/small-mat.bin -net2as_p {1}' \
       ::: 1 2 4 8 16
   echo "gen exit: $?"
   cp $OUT.json{,.$(date +%s)}
 fi
 [ -z $NOPLOT ] && yq -I0 -o=json '.Stdout | from_yaml | .net2as | .sz as $sz | .local.[] | {"n": .size, "t": .time, "sz": $sz}' output/ne4-01.json \
-    | experiments/plot.py -f json --scatter -x n -y t -g sz --save $OUT.png $PLOTARGS --log xy --ref "1.5;1e3,3e3;5e-4"
+    | experiments/plot.py -f json --scatter -x n -y t -g sz --save $OUT.png $PLOTARGS --log xy --ref "2;8e3,3e4;1e-2"
