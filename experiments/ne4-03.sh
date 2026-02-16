d4a793d1df842ed5fa8c94a1b74ba9d3457044af
Joseph Holten <joseph@holten.com>
2026-02-16 12:32:36 +0100

    add time plot and ne4-03

--- experiments/ne4-03.sh
+++ experiments/ne4-03.sh
@@ -6,8 +6,8 @@ mkdir -p $OUT_DIR
 
 if [ -z $NOGEN ]; then
   parallel -j 1 --progress --bar --results $OUT.json \
-    'build/rel/experiments/network -plot 0 -net2as_print_local -domain domains/paper-small.geo.h5 -mat_cache output/small-mat.bin -net2as_p {1} -net2as_pc_factor_mat_solver_type mumps' \
-      ::: 1 2 4 8 16
+    'build/rel/experiments/network -plot 0 -net2as_print_local -domain domains/paper-big.geo.h5 -mat_cache output/big-mat.bin -net2as_p {1} -net2as_pc_factor_mat_solver_type mumps' \
+      ::: 1 2 4 8 16 24
   echo "gen exit: $?"
   cp $OUT.json{,.$(date +%s)}
 fi
@@ -15,5 +15,3 @@ fi
     | experiments/plot.py -f json --scatter -x n -y t -g p --save $OUT-a.png $PLOTARGS --log xy --ref "1;5e2,2e3;6e-4"
 [ -z $NOPLOT ] && yq -I0 -o=json '.Stdout | from_yaml | .net2as | .sz as $sz | .local.[] | {"n": .size, "fill": .fill, "p": $sz}' output/ne4-01.json \
     | experiments/plot.py -f json --scatter -x n -y fill -g p --save $OUT-b.png $PLOTARGS --log xy
-[ -z $NOPLOT ] && yq -I0 -o=json '.Stdout | from_yaml | {"p": .net2as.sz, "t": .t_ksp}' output/ne4-01.json \
-    | experiments/plot.py -f json -x p -y t --save $OUT-c.png $PLOTARGS --log xy
