705014d881f800b1ca3a95c487037b5a3a4f875f
Joseph Holten <joseph@holten.com>
2026-02-16 11:37:59 +0100

    simple test

---
+++ experiments/ne4-02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+: "${OUT:=${OUT_DIR:=output}/$(basename "${0%.sh}")}" # set default if unset
+mkdir -p $OUT_DIR
+
+if [ -z $NOGEN ]; then
+  parallel -j 1 --progress --bar --results $OUT.json \
+    'mpirun -n 8 build/rel/experiments/network -plot 0 -domain domains/paper-{1}.geo.h5 -mat_cache output/{1}-mat.bin -pc_type jacobi -ksp_monitor_yaml -ksp_atol 1e-5' \
+      ::: small
+  echo "gen exit: $?"
+  cp $OUT.json{,.$(date +%s)}
+fi
+
+[ -z $NOPLOT ] && yq -I0 -o=json '.Stdout |= from_yaml | .V[0] as $d | .Stdout.ksp_monitor[] | {"it": .it, "rnorm": .rnorm, "d": $d}' output/ne4-02.json \
+    | experiments/plot.py -f json -x it -y rnorm -g d --save $OUT.png $PLOTARGS --log xy --marker ''
