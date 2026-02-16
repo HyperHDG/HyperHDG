b50b95837671a57ca3ba238c8267ede49ae8b5ed
Joseph Holten <joseph@holten.com>
2026-02-16 11:39:59 +0100

    now with big

--- experiments/ne4-02.sh
+++ experiments/ne4-02.sh
@@ -6,11 +6,11 @@ mkdir -p $OUT_DIR
 
 if [ -z $NOGEN ]; then
   parallel -j 1 --progress --bar --results $OUT.json \
-    'mpirun -n 8 build/rel/experiments/network -plot 0 -domain domains/paper-{1}.geo.h5 -mat_cache output/{1}-mat.bin -pc_type jacobi -ksp_monitor_yaml -ksp_atol 1e-5' \
-      ::: small
+    'mpirun -n 8 build/rel/experiments/network -plot 0 -domain domains/paper-{1}.geo.h5 -mat_cache output/{1}-mat.bin -pc_type jacobi -ksp_monitor_yaml -ksp_atol 1e-10' \
+      ::: small big
   echo "gen exit: $?"
   cp $OUT.json{,.$(date +%s)}
 fi
 
-[ -z $NOPLOT ] && yq -I0 -o=json '.Stdout |= from_yaml | .V[0] as $d | .Stdout.ksp_monitor[] | {"it": .it, "rnorm": .rnorm, "d": $d}' output/ne4-02.json \
-    | experiments/plot.py -f json -x it -y rnorm -g d --save $OUT.png $PLOTARGS --log xy --marker ''
+[ -z $NOPLOT ] && yq -I0 -o=json '.Stdout |= from_yaml | .V[0] as $domain | .Stdout.ksp_monitor[] | {"it": .it, "rnorm": .rnorm, "domain": $d}' output/ne4-02.json \
+    | experiments/plot.py -f json -x it -y rnorm -g domain --save $OUT.png $PLOTARGS --log xy --marker ''
