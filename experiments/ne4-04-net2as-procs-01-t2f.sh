bdc5d89e029f8e4130c6e0f67ae89fc4b70dbe18
Joseph Holten <joseph@holten.com>
2026-02-18 00:51:12 +0100

    plot efficiency

--- experiments/ne4-04-net2as-procs-01-t2f.sh
+++ experiments/ne4-04-net2as-procs-01-t2f.sh
@@ -4,4 +4,6 @@ set -x
 : "${OUT:=${OUT_DIR:=output}/ne4-04-net2as-procs}" # set default if unset
 mkdir -p $OUT_DIR
 
-yq -I0 -o=json '.Stdout | {"p": .mpi.sz, "t_t2f": .t_t2f}' $OUT.json | experiments/plot.py -f json -x p -y t_t2f
+jq -c '.Stdout | {"p": .mpi.sz, t_t2f}' $OUT.json \
+    | jq -sc '.[0].t_t2f as $t1 | .[] | {p, E_t2f: ($t1 / (.p * .t_t2f))}' \
+    | experiments/plot.py -f json -x p -y E_t2f --log x
