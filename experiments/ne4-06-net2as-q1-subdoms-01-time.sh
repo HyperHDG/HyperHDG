9222a5c6c93e6ac5a818ab83a817772ea99724c6
Joseph Holten <joseph@holten.com>
2026-02-18 04:56:03 +0100

    for q1 also subdom direct solvers

---
+++ experiments/ne4-06-net2as-q1-subdoms-01-time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+
+: "${OUT:=${OUT_DIR:=output}/ne4-06-net2as-q1-subdoms}" # set default if unset
+mkdir -p $OUT_DIR
+
+jq -c '.Stdout | .net2as.sz as $p | .net2as.local[] | {p: $p, fill, time, size}' $OUT.json \
+    | experiments/plot.py -f json -x size -y time -g p --log xy --ref "1.5;2e4,6e4;2e-2"
+
