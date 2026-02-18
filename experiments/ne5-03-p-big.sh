8c9bee1df90c1ec460a3c0503d9baa3c3c7f5fc0
Joseph Holten <joseph@holten.com>
2026-02-19 00:08:28 +0100

    more tests

---
+++ experiments/ne5-03-p-big.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+: "${OUT:=${OUT_DIR:=output}/$(basename "${0%.sh}")}" # set default if unset
+mkdir -p $OUT_DIR
+
+DATA_DIR=.
+DOMAIN="paper-big"
+DOMAIN="-domain $DATA_DIR/domains/$DOMAIN.geo.h5 -mat_cache $DATA_DIR/output/$DOMAIN.bin"
+BIN_DIR=build/rel/experiments
+
+parallel --progress --bar --results $OUT.json \
+  "$BIN_DIR/network -plot 0 $DOMAIN -net2as_p {1} -net2as_cb_type {2}" \
+  ::: 1 2 4 8 16 24 ::: q1 pu
+echo "gen exit: $?"
+yq -i '.Stdout |= from_yaml' $OUT.json
+cp $OUT.json{,.$(date +%s)}
+
