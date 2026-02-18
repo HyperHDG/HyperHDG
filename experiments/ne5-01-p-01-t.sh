95c70927490f6053cb37168c02415a2682209358
Joseph Holten <joseph@holten.com>
2026-02-18 20:56:40 +0100

    basic p scaling working

---
+++ experiments/ne5-01-p-01-t.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+: "${OUT:=${OUT_DIR:=output}/ne5-01-p}" # set default if unset
+jq -c '.Stdout | {p: .net2as.sz, t_ksp, t_it: .t_iteration, t: (.t_ksp + .t_iteration), it: .iterations}' $OUT.json
--- experiments/ne5-01-p.sh
+++ experiments/ne5-01-p.sh
@@ -8,13 +8,11 @@ DATA_DIR=.
 DOMAIN="paper-small"
 DOMAIN="-domain $DATA_DIR/domains/$DOMAIN.geo.h5 -mat_cache $DATA_DIR/output/$DOMAIN.bin"
 BIN_DIR=build/rel/experiments
-export OMP_NUM_THREADS=1
 
-parallel -j 1 --progress --bar --results $OUT.json \
+parallel --progress --bar --results $OUT.json \
   "$BIN_DIR/network -plot 0 $DOMAIN -net2as_p" \
   ::: 1 2 4 8 16
 echo "gen exit: $?"
 yq -i '.Stdout |= from_yaml' $OUT.json
 cp $OUT.json{,.$(date +%s)}
 
-jq -c '.Stdout | {p: .net2as.p, t_ksp, t_iteration, t: (.t_ksp + .t_iteration)}' $OUT.json
