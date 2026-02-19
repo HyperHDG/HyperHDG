5fc379887b08f4b8b159e9ed92691cfdcaf60666
Joseph Holten <joseph@holten.com>
2026-02-19 16:05:22 +0100

    single expression

--- experiments/ne5-04-procs-big-01-E.sh
+++ experiments/ne5-04-procs-big-01-E.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -x
 : "${OUT:=${OUT_DIR:=output}/ne5-04-procs-big}" # set default if unset
-jq -sc 'map(.Stdout | {p: .mpi.sz, t_t2f}) | sort_by(.p) | .[0].t_t2f as $t2f | .[] | {p, stage: "t2f", E: ($t2f / (.t_t2f * .p))}' $OUT.json
-jq -sc 'map(.Stdout | {p: .mpi.sz, t_ksp}) | sort_by(.p) | .[]' $OUT.json
-jq -sc 'map(.Stdout | {p: .mpi.sz, t_ksp}) | sort_by(.p) | .[0].t_ksp as $ksp | .[] | {p, stage: "ksp", E: ($ksp / (.t_ksp * .p))}' $OUT.json
-jq -sc 'map(.Stdout | {p: .mpi.sz, t_it: .t_iteration}) | sort_by(.p) | .[0].t_it as $it | .[] | {p, stage: "it", E: ($it / (.t_it * .p))}' $OUT.json
-
+jq -sc '
+  map(.Stdout | {p: .mpi.sz, t_t2f, t_ksp, t_it: .t_iteration}) | sort_by(.p) |
+  . as $all |
+  ["t_t2f", "t_ksp", "t_it"] | .[] as $key |
+  $all | (.[0][$key]) as $base |
+  .[] | {p, stage: $key, E: ($base / (.[$key] * .p))}
+' $OUT.json
