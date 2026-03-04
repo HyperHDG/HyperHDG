1ccdebc2ab914a7fb2f9c59cf807dd3b0e9d47bd
Joseph Holten <joseph@holten.com>
2026-03-04 16:52:13 +0100

    swapped signs

--- include/HyperHDG/local_solver/timowave.hxx
+++ include/HyperHDG/local_solver/timowave.hxx
@@ -773,7 +773,7 @@ class TimoshenkoWave
     for (unsigned int i = 0; i < n_shape_fct_; i++) {
       for (unsigned int j = 0; j < n_shape_fct_; j++) {
         SmallVec<hyEdge_dimT, lSol_float_t> grad_int_vec =
-          (-1.) * integrator::template integrate_vol_nablaphiphi<SmallVec<hyEdge_dimT, lSol_float_t>,
+          integrator::template integrate_vol_nablaphiphi<SmallVec<hyEdge_dimT, lSol_float_t>,
               decltype(hyEdgeT::geometry)>(i, j, hyper_edge.geometry);
         SmallVec<hyEdge_dimT, lSol_float_t> bdr_int;
 
@@ -787,9 +787,9 @@ class TimoshenkoWave
         // NOTE: also need theta of old v with extra coeffs
         // NOTE: why no normal_int_vec here?
         for (unsigned int dim = 0; dim < space_dim; dim++) {
-          flux_u[dim*n_shape_fct_ + i] -= grad_int_vec[0]
+          flux_u[dim*n_shape_fct_ + i] -= (-1)*grad_int_vec[0]
             * n_old[dim*n_shape_fct_ +j] + tau_ * bdr_int[0] * u_old[dim*n_shape_fct_+j];
-          flux_r[dim*n_shape_fct_ + i] -= grad_int_vec[0]
+          flux_r[dim*n_shape_fct_ + i] -= (-1)*grad_int_vec[0]
             * m_old[dim*n_shape_fct_ +j] + tau_ * bdr_int[0] * r_old[dim*n_shape_fct_+j];
 
           // NOTE: u_old are already summed in rhs_from_global_rhs
