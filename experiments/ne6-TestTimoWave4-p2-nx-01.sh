30a8b8d4e44e789e5a8a8b4fe4c8110630bf6a3f
Joseph Holten <joseph@holten.com>
2026-03-06 09:33:47 +0100

    fix indicated convergence order

--- experiments/ne6-TestTimoWave4-p2-nx-01.sh
+++ experiments/ne6-TestTimoWave4-p2-nx-01.sh
@@ -5,4 +5,4 @@ set -x
 mkdir -p $OUT_DIR
 
 jq -cs 'map(.Stdout | {e_abs, nx, nt}) | sort_by(.nx).[]' $OUT.json \
-   | experiments/plot.py -x nx -y e_abs --log xy --xbase 2 --ref '-2;8,32;6e-2'
+   | experiments/plot.py -x nx -y e_abs --log xy --xbase 2 --ref '-3;8,32;6e-2'
