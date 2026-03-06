46cf62afc2394b9c1e8dc0d111c853fc094fb759
Joseph Holten <joseph@holten.com>
2026-03-06 09:12:12 +0100

    verified nx convergence

---
+++ experiments/ne6-TestTimoWave4-p2-nx-01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+: "${OUT:=${OUT_DIR:=output}/ne6-TestTimoWave4-p2-nx}" # set default if unset
+mkdir -p $OUT_DIR
+
+jq -cs 'map(.Stdout | {e_abs, nx, nt}) | sort_by(.nx).[]' $OUT.json \
+   | experiments/plot.py -x nx -y e_abs --log xy --xbase 2 --ref '-2;8,32;6e-2'
