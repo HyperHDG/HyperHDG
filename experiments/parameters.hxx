659ef4c3bad787ea92312460638f9fa91b1c0329
Joseph Holten <joseph@holten.com>
2026-03-12 15:41:45 +0100

    this works for single1, single2
    HyperHDG $ cmake --build --preset rel --target timowave && build/rel/experiments/timowave -test 4 -deg 6 -nx 25 -nt 1 -domain domains/single2.geo
    ninja: no work to do.
    timowave_test: 4
    poly_deg: 6
    tau: 1.00000e+00
    theta: 5.00000e-01
    nt: 1
    nx: 25
    dt: 1.00000e+00
    T: 1.00000e+00
    t_Assembly: 7.39223e-02
    t_Timestepping: 2.10320e-02
    e_abs: 2.17768e-11
    iterations: 26
    rnorm: 4.68165e-14
    creason: CONVERGED_RTOL
    avg_iterations: 2.60000e+01
    output: output/timowave.*.vtu
    HyperHDG $ cmake --build --preset rel --target timowave && build/rel/experiments/timowave -test 4 -deg 6 -nx 25 -nt 1 -domain domains/single1.geo
    ninja: no work to do.
    timowave_test: 4
    poly_deg: 6
    tau: 1.00000e+00
    theta: 5.00000e-01
    nt: 1
    nx: 25
    dt: 1.00000e+00
    T: 1.00000e+00
    t_Assembly: 7.38402e-02
    t_Timestepping: 1.96346e-02
    e_abs: 2.80892e-11
    iterations: 24
    rnorm: 3.36778e-15
    creason: CONVERGED_RTOL
    avg_iterations: 2.40000e+01
    output: output/timowave.*.vtu

--- experiments/parameters.hxx
+++ experiments/parameters.hxx
@@ -633,7 +633,7 @@ struct TestTimoWave4
                                          const param_float_t time = 0.)
   {
     SmallVec<space_dimT, param_float_t> res(0.);
-    res[2] = omega*omega*cos(omega*point[1]);
+    res[2] = omega*omega*cos(omega*(point[0]+point[1]));
     return scalar_product(res, normal);
   }
   // g
@@ -643,6 +643,7 @@ struct TestTimoWave4
   {
     SmallVec<space_dimT, param_float_t> res(0.);
     res[0] = -omega*(sin(omega*point[1])); // NOTE: again! opposite sign as mathematica
+    res[1] = -omega*(sin(omega*point[0]));
     return scalar_product(res, normal);
   }
   static param_float_t dirichlet_value_u(const Point<space_dimT, param_float_t>& point,
@@ -674,7 +675,7 @@ struct TestTimoWave4
 
   static SmallVec<space_dimT, param_float_t> initial_u(const Point<space_dimT, param_float_t>& point, const param_float_t time = 0.) {
     SmallVec<space_dimT, param_float_t> res(0.);
-    res[2] = cos(omega*(point[1]));
+    res[2] = cos(omega*(point[0]+point[1]));
     return res;
   }
 
@@ -695,7 +696,7 @@ struct TestTimoWave4
 
   static SmallVec<space_dimT, param_float_t> initial_n(const Point<space_dimT, param_float_t>& point, const param_float_t time = 0.) {
     SmallVec<space_dimT, param_float_t> res(0.);
-    res[2] = omega*sin(omega*(point[1]));
+    res[2] = omega*sin(omega*(point[0]+point[1]));
     return res;
   }
 
