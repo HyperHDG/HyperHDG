a174065a08f2cae259e38a319d5c60f1b193f0eb
Joseph Holten <joseph@holten.com>
2026-03-30 15:16:25 +0200

    activate printing

--- include/HyperHDG/local_solver/timowave.hxx
+++ include/HyperHDG/local_solver/timowave.hxx
@@ -988,6 +988,21 @@ class TimoshenkoWave
         m_old[i+dim*n_shape_fct_] = res[dim];
     }
 
+    std::cout << "----- make_initial (normals)" << std::endl;
+    std::cout << (Point<space_dim, lSol_float_t>)hyper_edge.geometry.inner_normal(0);
+    std::cout << (Point<space_dim, lSol_float_t>)hyper_edge.geometry.outer_normal(0);
+    std::cout << (Point<space_dim, lSol_float_t>)hyper_edge.geometry.outer_normal(1);
+
+    std::cout << "----- make_initial (glob)" << std::endl;
+    std::cout << "u " << hyper_edge.data.u_old;
+    std::cout << "v " << hyper_edge.data.v_old;
+    for (unsigned int i=0; i < lambda_values.size(); i++) {
+      std::cout << "lambda " << i << "| ";
+      for (unsigned int j=0; j < lambda_values[i].size(); j++)
+        std::cout << lambda_values[i][j] << " ";
+      std::cout << std::endl;
+    }
+
     // transform global dofs to edge dofs
     u_old = glob_dof_to_loc_dof(u_old, hyper_edge);
     r_old = glob_dof_to_loc_dof(r_old, hyper_edge);
@@ -999,23 +1014,23 @@ class TimoshenkoWave
     auto lambda_values_loc = node_dof_to_edge_dof(lambda_values, hyper_edge);
     compute_fluxes(lambda_values_loc, hyper_edge, time);
 
-    // std::cout << "----- make_initial" << std::endl;
-    // std::cout << "u " << hyper_edge.data.u_old << std::endl;
-    // std::cout << "r " << hyper_edge.data.r_old << std::endl;
-    // std::cout << "n " << hyper_edge.data.n_old << std::endl;
-    // std::cout << "m " << hyper_edge.data.m_old << std::endl;
-    // std::cout << "v " << hyper_edge.data.v_old << std::endl;
-    // std::cout << "s " << hyper_edge.data.s_old << std::endl;
-    // for (unsigned int i=0; i < lambda_values.size(); i++) {
-    //   std::cout << "lambda " << i << "| ";
-    //   for (unsigned int j=0; j < lambda_values[i].size(); j++)
-    //     std::cout << lambda_values_loc[i][j] << " ";
-    //   std::cout << std::endl;
-    // }
-    // std::cout << "flux_u " << hyper_edge.data.flux_u << std::endl;
-    // std::cout << "flux_r " << hyper_edge.data.flux_r << std::endl;
-    // std::cout << "flux_v " << hyper_edge.data.flux_v << std::endl;
-    // std::cout << "flux_s " << hyper_edge.data.flux_s << std::endl;
+    std::cout << "----- make_initial" << std::endl;
+    std::cout << "u " << hyper_edge.data.u_old << std::endl;
+    std::cout << "r " << hyper_edge.data.r_old << std::endl;
+    std::cout << "n " << hyper_edge.data.n_old << std::endl;
+    std::cout << "m " << hyper_edge.data.m_old << std::endl;
+    std::cout << "v " << hyper_edge.data.v_old << std::endl;
+    std::cout << "s " << hyper_edge.data.s_old << std::endl;
+    for (unsigned int i=0; i < lambda_values.size(); i++) {
+      std::cout << "lambda " << i << "| ";
+      for (unsigned int j=0; j < lambda_values[i].size(); j++)
+        std::cout << lambda_values_loc[i][j] << " ";
+      std::cout << std::endl;
+    }
+    std::cout << "flux_u " << hyper_edge.data.flux_u << std::endl;
+    std::cout << "flux_r " << hyper_edge.data.flux_r << std::endl;
+    std::cout << "flux_v " << hyper_edge.data.flux_v << std::endl;
+    std::cout << "flux_s " << hyper_edge.data.flux_s << std::endl;
 
     return lambda_values;
   }
