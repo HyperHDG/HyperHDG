6b77e86aa8c2ab294ffc5a0a51f173348d1a0bf3
Joseph Holten <joseph@holten.com>
2026-04-14 17:20:14 +0200

    set figsize

--- experiments/ne8-01-conv-t-01.sh
+++ experiments/ne8-01-conv-t-01.sh
@@ -6,7 +6,7 @@ mkdir -p $OUT_DIR
 
 jq -c '.Stdout | {dt, e_abs, theta}' $OUT.json \
     | experiments/plot.py -g theta -x dt -y e_abs --log xy \
-        --xbase 2 \
+        --xbase 2 --figsize '4,4' \
         --ref "2;1e-2,2e-2;1e-3|1;1e-2,2e-2;1e-1" \
         --save "$OUT.png,$OUT.pgf" \
         --xlabel 'timestep size $\Delta t$' --ylabel 'maximum $L^2$ error'
