09f64f6d748a6ebe50b64c96ca18d78236bd51c1
Joseph Holten <joseph@holten.com>
2026-04-14 17:09:44 +0200

    plot for both theta=1 and theta=.5

--- experiments/ne8-01-conv-t-01.sh
+++ experiments/ne8-01-conv-t-01.sh
@@ -4,6 +4,9 @@ set -x
 : "${OUT:=${OUT_DIR:=output}/ne8-01-conv-t}"
 mkdir -p $OUT_DIR
 
-jq -c '.Stdout | {dt, e_abs}' $OUT.json \
-    | experiments/plot.py -x dt -y e_abs --log xy --ref "2;1e-2,2e-2;1e-3" --save "$OUT.png,$OUT.pgf" \
+jq -c '.Stdout | {dt, e_abs, theta}' $OUT.json \
+    | experiments/plot.py -g theta -x dt -y e_abs --log xy \
+        --xbase 2 \
+        --ref "2;1e-2,2e-2;1e-3|1;1e-2,2e-2;1e-1" \
+        --save "$OUT.png,$OUT.pgf" \
         --xlabel 'timestep size $\Delta t$' --ylabel 'maximum $L^2$ error'
