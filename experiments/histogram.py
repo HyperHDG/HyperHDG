f2398e014fddf6cd95afbae14926d281c245624d
Joseph Holten <joseph@holten.com>
2026-04-16 17:34:41 +0200

    show k shear as well

--- experiments/histogram.py
+++ experiments/histogram.py
@@ -21,12 +21,27 @@ with h5py.File(args.input, "r") as f:
     points = f["domain/points"][:]
     edges  = f["domain/edges"][:]
     unit_length = f["domain"].attrs["unit_length"]
+    props = f["domain/properties"][:]
 
 endpoints = points[edges]
 lengths = np.linalg.norm(endpoints[:, 1] - endpoints[:, 0], axis=1)
 
+kG1A, kG2A = props[:, 1], props[:, 2]
+E1I1, E2I2 = props[:, 4], props[:, 5]
+k_shear = np.sqrt(np.maximum(kG1A / E1I1, kG2A / E2I2))
+
+lo = min(lengths.min(), (1/k_shear).min())
+hi = max(lengths.max(), (1/k_shear).max())
 bins = np.logspace(np.log10(lengths.min()), np.log10(lengths.max()), args.bins + 1)
-plt.hist(lengths, bins=bins, weights=np.ones_like(lengths) / len(lengths))
+
+plt.hist(lengths, bins=bins, weights=np.ones_like(lengths) / len(lengths), histtype="step", label="lengths")
+plt.hist(1/k_shear, bins=bins, weights=np.ones_like(k_shear)/len(k_shear),
+         histtype="step", label=r"$k_shear=\sqrt{EI/kGA}$")
+
+kG1A, kG2A = props[:, 1], props[:, 2]
+E1I1, E2I2 = props[:, 4], props[:, 5]
+k_shear = np.sqrt(np.maximum(kG1A / E1I1, kG2A / E2I2))
+
 plt.xscale("log")
 plt.yscale("log")
 plt.xlabel(f"edge length {unit_length}")
