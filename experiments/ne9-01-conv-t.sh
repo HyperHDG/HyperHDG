b25d2bc5942078de92686fbc03e4bec2d9d28adc
Joseph Holten <joseph@holten.com>
2026-05-13 13:57:10 +0200

    fixed experiments look all good
    `-plot` now takes a path for the vtkhdf file
    
    require the OMP_NUM_THREADS
    
    by default timowave uses the net2as preconditioner
    -> need to turn that off

---
+++ experiments/ne9-01-conv-t.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+: "${OUT:=${OUT_DIR:=output}/$(basename "${0%.sh}")}" # set default if unset
+mkdir -p $OUT_DIR
+
+DATA_DIR=.
+DOMAIN="cross.geo"
+DOMAIN="-domain $DATA_DIR/domains/$DOMAIN"
+BIN_DIR=build/rel/experiments
+export OMP_NUM_THREADS=1
+
+parallel --progress --bar --results $OUT.json \
+  "$BIN_DIR/timowave $DOMAIN -deg 6 -nx 20 -theta {2} -test 4 -nt {1} -pc_type none" \
+  ::: 1 2 4 8 16 32 64 128 ::: 1 0.5
+echo "gen exit: $?"
+yq -i '.Stdout |= from_yaml' $OUT.json
+cp $OUT.json{,.$(date +%s)}
