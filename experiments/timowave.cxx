691ed9118d0e9029bffe734fccadd8b3d0cd0c29
Joseph Holten <joseph@holten.com>
2026-05-13 11:28:45 +0200

    bugfix

--- experiments/timowave.cxx
+++ experiments/timowave.cxx
@@ -90,7 +90,7 @@ int main(int argc, char **argv) {
     PetscCall(PetscOptionsString("-domain", "domain path", NULL, domain_path, domain_path, PATH_MAX, &is_set));
     PetscCall(PetscOptionsBool("-ksp_monitor_yaml", "set yaml ksp monitor", NULL, ksp_monitor_yaml, &ksp_monitor_yaml, &is_set));
     PetscCall(PetscOptionsInt("-test", "timowave test", NULL, timowave_test, &timowave_test, &is_set));
-    PetscCall(PetscOptionsInt("-print_timestep", "print timestep progress", NULL, print_timestep, &print_timestep, &is_set));
+    PetscCall(PetscOptionsBool("-print_timestep", "print timestep progress", NULL, print_timestep, &print_timestep, &is_set));
     PetscOptionsEnd();
 
     PetscCall(PetscOptionsGetBool(NULL, NULL, "-help", &help, &is_set));
