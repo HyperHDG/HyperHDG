6a5cf355c4b4133b95be73f94dc3cade74bd3d42
Joseph Holten <joseph@holten.com>
2026-05-28 18:21:24 +0200

    output and ref triangles

--- experiments/ne12-01-conv-trace-01.sh
+++ experiments/ne12-01-conv-trace-01.sh
@@ -6,9 +6,7 @@ mkdir -p $OUT_DIR
 
 jq -c '.Stdout | {nx, deg: .poly_deg, e_trace, tau_s, e_abs}' $OUT.json \
     | experiments/plot.py -g deg,tau_s -x nx --trans '1/x,y' -y e_trace \
-                          --log xy  --xbase 2 --xlabel 'discretization size h' \
-                          --ylabel 'maximum $L^2$ error in trace variables'
-#--ref "2;64,32;4e-3|3;64,32;3e-5|4;32,16;3e-6" \
-#--save "$OUT.png,$OUT.pgf" --figsize '2.5,2.5'
-#--legend ''
-
+        --log xy  --xbase 2 --xlabel 'discretization size h' \
+        --ylabel 'maximum $L^2$ error in trace variables' \
+        --ref "2.5;64,32;4e-3|3.5;64,32;3e-6|4.5;16,8;3e-6" \
+        --save "$OUT.png"
