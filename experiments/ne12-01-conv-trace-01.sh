ac8cfcbabd24c7b5f760478968289b5c3486847a
Joseph Holten <joseph@holten.com>
2026-05-29 10:01:37 +0200

    small update

--- experiments/ne12-01-conv-trace-01.sh
+++ experiments/ne12-01-conv-trace-01.sh
@@ -5,8 +5,8 @@ set -x
 mkdir -p $OUT_DIR
 
 jq -c '.Stdout | {nx, deg: .poly_deg, e_trace, tau_s, e_abs}' $OUT.json \
-    | experiments/plot.py -g deg,tau_s -x nx --trans '1/x,y' -y e_trace \
+    | experiments/plot.py -g deg -x nx --trans '1/x,y' -y e_trace -w 'tau_s==0'\
         --log xy  --xbase 2 --xlabel 'discretization size h' \
-        --ylabel 'maximum $L^2$ error in trace variables' \
+        --ylabel 'max $L^2$ error in $\lambda$' \
         --ref "2.5;64,32;4e-3|3.5;64,32;3e-6|4.5;16,8;3e-6" \
-        --save "$OUT.png"
+        --save "$OUT.png,$OUT.pgf" --figsize '2.5,2.5' --legend ''
