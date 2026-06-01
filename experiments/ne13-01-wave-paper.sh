e017e812fbda9ea0751d0035558a56cec7a03c4b
Joseph Holten <joseph@holten.com>
2026-06-01 10:24:34 +0200

    restructure test so that cp of old files always runs

--- experiments/ne13-01-wave-paper.sh
+++ experiments/ne13-01-wave-paper.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-set -xeo
+set -xeo pipefail
 
-BUILD=build/rel/experiments
-N=7
-OUT=output/0529-norescale
-INPUT=$HOME/phd/nextcloud/networks/morgan-2026-05-20/net1/sca/
+: ${BUILD:=build/rel/experiments}
+: ${OUTDIR:=output}
+: ${INPUT:=$HOME/phd/nextcloud/networks/morgan-2026-05-20/net1/sca/}
+NAME=$(basename -s .sh $0)
+NOW=$(date +%s)
+OUT=$OUTDIR/$NAME.$NOW
 DOMAIN=$OUT/domain.geo.h5
 TRACE=$OUT/trace.h5
 STATIC=$OUT/static.vtkhdf
 WAVE=$OUT/wave.vtkhdf
-VID=$OUT/0529-norescale.avi
-IMG=$OUT/0529-norescale.png
+VID=$OUT/$NAME.avi
+IMG=$OUT/$NAME.png
 PERF=$OUT/perf.flamegraph
 LOG=$OUT/log.yaml
 NET="-pc_type net2as -net2as_p 1 -net2as_cb_type pu -net2as_pc_factor_mat_solver_type mumps"
 export OMP_NUM_THREADS=1
 
 mkdir -p $OUT
+ln -sfn $NAME.$NOW $OUTDIR/$NAME
 cmake --build --preset rel
 python experiments/make_geo2.py -i $INPUT --clamp-xy .2 -o $DOMAIN --dirichlet xmax=68 xmin=63
 $BUILD/network -domain $DOMAIN  -comp 2 -strain .15 $NET \
@@ -28,4 +31,3 @@ experiments/netvis.py $WAVE --view iso -o $VID -r 1
 experiments/netvis.py $WAVE --view pside --frames 0,.5e-5,1e-5 --frame-colors tan,purple,blue \
                       -o $IMG --axis 0 --beams 1
 #ffmpeg -i $OUT/$NAME-wave.avi -c:v libx264 -c:a aac $OUT/$NAME-wave.mp4
-cp $OUT $OUT.$(date +%s)
