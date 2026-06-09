6b11ec04a3561277d21730bce22a841cdd4251be
Joseph Holten <joseph@holten.com>
2026-06-09 14:52:44 +0200

    comment out old test

--- experiments/timowave.cxx
+++ experiments/timowave.cxx
@@ -66,7 +66,7 @@ PetscErrorCode PetscHDGCreate(
 ) {
   PetscFunctionBeginUser;
   if      (0 == strcmp(test, "stiffness")) PetscCall(CreateDeg<TimoshenkoStiffness>(poly_deg, path, tau, theta, dt, hdg));
-  else if (0 == strcmp(test, "wave1"))     PetscCall(CreateDeg<TestTimoWave1>(poly_deg, path, tau, theta, dt, hdg));
+  //else if (0 == strcmp(test, "wave1"))     PetscCall(CreateDeg<TestTimoWave1>(poly_deg, path, tau, theta, dt, hdg));
   else if (0 == strcmp(test, "wave4"))     PetscCall(CreateDeg<TestTimoWave4>(poly_deg, path, tau, theta, dt, hdg));
   else PetscCheck(false, PETSC_COMM_WORLD, PETSC_ERR_ARG_WRONG, "unknown test = \"%s\"", test);
   PetscFunctionReturn(PETSC_SUCCESS);
