68386b62911d1a92d17a8fb8e342f70d49b03ffc
Joseph Holten <joseph@holten.com>
2026-06-16 11:47:23 +0200

    simple test with p

---
+++ experiments/ne18-01-dd-01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xeo pipefail
+
+: ${OUTDIR:=output}
+OUT=$OUTDIR/ne18-01-dd
+LOG=$OUT/log.json
+IMG=$OUT/ne18-01-dd-01-p-cond.png
+
+jq -cs 'sort_by(.net2as.nocoarse, .net2as.p[0]) | .[]
+       | {p: .net2as.p[0], nocoarse: .net2as.nocoarse, iters: .iterations, cond, creason}' $LOG \
+  | experiments/plot.py -x p -y cond -g nocoarse --save $IMG --log xy
--- experiments/ne18-01-dd.sh
+++ experiments/ne18-01-dd.sh
@@ -15,7 +15,7 @@ WAVE=$OUT/wave.vtkhdf
 VID=$OUT/$NAME.avi
 IMG=$OUT/$NAME.png
 PERF=$OUT/perf.flamegraph
-LOG=$OUT/log.yaml
+LOG=$OUT/log.json
 DIRECT="-pc_type cholesky -ksp_type preonly -pc_factor_mat_solver_type mumps"
 NET="-pc_type net2as -net2as_cb_type q1 -net2as_pc_factor_mat_solver_type mumps"
 CUT=.4
@@ -29,11 +29,9 @@ git rev-parse HEAD > $OUT/rev
 if ! git diff-index --quiet HEAD; then echo dirty >> $OUT/rev; fi
 
 python experiments/make_geo2.py -i $INPUT --clamp-xy $CUT -o $DOMAIN --dirichlet xmax=68 xmin=63
-mpirun -n 8 $BUILD/network -domain $DOMAIN  -comp 2 -strain .15 $NET -net2as_p 1 \
-               -trace_view hdf5:$TRACE -plot $STATIC | tee $LOG.1
-mpirun -n 8 $BUILD/network -domain $DOMAIN  -comp 2 -strain .15 $NET -net2as_p 2 \
-               -trace_view hdf5:$TRACE -plot $STATIC | tee $LOG.2
-mpirun -n 8 $BUILD/network -domain $DOMAIN  -comp 2 -strain .15 $NET -net2as_p 2 \
-               -trace_view hdf5:$TRACE -plot $STATIC -net2as_nocoarse | tee $LOG.3
-#experiments/netvis.py $STATIC --view pside -o $IMG --axis 0 --beams 1
-#ffmpeg -i $OUT/$NAME-wave.avi -c:v libx264 -c:a aac $OUT/$NAME-wave.mp4
+for nc in "" "-net2as_nocoarse"; do
+for p in 1 2 3 4 5; do
+  mpirun -n 8 $BUILD/network -domain $DOMAIN  -comp 2 -strain .15 $NET -net2as_p $p $nc \
+                 -trace_view hdf5:$TRACE | yq -o json -I0 >> $LOG
+done
+done
