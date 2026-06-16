5c374e25e50454287b56cbea25d63fe0800eb549
Joseph Holten <joseph@holten.com>
2026-06-16 08:42:20 +0200

    test with center

--- experiments/ne17-05-ripple-net.sh
+++ experiments/ne17-05-ripple-net.sh
@@ -33,7 +33,7 @@ VID=$OUT/$NAME.mp4
 IMG=$OUT/$NAME.png
 PERF=$OUT/perf.flamegraph
 LOG=$OUT/log.yaml
-CUT=.2
+CUT=.3
 MUMPS="-pc_type cholesky -ksp_type preonly -pc_factor_mat_solver_type mumps"
 NET="-pc_type net2as -net2as_p 1 -net2as_cb_type pu -net2as_pc_factor_mat_solver_type cholmod"
 export OMP_NUM_THREADS=1
@@ -45,12 +45,15 @@ cp $BUILD/{network,timowave} experiments/{make_geo2,netvis}.py $OUT
 git rev-parse HEAD > $OUT/rev
 if ! git diff-index --quiet HEAD; then echo dirty >> $OUT/rev; fi
 
-python experiments/make_geo2.py -i $INPUT --clamp $CUT -o $DOMAIN --dirichlet xmin=63 xmax=63 ymin=63 ymax=63 -t 1e-1
-mpirun -n 8 $BUILD/timowave -domain $DOMAIN -test drumhead \
-                -std_x 3e1 -std_t 8e-8 -energy 3e1 -tap_time 2e-7 -T 2e-6 -nt 60 $NET \
-                -plot $WAVE -deg 6 -log_view :$PERF:ascii_flamegraph -tau 1e0 \
-                -print_timestep | tee $LOG
-experiments/nethist.py $WAVE
+python experiments/make_geo2.py -i $INPUT --clamp $CUT -o $DOMAIN \
+       --dirichlet xmin=63 xmax=63 ymin=63 ymax=63 center=68 -t 3e-2
+experiments/netvis.py $DOMAIN --beams 1 --color-by types_points --color-categories 63,68
+#mpirun -n 8 $BUILD/network -domain $DOMAIN -test stiffness -comp 2 -strain .1
+#mpirun -n 8 $BUILD/timowave -domain $DOMAIN -test drumhead \
+#                -std_x 1e2 -std_t 8e-8 -energy 3e+1 -tap_time 2e-7 -T 2e-6 -nt 60 $NET \
+#                -plot $WAVE -deg 6 -log_view :$PERF:ascii_flamegraph -tau 1e0 \
+#                -print_timestep | tee $LOG
+#experiments/nethist.py $WAVE
 #experiments/netvis.py $WAVE --view iso --beams 1 -o $AVI --color-by values:8
 #ffmpeg -i $AVI -c:v libx264 -c:a aac $VID
 #experiments/nethist.py $WAVE --bins 100
