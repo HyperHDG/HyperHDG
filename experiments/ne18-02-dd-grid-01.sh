ad76355d9bb8f3fbc7485e996e993a2bf5ec0f1c
Joseph Holten <joseph@holten.com>
2026-06-16 12:36:33 +0200

    test with grid => bounded cond over large p

---
+++ experiments/ne18-02-dd-grid-01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xeo pipefail
+
+: ${OUTDIR:=output}
+OUT=$OUTDIR/ne18-02-dd-grid
+LOG=$OUT/log.json
+IMG=$OUT/ne18-02-dd-grid-01-p-cond.png
+
+jq -cs 'sort_by(.net2as.nocoarse, .net2as.p[0]) | .[]
+       | {p: .net2as.p[0], nocoarse: .net2as.nocoarse, iters: .iterations, cond, creason}' $LOG \
+  | experiments/plot.py -x p -y cond -g nocoarse --save $IMG --log xy
