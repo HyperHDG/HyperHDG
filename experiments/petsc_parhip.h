c6248ee3754278a510557b97c63d21edeb43cdb0
Joseph Holten <joseph@holten.com>
2026-06-18 00:32:47 +0200

    cleanup: remove old code and restructure folders

--- experiments/petsc_parhip.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <petsc.h>
-
-PetscErrorCode MatPartitioningApply_ParHIP(MatPartitioning part, IS *partition);
-PetscErrorCode MatPartitioningDestroy_ParHIP(MatPartitioning part);
-PetscErrorCode MatPartitioningCreate_ParHIP(MatPartitioning part);
-
