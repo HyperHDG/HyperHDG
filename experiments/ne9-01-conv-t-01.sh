f940d35c456256ad50725c29a0ce182676ea2c2f
Joseph Holten <joseph@holten.com>
2026-07-02 13:12:27 +0200

    plot relative error and output tikz

--- experiments/ne9-01-conv-t-01.sh
+++ experiments/ne9-01-conv-t-01.sh
@@ -4,10 +4,10 @@ set -x
 : "${OUT:=${OUT_DIR:=output}/ne9-01-conv-t}"
 mkdir -p $OUT_DIR
 
-jq -c '.Stdout | {dt, e_abs, theta}' $OUT.json \
-    | experiments/plot.py -g theta -x dt -y e_abs --log xy \
+jq -c '.Stdout | {dt, e_rel, e_abs, theta}' $OUT.json \
+    | experiments/plot.py -g theta -x dt -y e_rel --log xy \
         --xbase 2 --figsize '2.5,2.5' \
-        --ref "2;.015625,.03125;2e-3|1;1e-2,2e-2;1e-1" \
-        --save "$OUT.png,$OUT.pgf" \
+        --ref "2;.015625,.03125;2e-3|1;1e-2,2e-2;8e-2" \
+        --save "$OUT.png,$OUT.pgf" --tikz "$OUT" \
         --xlabel 'timestep size $\Delta t$' --ylabel 'maximum $L^2$ error' \
         --legend ''
