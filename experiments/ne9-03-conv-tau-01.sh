31c564c1dac250da1b74f2e7688281a3bb481130
Joseph Holten <joseph@holten.com>
2026-07-02 14:12:31 +0200

    update tau plot to tikz

--- experiments/ne9-03-conv-tau-01.sh
+++ experiments/ne9-03-conv-tau-01.sh
@@ -4,10 +4,8 @@ set -x
 : "${OUT:=${OUT_DIR:=output}/ne9-03-conv-tau}"
 mkdir -p $OUT_DIR
 
-jq -c '.Stdout | {nx, e_abs, tau_s}' $OUT.json \
-    | experiments/plot.py -x nx --trans '1/x,y' -y e_abs --log xy -g tau_s \
+jq -c '.Stdout | {nx, e_rel, tau_s}' $OUT.json \
+    | experiments/plot.py -x nx --trans '1/x,y' -y e_rel --log xy -g tau_s \
         --xbase 2 --xlabel 'discretization size h' --ylabel 'maximum $L^2$ error' \
-        --figsize '2.5,2.5' --save "$OUT.png,$OUT.pgf" \
-        --ref "2;64,32;6e-3|1;64,32;7e-2|2;32,16;1e-3" --legen ''
-#--ref "2;64,32;4e-3|3;64,32;3e-5|4;32,16;3e-6"
-# --legend ''
+        --figsize '2.5,2.5' --save "$OUT.png,$OUT.pgf" --tikz "$OUT" \
+        --ref "2;64,32;4e-3|1;64,32;9e-2|2;32,16;1e-3" --legend ''
