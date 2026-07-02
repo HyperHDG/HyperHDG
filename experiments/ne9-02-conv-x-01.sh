026f1c9e0610bc8c8df5f1f788f05e47f72411ba
Joseph Holten <joseph@holten.com>
2026-07-02 13:32:44 +0200

    update spatial convergence plot

--- experiments/ne9-02-conv-x-01.sh
+++ experiments/ne9-02-conv-x-01.sh
@@ -4,5 +4,8 @@ set -x
 : "${OUT:=${OUT_DIR:=output}/ne9-02-conv-x}"
 mkdir -p $OUT_DIR
 
-jq -c '.Stdout | {nx, e_abs, deg: .poly_deg}' $OUT.json \
-    | experiments/plot.py -g deg -x nx --trans '1/x,y' -y e_abs --log xy --save "$OUT.png,$OUT.pgf" --xbase 2 --xlabel 'discretization size h' --ylabel 'maximum $L^2$ error' --figsize '2.5,2.5' --legend '' --ref "2;64,32;4e-3|3;64,32;3e-5|4;32,16;3e-6" \
+jq -c '.Stdout | {nx, e_rel, deg: .poly_deg}' $OUT.json \
+  | experiments/plot.py -g deg -x nx --trans '1/x,y' -y e_rel --log xy \
+      --save "$OUT.png,$OUT.pgf" --tikz "$OUT" \
+      --xbase 2 --xlabel 'discretization size h' --ylabel 'maximum $L^2$ error' \
+      --legend '' --ref "2;64,32;4e-3|3;64,32;3e-5|4;32,16;3e-6"
