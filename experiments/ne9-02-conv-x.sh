24ed729f0c4fabfae779ad8c34fdd017f7f8e4ce
Joseph Holten <joseph@holten.com>
2026-07-13 13:12:18 +0200

    ne9-02: direct solve (preonly+lu) - removes CG rtol floor at deg3/nx64

--- experiments/ne9-02-conv-x.sh
+++ experiments/ne9-02-conv-x.sh
@@ -11,7 +11,7 @@ BIN_DIR=build/rel/experiments
 export OMP_NUM_THREADS=1
 
 parallel --progress --bar --results $OUT.json \
-  "$BIN_DIR/timowave $DOMAIN -deg {2} -nx {1} -theta .5 -test wave4 -nt {3} -pc_type none" \
+  "$BIN_DIR/timowave $DOMAIN -deg {2} -nx {1} -theta .5 -test wave4 -nt {3} -ksp_type preonly -pc_type lu" \
   ::: 2 4 8 16 32 64 ::: 1 2 3 :::+ 500 1000 8000
 echo "gen exit: $?"
 yq -i '.Stdout |= from_yaml' $OUT.json
